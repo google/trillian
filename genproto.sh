36964637abb28acba3a1e06f9bf05772154b995e
Al Cutter <al@google.com>
2016-06-30 15:30:36 +0100

    fix genproto

--- genproto.sh
+++ genproto.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PROTODIRS=" $(find . -name \*.proto -printf '%h\n' | sort | uniq | xargs echo)"
+PROTODIRS=" $(find `pwd` -name \*.proto -printf '%h\n' | sort | uniq | xargs echo)"
 INCDIRS=${PROTODIRS// / -I }
 for DIR in ${PROTODIRS}; do
   CMD="protoc --go_out=plugins=grpc:. ${INCDIRS} ${DIR}/*.proto"
