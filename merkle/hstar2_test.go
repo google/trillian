66eba53f8ea481fe2a8c338163508ac2aa47725c
Al Cutter <al@google.com>
2016-07-05 17:11:27 +0100

    Allow hstar2 to calculate subroots of larger trees

--- merkle/hstar2_test.go
+++ merkle/hstar2_test.go
@@ -98,7 +98,7 @@ func TestHStar2GetSet(t *testing.T) {
 		if len(values) != 1 {
 			t.Fatalf("Should only have 1 leaf per run, got %d", len(values))
 		}
-		root, err := s.HStar2Nodes(s.hasher.Size()*8, values,
+		root, err := s.HStar2Nodes(s.hasher.Size()*8, 0, values,
 			func(depth int, index *big.Int) (trillian.Hash, error) {
 				return cache[fmt.Sprintf("%x/%d", index, depth)], nil
 			},
@@ -111,6 +111,79 @@ func TestHStar2GetSet(t *testing.T) {
 		}
 		if expected, got := mustDecode(x.rootB64), root; !bytes.Equal(expected, got) {
 			t.Fatalf("Expected root:\n%v\nGot:\n%v", base64.StdEncoding.EncodeToString(expected), base64.StdEncoding.EncodeToString(got))
+		}
+	}
+}
+
+// Checks that we calculate the same empty root hash as a 256-level tree has
+// when calculating top subtrees using an appropriate offset.
+func TestHStar2OffsetEmptyRootKAT(t *testing.T) {
+	th := NewRFC6962TreeHasher(trillian.NewSHA256())
+	s := NewHStar2(th)
+
+	for size := 1; size < 255; size++ {
+		root, err := s.HStar2Nodes(size, s.hasher.Size()*8-size, []HStar2LeafHash{},
+			func(int, *big.Int) (trillian.Hash, error) { return nil, nil },
+			func(int, *big.Int, trillian.Hash) error { return nil })
+		if err != nil {
+			t.Fatalf("Failed to calculate root %v", err)
+		}
+		if expected, got := mustDecode(sparseEmptyRootHashB64), root; !bytes.Equal(expected, got) {
+			t.Fatalf("Expected root:\n%v\nGot:\n%v", base64.StdEncoding.EncodeToString(expected), base64.StdEncoding.EncodeToString(got))
+		}
+	}
+}
+
+// Create intermediate "root" values for the passed in HStar2LeafHashes.
+// These "root" hashes are from (assumed distinct) subtrees of size
+// 256-prefixSize, and can be passed in as leaves to top-subtree calculation.
+func rootsForTrimmedKeys(t *testing.T, prefixSize int, lh []HStar2LeafHash) []HStar2LeafHash {
+	ret := make([]HStar2LeafHash, 0)
+	th := NewRFC6962TreeHasher(trillian.NewSHA256())
+	s := NewHStar2(th)
+	for i := range lh {
+		prefix := new(big.Int).Rsh(lh[i].Index, uint(s.hasher.Size()*8-prefixSize))
+		b := lh[i].Index.Bytes()
+		// ensure we've got any chopped of leading zero bytes
+		for len(b) < 32 {
+			b = append([]byte{0}, b...)
+		}
+		lh[i].Index.SetBytes(b[prefixSize/8:])
+		root, err := s.HStar2Root(s.hasher.Size()*8-prefixSize, []HStar2LeafHash{lh[i]})
+		if err != nil {
+			t.Fatalf("Failed to calculate root %v", err)
+		}
+		ret = append(ret, HStar2LeafHash{prefix, root})
+	}
+	return ret
+}
+
+// Checks that splitting the calculation of a 256-level tree into two phases
+// (single top subtree of size n, and multipl bottom subtrees of size 256-n)
+// still arrives at the same Known Answers for root hash.
+func TestHStar2OffsetRootKAT(t *testing.T) {
+	th := NewRFC6962TreeHasher(trillian.NewSHA256())
+	s := NewHStar2(th)
+
+	m := make(map[string]string)
+
+	for i, x := range simpleTestVector {
+		// start at 24 so we can assume that key prefixes are probably unique by then
+		// TODO(al): improve rootsForTrimmedKeys to use a map and remove this
+		// requirement.
+		for size := 24; size < 256; size += 8 {
+			m[x.k] = x.v
+			intermediates := rootsForTrimmedKeys(t, size, createHStar2Leaves(th, m))
+
+			root, err := s.HStar2Nodes(size, s.hasher.Size()*8-size, intermediates,
+				func(int, *big.Int) (trillian.Hash, error) { return nil, nil },
+				func(int, *big.Int, trillian.Hash) error { return nil })
+			if err != nil {
+				t.Fatalf("Failed to calculate root at iteration %d: %v", i, err)
+			}
+			if expected, got := mustDecode(x.rootB64), root; !bytes.Equal(expected, got) {
+				t.Fatalf("Expected root:\n%v\nGot:\n%v", base64.StdEncoding.EncodeToString(expected), base64.StdEncoding.EncodeToString(got))
+			}
 		}
 	}
 }
