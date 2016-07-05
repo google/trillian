d458163b4e88b7354553114e65962fbb40fe6140
Martin Smith <mhs@google.com>
2016-07-05 15:35:20 +0100

    Fix sequencer tests

--- server/sequencer_manager_test.go
+++ server/sequencer_manager_test.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"fmt"
+	"io"
 	"testing"
 	"time"
 
@@ -9,7 +10,7 @@ import (
 	"github.com/google/trillian/crypto"
 	"github.com/google/trillian/storage"
 	"github.com/google/trillian/util"
-	"errors"
+	"github.com/stretchr/testify/mock"
 )
 
 // Arbitrary time for use in tests
@@ -22,32 +23,11 @@ var testLeaf0Hash = trillian.Hash{0, 1, 2, 3, 4, 5}
 var testLeaf0 = trillian.LogLeaf{Leaf: trillian.Leaf{LeafHash: testLeaf0Hash, LeafValue: nil, ExtraData: nil}, SequenceNumber: 0}
 var testRoot0 = trillian.SignedLogRoot{TreeSize: 0, TreeRevision: 0, LogId: logID1.LogID}
 var updatedNodes0 = []storage.Node{storage.Node{NodeID: storage.NodeID{Path: []uint8{0x0}, PrefixLenBits: 0, PathLenBits: 6}, Hash: trillian.Hash{0x0, 0x1, 0x2, 0x3, 0x4, 0x5}, NodeRevision: 1}}
-var updatedRoot = trillian.SignedLogRoot{LogId: logID1.LogID, TimestampNanos: fakeTime.UnixNano(), RootHash: []uint8{0x0, 0x1, 0x2, 0x3, 0x4, 0x5}, TreeSize: 1, Signature: &trillian.DigitallySigned{}, TreeRevision: 1}
+var updatedRoot = trillian.SignedLogRoot{LogId: logID1.LogID, TimestampNanos: fakeTime.UnixNano(), RootHash: []uint8{0x0, 0x1, 0x2, 0x3, 0x4, 0x5}, TreeSize: 1, Signature: &trillian.DigitallySigned{Signature: []byte("signed")}, TreeRevision: 1}
 
 func TestSequencerManagerNothingToDo(t *testing.T) {
 	mockStorage := new(storage.MockLogStorage)
 	mockTx := new(storage.MockLogTX)
-	mockKeyManager := new(crypto.MockKeyManager)
-
-	mockStorage.On("Begin").Return(mockTx, nil)
-	mockTx.On("GetActiveLogIDs").Return([]trillian.LogID{}, nil)
-	mockTx.On("Commit").Return(nil)
-
-	sm := NewSequencerManager(mockKeyManager)
-
-	sm.ExecutePass([]trillian.LogID{}, createTestContext(mockStorageProviderForSequencer(mockStorage)))
-
-	mockStorage.AssertExpectations(t)
-	mockTx.AssertExpectations(t)
-}
-
-func TestSequencerManagerSingleLogErrorOnGetLogIDs(t *testing.T) {
-	mockStorage := new(storage.MockLogStorage)
-	mockTx := new(storage.MockLogTX)
-
-	mockStorage.On("Begin").Return(mockTx, nil)
-	mockTx.On("GetActiveLogIDs").Return([]trillian.LogID{}, errors.New("getactivelogidsfailed"))
-	mockTx.On("Rollback").Return(nil)
 	mockKeyManager := new(crypto.MockKeyManager)
 
 	sm := NewSequencerManager(mockKeyManager)
@@ -64,7 +44,6 @@ func TestSequencerManagerSingleLogNoLeaves(t *testing.T) {
 	logID := trillian.LogID{TreeID: 1, LogID: []byte("Test")}
 
 	mockStorage.On("Begin").Return(mockTx, nil)
-	mockTx.On("GetActiveLogIDs").Return([]trillian.LogID{logID}, nil)
 	mockTx.On("Commit").Return(nil)
 	mockTx.On("DequeueLeaves", 50).Return([]trillian.LogLeaf{}, nil)
 	mockKeyManager := new(crypto.MockKeyManager)
@@ -82,10 +61,10 @@ func TestSequencerManagerSingleLogOneLeaf(t *testing.T) {
 	mockTx := new(storage.MockLogTX)
 	mockKeyManager := new(crypto.MockKeyManager)
 	logID := trillian.LogID{TreeID: 1, LogID: []byte("Test")}
+	hasher := trillian.NewSHA256()
 
 	// Set up enough mockery to be able to sequence. We don't test all the error paths
 	// through sequencer as other tests cover this
-	mockTx.On("GetActiveLogIDs").Return([]trillian.LogID{logID}, nil)
 	mockTx.On("Commit").Return(nil)
 	mockTx.On("DequeueLeaves", 50).Return([]trillian.LogLeaf{testLeaf0}, nil)
 	mockTx.On("LatestSignedLogRoot").Return(testRoot0, nil)
@@ -94,12 +73,21 @@ func TestSequencerManagerSingleLogOneLeaf(t *testing.T) {
 	mockTx.On("StoreSignedLogRoot", updatedRoot).Return(nil)
 	mockStorage.On("Begin").Return(mockTx, nil)
 
+	mockSigner := new(crypto.MockSigner)
+	mockSigner.On("Sign", mock.MatchedBy(
+		func(other io.Reader) bool {
+			return true
+		}), []byte{0x17, 0xe8, 0x8d, 0xb1, 0x87, 0xaf, 0xd6, 0x2c, 0x16, 0xe5, 0xde, 0xbf, 0x3e, 0x65, 0x27, 0xcd, 0x0, 0x6b, 0xc0, 0x12, 0xbc, 0x90, 0xb5, 0x1a, 0x81, 0xc, 0xd8, 0xc, 0x2d, 0x51, 0x1f, 0x43}, hasher).Return([]byte("signed"), nil)
+	mockKeyManager.On("Signer").Return(*mockSigner, nil)
+
 	sm := NewSequencerManager(mockKeyManager)
 
 	sm.ExecutePass([]trillian.LogID{logID}, createTestContext(mockStorageProviderForSequencer(mockStorage)))
 
 	mockStorage.AssertExpectations(t)
 	mockTx.AssertExpectations(t)
+	mockSigner.AssertExpectations(t)
+	mockKeyManager.AssertExpectations(t)
 }
 
 func mockStorageProviderForSequencer(mockStorage storage.LogStorage) LogStorageProviderFunc {
