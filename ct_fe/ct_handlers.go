96396154731fcc01b1fa7e99572937d126607978
Martin Smith <mhs@google.com>
2016-07-07 09:03:34 +0100

    Add RPC client connection

--- ct_fe/ct_handlers.go
+++ ct_fe/ct_handlers.go
@@ -2,8 +2,11 @@ package ct_fe
 
 import (
 	"encoding/json"
-	"net/http"
+	"errors"
 	"io/ioutil"
+	"net/http"
+
+	"github.com/google/trillian"
 )
 
 const (
@@ -14,12 +17,20 @@ const (
 	httpMethodGet = "GET"
 )
 
+type CtRequestHandlers struct {
+	rpcClient trillian.TrillianLogClient
+}
+
+func NewCtRequestHandlers(rpcClient trillian.TrillianLogClient) *CtRequestHandlers {
+	return &CtRequestHandlers{rpcClient}
+}
+
 func pathFor(req string) string {
 	return ctV1BasePath + req
 }
 
 type addChainRequest struct {
-	chain []string
+	Chain []string
 }
 
 func parseBodyAsJSONChain(w http.ResponseWriter, r *http.Request) (addChainRequest, error) {
@@ -32,15 +43,22 @@ func parseBodyAsJSONChain(w http.ResponseWriter, r *http.Request) (addChainReque
 
 	var req addChainRequest
 	if err := json.Unmarshal(body, &req); err != nil {
-		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		http.Error(w, http.StatusText(http.StatusBadRequest) + ": " + err.Error(), http.StatusBadRequest)
 		return addChainRequest{}, err
+	}
+
+	// The cert chain is not allowed to be empty. We'll defer other validation for later
+	if len(req.Chain) == 0 {
+		http.Error(w, http.StatusText(http.StatusBadRequest) + ": cert chain cannot be empty", http.StatusBadRequest)
+		return addChainRequest{}, errors.New("cert chain was empty")
 	}
 
 	return req, nil
 }
 
-// enforceMethod checks that the request method is the one we expect. If it returns
-// false then the http status has been set appropriately
+// enforceMethod checks that the request method is the one we expect and does some additional
+// common request validation. If it returns false then the http status has been set appropriately
+// and no further action is needed
 func enforceMethod(w http.ResponseWriter, r *http.Request, method string) bool {
 	if r.Method != method {
 		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
@@ -135,7 +153,9 @@ func getEntryAndProofHandler(w http.ResponseWriter, r *http.Request) {
 	http.Error(w, http.StatusText(http.StatusNotImplemented), http.StatusNotImplemented)
 }
 
-func RegisterCTHandlers() {
+// RegisterCTHandlers registers a HandleFunc for all of the RFC6962 defined methods.
+// TODO(Martin2112): This registers on default ServeMux, might need more flexibility?
+func (c CtRequestHandlers) RegisterCTHandlers() {
 	http.HandleFunc(pathFor("add-chain"), addChainHandler)
 	http.HandleFunc(pathFor("add-pre-chain"), addPreChainHandler)
 	http.HandleFunc(pathFor("get-sth"), getSthHandler)
--- ct_fe/ct_server/main.go
+++ ct_fe/ct_server/main.go
@@ -7,14 +7,31 @@ import (
 
 	"github.com/google/trillian/ct_fe"
 	"github.com/golang/glog"
+	"google.golang.org/grpc"
+	"github.com/google/trillian"
 )
 
+var rpcBackendFlag = flag.String("log_rpc_backend", "localhost:8090", "Backend Log RPC server to use")
 var serverPortFlag = flag.Int("port", 8091, "Port to serve CT log requests on")
 
-// TODO(Martin2112): Support TLS and other stuff, this is just to get started
 func main() {
 	flag.Parse()
 
-	ct_fe.RegisterCTHandlers()
+	// TODO(Martin2112): Support TLS and other stuff for RPC client and http server, this is just to
+	// get started. Uses a blocking connection so we don't start serving before we're connected
+	// to backend.
+	conn, err := grpc.Dial(*rpcBackendFlag, grpc.WithInsecure(), grpc.WithBlock())
+
+	if err != nil {
+		glog.Fatalf("Could not connect to rpc server: %v", err)
+	}
+
+	defer conn.Close()
+  client := trillian.NewTrillianLogClient(conn)
+
+	// Create and register the handlers using the RPC client we just set up
+	handlers := ct_fe.NewCtRequestHandlers(client)
+	handlers.RegisterCTHandlers()
+
 	glog.Warningf("Server exited: %v", http.ListenAndServe("localhost:" + strconv.Itoa(*serverPortFlag), nil))
 }
