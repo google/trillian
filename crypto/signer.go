b1d2d5080b28b11590e8ae9a671d715b90eebb15
Martin Smith <mhs@google.com>
2016-07-08 14:00:31 +0100

    Summon the awesome power of objecthash to the project
    Add code to signer to serialize and sign log roots and update tests.

--- crypto/signer.go
+++ crypto/signer.go
@@ -3,10 +3,23 @@ package crypto
 import (
 	"crypto"
 	"crypto/rand"
+	"encoding/json"
 	"fmt"
 
+	"github.com/benlaurie/objecthash/go/objecthash"
 	"github.com/google/trillian"
+	"github.com/golang/glog"
 )
+
+// rootForSerialization is used when creating the JSON to be fed to the hasher for signed roots
+// Using this may look like copying protos to other protos but we want to ensure that only
+// expected fields are included in the hash as this cannot change without impacting clients and
+// the SignedLogRoot proto could evolve.
+type rootForSerialization struct {
+	TimestampNanos int64
+	TreeSize       int64
+	RootHash       []byte
+}
 
 // Signer is responsible for signing log-related data and producing the appropriate
 // application specific signature objects.
@@ -42,4 +55,43 @@ func (s Signer) Sign(data []byte) (trillian.DigitallySigned, error) {
 		SignatureAlgorithm: s.sigAlgorithm,
 		HashAlgorithm:      s.hasher.HashAlgorithm(),
 		Signature:          sig}, nil
+}
+
+// LogRootToJson returns the JSON representation of a signed log root that should be fed to
+// objecthash when verifying a signed root.
+// TODO(Martin2112): Decouple code to verify roots from Signer as you need a private
+// key to create these objects.
+func (s Signer) LogRootToJson(root trillian.SignedLogRoot) (string, error) {
+	rootToSerialize := rootForSerialization{TimestampNanos: root.TimestampNanos, TreeSize: root.TreeSize, RootHash: root.RootHash}
+
+	serializedJson, err := json.Marshal(&rootToSerialize)
+
+	if err != nil {
+		glog.Warningf("Failed to create json root for signing: %v", err)
+		return "", err
+	}
+
+	return string(serializedJson), nil
+}
+
+// SignLogRoot updates a log root to include a signature from the crypto signer this object
+// was created with. Signatures use objecthash on a fixed JSON format of the root.
+func (s Signer) SignLogRoot(root *trillian.SignedLogRoot) error {
+	serializedJson, err := s.LogRootToJson(*root)
+
+	if err != nil {
+		// already logged, just return
+		return err
+	}
+
+	objectHash := objecthash.CommonJSONHash(serializedJson)
+	signature, err := s.Sign(objectHash[:])
+
+	if err != nil {
+		glog.Warningf("Signer failed to sign root: %v", err)
+		return err
+	}
+
+	(*root).Signature = &signature
+	return nil
 }
--- crypto/signer_test.go
+++ crypto/signer_test.go
@@ -48,11 +48,7 @@ func TestSigner(t *testing.T) {
 			return s.HashFunc() == crypto.SHA256
 		})).Return([]byte(result), nil)
 
-	hasher, err := trillian.NewHasher(trillian.HashAlgorithm_SHA256)
-	if err != nil {
-		t.Fatalf("Failed to create new hasher: %s", err)
-	}
-	logSigner := NewSigner(hasher, trillian.SignatureAlgorithm_RSA, mockSigner)
+	logSigner := createTestSigner(t, *mockSigner)
 
 	sig, err := logSigner.Sign([]byte(message))
 
@@ -77,13 +73,9 @@ func TestSignerFails(t *testing.T) {
 		digest[:],
 		mock.MatchedBy(func(s crypto.SignerOpts) bool { return s.HashFunc() == crypto.SHA256 })).Return(digest, errors.New("sign"))
 
-	hasher, err := trillian.NewHasher(trillian.HashAlgorithm_SHA256)
-	if err != nil {
-		t.Fatalf("Failed to create new hasher: %s", err)
-	}
-	logSigner := NewSigner(hasher, trillian.SignatureAlgorithm_RSA, mockSigner)
+	logSigner := createTestSigner(t, *mockSigner)
 
-	_, err = logSigner.Sign([]byte(message))
+	_, err := logSigner.Sign([]byte(message))
 
 	if err == nil {
 		t.Fatalf("Ignored a signing error: %v", err)
@@ -92,6 +84,70 @@ func TestSignerFails(t *testing.T) {
 	ensureErrorContains(t, err, "sign")
 
 	mockSigner.AssertExpectations(t)
+}
+
+func TestLogRootToJson(t *testing.T) {
+	mockSigner := new(mockSigner)
+	logSigner := createTestSigner(t, *mockSigner)
+	root := trillian.SignedLogRoot{TimestampNanos: 2267709, RootHash: []byte("Islington"), TreeSize: 2}
+	jsonRoot, err := logSigner.LogRootToJson(root)
+
+	if err != nil {
+		t.Fatalf("Error in serializing root: %v", err)
+	}
+
+	// Binary data like the root hash gets base64 encoded
+	expected := `{"TimestampNanos":2267709,"TreeSize":2,"RootHash":"SXNsaW5ndG9u"}`
+	assert.Equal(t, expected, jsonRoot, "Incorrect log root serialization format")
+	mockSigner.AssertExpectations(t)
+}
+
+func TestSignLogRootSignerFails(t *testing.T) {
+	mockSigner := new(mockSigner)
+
+	mockSigner.On("Sign",
+		mock.MatchedBy(func(io.Reader) bool {
+			return true
+		}),
+		[]byte{0x53, 0x71, 0x31, 0x25, 0x6, 0x6d, 0xc7, 0x66, 0xa4, 0xdf, 0x1f, 0xd3, 0x4b, 0x40, 0x73, 0x1e, 0x4a, 0xc0, 0x50, 0xa2, 0xf9, 0x5c, 0x54, 0x7b, 0xa2, 0xea, 0x83, 0xa1, 0xf0, 0xb6, 0x8c, 0xea},
+		mock.MatchedBy(func(s crypto.SignerOpts) bool {
+			return s.HashFunc() == crypto.SHA256
+		})).Return([]byte{}, errors.New("signfail"))
+
+	logSigner := createTestSigner(t, *mockSigner)
+
+	root := trillian.SignedLogRoot{TimestampNanos: 2267709, RootHash: []byte("Islington"), TreeSize: 2}
+	err := logSigner.SignLogRoot(&root)
+
+	ensureErrorContains(t, err, "signfail")
+}
+
+func TestSignLogRoot(t *testing.T) {
+	mockSigner := new(mockSigner)
+
+	mockSigner.On("Sign",
+		mock.MatchedBy(func(io.Reader) bool {
+			return true
+		}),
+		[]byte{0x53, 0x71, 0x31, 0x25, 0x6, 0x6d, 0xc7, 0x66, 0xa4, 0xdf, 0x1f, 0xd3, 0x4b, 0x40, 0x73, 0x1e, 0x4a, 0xc0, 0x50, 0xa2, 0xf9, 0x5c, 0x54, 0x7b, 0xa2, 0xea, 0x83, 0xa1, 0xf0, 0xb6, 0x8c, 0xea},
+		mock.MatchedBy(func(s crypto.SignerOpts) bool {
+			return s.HashFunc() == crypto.SHA256
+		})).Return([]byte(result), nil)
+
+	logSigner := createTestSigner(t, *mockSigner)
+
+	root := trillian.SignedLogRoot{TimestampNanos: 2267709, RootHash: []byte("Islington"), TreeSize: 2}
+	err := logSigner.SignLogRoot(&root)
+
+	if err != nil {
+		t.Fatalf("Failed to sign log root: %v", err)
+	}
+
+	expected := trillian.SignedLogRoot{TimestampNanos: 2267709, RootHash: []byte("Islington"), TreeSize: 2,
+		Signature: &trillian.DigitallySigned{SignatureAlgorithm:trillian.SignatureAlgorithm_RSA,
+			HashAlgorithm: trillian.HashAlgorithm_SHA256,
+			Signature: []byte("echo")}}
+	assert.Equal(t, expected, root, "Expected a correctly signed root")
 }
 
 // TODO(Martin2112): Tidy up so we only have one copy of this
@@ -103,4 +159,13 @@ func ensureErrorContains(t *testing.T, err error, s string) {
 	if !strings.Contains(err.Error(), s) {
 		t.Errorf("Got the wrong type of error: %v", err)
 	}
+}
+
+func createTestSigner(t *testing.T, mock mockSigner) *Signer {
+	hasher, err := trillian.NewHasher(trillian.HashAlgorithm_SHA256)
+	if err != nil {
+		t.Fatalf("Failed to create new hasher: %s", err)
+	}
+
+	return NewSigner(hasher, trillian.SignatureAlgorithm_RSA, mock)
 }
