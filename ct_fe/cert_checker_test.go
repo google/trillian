981a6d33cc80bed79d7f0170e3d260da00836e8f
Martin Smith <mhs@google.com>
2016-07-13 13:17:17 +0100

    Move test certs to testonly package

--- ct_fe/cert_checker_test.go
+++ ct_fe/cert_checker_test.go
@@ -6,32 +6,12 @@ import (
 
 	"github.com/google/certificate-transparency/go/x509"
 	"github.com/google/certificate-transparency/go/x509/pkix"
+	"github.com/google/trillian/testonly"
 	"github.com/stretchr/testify/assert"
 )
 
-const validPrecertPEM string = `
------BEGIN CERTIFICATE-----
-MIIC3zCCAkigAwIBAgIBBzANBgkqhkiG9w0BAQUFADBVMQswCQYDVQQGEwJHQjEk
-MCIGA1UEChMbQ2VydGlmaWNhdGUgVHJhbnNwYXJlbmN5IENBMQ4wDAYDVQQIEwVX
-YWxlczEQMA4GA1UEBxMHRXJ3IFdlbjAeFw0xMjA2MDEwMDAwMDBaFw0yMjA2MDEw
-MDAwMDBaMFIxCzAJBgNVBAYTAkdCMSEwHwYDVQQKExhDZXJ0aWZpY2F0ZSBUcmFu
-c3BhcmVuY3kxDjAMBgNVBAgTBVdhbGVzMRAwDgYDVQQHEwdFcncgV2VuMIGfMA0G
-CSqGSIb3DQEBAQUAA4GNADCBiQKBgQC+75jnwmh3rjhfdTJaDB0ym+3xj6r015a/
-BH634c4VyVui+A7kWL19uG+KSyUhkaeb1wDDjpwDibRc1NyaEgqyHgy0HNDnKAWk
-EM2cW9tdSSdyba8XEPYBhzd+olsaHjnu0LiBGdwVTcaPfajjDK8VijPmyVCfSgWw
-FAn/Xdh+tQIDAQABo4HBMIG+MB0GA1UdDgQWBBQgMVQa8lwF/9hli2hDeU9ekDb3
-tDB9BgNVHSMEdjB0gBRfnYgNyHPmVNT4DdjmsMEktEfDVaFZpFcwVTELMAkGA1UE
-BhMCR0IxJDAiBgNVBAoTG0NlcnRpZmljYXRlIFRyYW5zcGFyZW5jeSBDQTEOMAwG
-A1UECBMFV2FsZXMxEDAOBgNVBAcTB0VydyBXZW6CAQAwCQYDVR0TBAIwADATBgor
-BgEEAdZ5AgQDAQH/BAIFADANBgkqhkiG9w0BAQUFAAOBgQACocOeAVr1Tf8CPDNg
-h1//NDdVLx8JAb3CVDFfM3K3I/sV+87MTfRxoM5NjFRlXYSHl/soHj36u0YtLGhL
-BW/qe2O0cP8WbjLURgY1s9K8bagkmyYw5x/DTwjyPdTuIo+PdPY9eGMR3QpYEUBf
-kGzKLC0+6/yBmWTr2M98CIY/vg==
------END CERTIFICATE-----
-`
-
 func TestIsPrecertificate(t *testing.T) {
-	cert := pemToCert(t, validPrecertPEM)
+	cert := pemToCert(t, testonly.PrecertPEMValid)
 
 	isPrecert, err := IsPrecertificate(cert)
 
@@ -47,7 +27,7 @@ func TestIsPrecertificate(t *testing.T) {
 }
 
 func TestIsPrecertificateInvalidNonCriticalExtension(t *testing.T) {
-	cert := pemToCert(t, validPrecertPEM)
+	cert := pemToCert(t, testonly.PrecertPEMValid)
 	// Invalid because it's not marked as critical
 	ext := pkix.Extension{Id: ctPoisonExtensionOid, Critical: false, Value: asn1NullBytes}
 
@@ -58,7 +38,7 @@ func TestIsPrecertificateInvalidNonCriticalExtension(t *testing.T) {
 }
 
 func TestIsPrecertificateInvalidBytesInExtension(t *testing.T) {
-	cert := pemToCert(t, validPrecertPEM)
+	cert := pemToCert(t, testonly.PrecertPEMValid)
 	// Invalid because it's not asn.1 null
 	ext := pkix.Extension{Id: ctPoisonExtensionOid, Critical: false, Value: []byte{0x42, 0x42, 0x42}}
 
