258d81eea7232d703d4d5dff9fc2fb52de84c7a3
Martin Smith <mhs@google.com>
2016-07-20 13:48:12 +0100

    Add some chain tests for cert checker

--- examples/ct/cert_checker_test.go
+++ examples/ct/cert_checker_test.go
@@ -1,6 +1,7 @@
 package ct
 
 import (
+	"encoding/base64"
 	"encoding/pem"
 	"testing"
 
@@ -43,7 +44,7 @@ func TestIsPrecertificateInvalidNonCriticalExtension(t *testing.T) {
 	cert.Extensions = []pkix.Extension{ext}
 	_, err := IsPrecertificate(cert)
 
-	assert.Error(t, err, "Incorrectly accepted non critical CT extension")
+	assert.Error(t, err, "incorrectly accepted non critical CT extension")
 }
 
 func TestIsPrecertificateInvalidBytesInExtension(t *testing.T) {
@@ -54,7 +55,87 @@ func TestIsPrecertificateInvalidBytesInExtension(t *testing.T) {
 	cert.Extensions = []pkix.Extension{ext}
 	_, err := IsPrecertificate(cert)
 
-	assert.Error(t, err, "Incorrectly accepted invalid CT extension")
+	assert.Error(t, err, "incorrectly accepted invalid CT extension")
+}
+
+func TestCertCheckerInvalidChainAccepted(t *testing.T) {
+	// This shouldn't validate as it's missing the intermediate cert
+	chainPem := []string{testonly.LeafSignedByFakeIntermediateCertPem}
+	jsonChain := pemsToJsonChain(t, chainPem)
+	trustedRoots := NewPEMCertPool()
+
+	assert.True(t, trustedRoots.AppendCertsFromPEM([]byte(testonly.FakeCACertPem)), "failed to load fake root")
+
+	_, err := ValidateChain(jsonChain, *trustedRoots)
+
+	assert.Error(t, err, "verification accepted an invalid chain (missing intermediate)")
+}
+
+func TestCertCheckerInvalidChainRejectedOrdering(t *testing.T) {
+	// This chain shouldn't validate because the order of presentation is wrong
+	chainPem := []string{testonly.FakeIntermediateCertPem, testonly.LeafSignedByFakeIntermediateCertPem}
+	jsonChain := pemsToJsonChain(t, chainPem)
+	trustedRoots := NewPEMCertPool()
+
+	assert.True(t, trustedRoots.AppendCertsFromPEM([]byte(testonly.FakeCACertPem)), "failed to load fake root")
+
+	_, err := ValidateChain(jsonChain, *trustedRoots)
+
+	assert.Error(t, err, "verification accepted an invalid chain (ordering)")
+}
+
+func TestCertCheckerInvalidChainRejectedBadChain(t *testing.T) {
+	// This chain shouldn't validate because the chain contains unrelated certs
+	chainPem := []string{testonly.FakeIntermediateCertPem, testonly.TestCertPEM}
+	jsonChain := pemsToJsonChain(t, chainPem)
+	trustedRoots := NewPEMCertPool()
+
+	assert.True(t, trustedRoots.AppendCertsFromPEM([]byte(testonly.FakeCACertPem)), "failed to load fake root")
+
+	_, err := ValidateChain(jsonChain, *trustedRoots)
+
+	assert.Error(t, err, "verification accepted an invalid chain (unrelated)")
+}
+
+func TestCertCheckerInvalidChainRejectedBadChainUnrelatedAppended(t *testing.T) {
+	// This chain shouldn't validate because the otherwise valid chain contains an unrelated cert
+	// at the end
+	chainPem := []string{testonly.LeafSignedByFakeIntermediateCertPem, testonly.FakeIntermediateCertPem, testonly.TestCertPEM}
+	jsonChain := pemsToJsonChain(t, chainPem)
+	trustedRoots := NewPEMCertPool()
+
+	assert.True(t, trustedRoots.AppendCertsFromPEM([]byte(testonly.FakeCACertPem)), "failed to load fake root")
+
+	_, err := ValidateChain(jsonChain, *trustedRoots)
+
+	assert.Error(t, err, "verification accepted an invalid chain (unrelated at end)")
+}
+
+func TestCertCheckerValidChainAccepted(t *testing.T) {
+	// This chain should validate up to the fake root CA
+	chainPem := []string{testonly.LeafSignedByFakeIntermediateCertPem, testonly.FakeIntermediateCertPem}
+	jsonChain := pemsToJsonChain(t, chainPem)
+	trustedRoots := NewPEMCertPool()
+
+	assert.True(t, trustedRoots.AppendCertsFromPEM([]byte(testonly.FakeCACertPem)), "failed to load fake root")
+
+	validPath, err := ValidateChain(jsonChain, *trustedRoots)
+
+	assert.NoError(t, err, "unexpected error verifying valid chain")
+	assert.Equal(t, 2, len(validPath), "expected valid path of length 2")
+}
+
+// Builds a chain of base64 encoded certs as if they'd been submitted to a handler.
+// Note: ordering is important
+func pemsToJsonChain(t *testing.T, pemCerts []string) []string {
+	chain := []string{}
+
+	for _, pem := range pemCerts {
+		cert := pemToCert(t, pem)
+		chain = append(chain, base64.StdEncoding.EncodeToString(cert.Raw))
+	}
+
+	return chain
 }
 
 func pemToCert(t *testing.T, pemData string) *x509.Certificate {
