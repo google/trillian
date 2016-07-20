63cf4a006b5995e31b891d850041e0408b59cb6b
Al Cutter <al@google.com>
2016-07-20 17:32:48 +0100

    start on tree stratification

--- storage/mysql/storage_test.go
+++ storage/mysql/storage_test.go
@@ -19,7 +19,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var allTables = []string{"Unsequenced", "TreeHead", "SequencedLeafData", "LeafData", "Node", "TreeControl", "Trees", "MapLeaf", "MapHead"}
+var allTables = []string{"Unsequenced", "TreeHead", "SequencedLeafData", "LeafData", "Subtree", "TreeControl", "Trees", "MapLeaf", "MapHead"}
 
 // Must be 32 bytes to match sha256 length if it was a real hash
 var dummyHash = []byte("hashxxxxhashxxxxhashxxxxhashxxxx")
