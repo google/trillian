2482e511e960b35900ac7f5464d67e8d06d6ddec
Martin Smith <mhs@google.com>
2016-07-25 12:07:35 +0100

    Add CT repo URL

--- examples/ct/doc.go
+++ examples/ct/doc.go
@@ -5,5 +5,7 @@ log server using a Trillian log server as backend storage via its GRPC API.
 IMPORTANT: Only code rooted within this part of the tree should refer to the CT
 Github repository. Other parts of the system must not assume that the data they're
 processing is X.509 or CT related.
+
+The CT repository can be found at: https://github.com/google/certificate-transparency
 */
 package ct
