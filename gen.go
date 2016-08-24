9fcd636f8c1e94a3eba47bee339728ef810d2373
Al Cutter <al@google.com>
2016-08-24 17:44:33 +0100

    Enable auto-generation of protos too.

--- gen.go
+++ gen.go
@@ -1,4 +1,5 @@
 package trillian
 
-// Have to put this here, since I can't add it to the generated PB go code.
+//go:generate protoc --go_out=plugins=grpc:. github.com/google/trillian/*.proto
+
 //go:generate mockgen -self_package github.com/google/trillian -package trillian -destination mock_log_client.go github.com/google/trillian TrillianLogClient,TrillianLogServer,TrillianMapClient,TrillianMapServer
--- storage/gen.go
+++ storage/gen.go
@@ -1,3 +1,5 @@
 package storage
 
+//go:generate protoc --go_out=plugins=grpc:. github.com/google/trillian/storage/*.proto
+
 //go:generate mockgen -self_package github.com/google/trillian/storage -package storage -destination mock_storage.go -imports=trillian=github.com/google/trillian github.com/google/trillian/storage LogTX,MapTX,ReadOnlyLogTX,ReadOnlyMapTX,MapStorage,LogStorage
