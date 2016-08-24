eb7f10cd0bf669ff7396f6909a10a7e21a59c4d0
Martin Smith <mhs@google.com>
2016-08-24 15:03:34 +0100

    Rebase, move consistency path calculations to merkle package

--- merkle/merkle_path.go
+++ merkle/merkle_path.go
@@ -2,6 +2,7 @@ package merkle
 
 import (
 	"fmt"
+	
 	"github.com/google/trillian/storage"
 )
 
@@ -44,6 +45,83 @@ func CalcInclusionProofNodeAddresses(treeSize, index int64, maxBitLen int) ([]st
 	}
 
 	return proof, nil
+}
+
+// CalcConsistencyProofNodeAddresses returns the tree node IDs needed to
+// build a consistency proof between two specified tree sizes. The maxBitLen parameter
+// is copied into all the returned nodeIDs. The caller is responsible for checking that
+// the input tree sizes correspond to valid tree heads. All returned NodeIDs are tree
+// coordinates within the new tree. It is assumed that they will be fetched from storage
+// at a revision corresponding to the STH associated with the treeSize parameter.
+func CalcConsistencyProofNodeAddresses(previousTreeSize, treeSize int64, maxBitLen int) ([]storage.NodeID, error) {
+	if previousTreeSize > treeSize || previousTreeSize < 1 || treeSize < 1 || maxBitLen <= 0 {
+		return []storage.NodeID{}, fmt.Errorf("invalid params prior: %d treesize: %d, bitlen:%d", previousTreeSize, treeSize, maxBitLen)
+	}
+
+	return snapshotConsistency(previousTreeSize, treeSize, maxBitLen), nil
+}
+
+// snapshotConsistency does the calculation of consistency proof node addresses between
+// two snapshots. Based on the C++ code used by CT but adjusted to fit our situation.
+// In particular the code does not need to handle the case where overwritten node hashes
+// must be recursively computed because we have versioned nodes.
+func snapshotConsistency(snapshot1, snapshot2 int64, maxBitLen int) []storage.NodeID {
+	var proof []storage.NodeID
+
+	level := 0
+	node := snapshot1 - 1
+
+	// Compute the (compressed) path to the root of snapshot2.
+	// Everything left of 'node' is equal in both trees; no need to record.
+	for (node & 1) != 0 {
+		node /= 2
+		level++
+	}
+
+	if node != 0 {
+		// Not at the root of snapshot 1, record the node
+		proof = append(proof, storage.NewNodeIDForTreeCoords(int64(level), node, maxBitLen))
+	}
+
+	// Now append the path from this node to the root of snapshot2.
+	return append(proof, pathFromNodeToRootAtSnapshot(node, level, snapshot2, maxBitLen)...)
+}
+
+func pathFromNodeToRootAtSnapshot(node int64, level int, snapshot int64, maxBitLen int) []storage.NodeID {
+	var proof []storage.NodeID
+
+	if snapshot == 0 {
+		return proof
+	}
+
+	// Index of the last node.
+	lastNode := (snapshot - 1) >> uint(level)
+
+	// Move up, recording the sibling of the current node at each level.
+	for lastNode != 0 {
+		sibling := node ^ 1
+		if sibling < lastNode {
+			// The sibling is not the last node of the level in the snapshot tree
+			proof = append(proof, storage.NewNodeIDForTreeCoords(int64(level), sibling, maxBitLen))
+		} else if sibling == lastNode {
+			// The sibling is the last node of the level in the snapshot tree.
+			// In the C++ code we'd potentially recompute the node value here because we could be
+			// referencing a snapshot at a point before additional leaves were added to the tree causing
+			// some nodes to be overwritten. We have versioned tree nodes so this isn't necessary,
+			// we won't see any hashes written since the snapshot point. However we do have to account
+			// for missing levels in the tree.
+			drop := level - subtreeDepth(snapshot, level - 1)
+			sibling = sibling << uint(drop)
+			proof = append(proof, storage.NewNodeIDForTreeCoords(int64(level - drop), sibling, maxBitLen))
+		}
+
+		// Sibling > lastNode so does not exist, move up
+		node /= 2
+		lastNode /= 2
+		level++
+	}
+
+	return proof
 }
 
 // subtreeDepth calculates the depth of a subtree, used at the right of the tree which
--- merkle/merkle_path_test.go
+++ merkle/merkle_path_test.go
@@ -2,6 +2,7 @@ package merkle
 
 import (
 	"testing"
+
 	"github.com/google/trillian/storage"
 )
 
@@ -16,12 +17,24 @@ type calcPathTestData struct {
 	expectedPath []storage.NodeID
 }
 
+type consistencyProofTestData struct {
+	priorTreeSize int64
+	treeSize      int64
+	expectedProof []storage.NodeID
+}
+
 // Expected paths built by examination of the example 7 leaf tree in RFC 6962. When comparing
 // with the document remember that our storage node layers are always populated from the bottom up.
 var expectedPathSize7Index0 = []storage.NodeID{storage.NewNodeIDForTreeCoords(0, 1, 64), storage.NewNodeIDForTreeCoords(1, 1, 64), storage.NewNodeIDForTreeCoords(2, 1, 64)}
 var expectedPathSize7Index3 = []storage.NodeID{storage.NewNodeIDForTreeCoords(0, 2, 64), storage.NewNodeIDForTreeCoords(1, 0, 64), storage.NewNodeIDForTreeCoords(2, 1, 64)}
 var expectedPathSize7Index4 = []storage.NodeID{storage.NewNodeIDForTreeCoords(0, 5, 64), storage.NewNodeIDForTreeCoords(0, 6, 64), storage.NewNodeIDForTreeCoords(2, 0, 64)}
 var expectedPathSize7Index6 = []storage.NodeID{storage.NewNodeIDForTreeCoords(1, 2, 64), storage.NewNodeIDForTreeCoords(2, 0, 64)}
+
+// Expected consistency proofs built from the examples in RFC 6962. Again, in our implementation
+// node layers are filled from the bottom upwards.
+var expectedConsistencyProofFromSize6To7 = []storage.NodeID{storage.NewNodeIDForTreeCoords(1, 2, 64), storage.NewNodeIDForTreeCoords(0, 6, 64), storage.NewNodeIDForTreeCoords(2, 0, 64)}
+var expectedConsistencyProofFromSize3To7 = []storage.NodeID{storage.NewNodeIDForTreeCoords(0, 2, 64), storage.NewNodeIDForTreeCoords(0, 3, 64), storage.NewNodeIDForTreeCoords(1, 0, 64), storage.NewNodeIDForTreeCoords(2, 1, 64)}
+var expectedConsistencyProofFromSize4To7 = []storage.NodeID{storage.NewNodeIDForTreeCoords(2, 1, 64)}
 
 var bitLenTests = []bitLenTestData{{0, 0}, {1, 1}, {2, 2}, {3, 2}, {12, 4}}
 
@@ -42,6 +55,20 @@ var pathTestBad = []calcPathTestData{
 	{7, -1, []storage.NodeID{}},
 	{7, 8, []storage.NodeID{}},
 }
+
+// These should compute the expected consistency proofs
+var consistencyTests = []consistencyProofTestData{
+	{6, 7, expectedConsistencyProofFromSize6To7},
+	{3, 7, expectedConsistencyProofFromSize3To7},
+	{4, 7, expectedConsistencyProofFromSize4To7}}
+
+// These should all fail to provide proofs
+var consistencyTestsBad = []consistencyProofTestData{
+	{0, -1, []storage.NodeID{}},
+	{-10, 0, []storage.NodeID{}},
+	{-1, -1, []storage.NodeID{}},
+	{0, 0, []storage.NodeID{}},
+	{9, 8, []storage.NodeID{}}}
 
 func TestBitLen(t *testing.T) {
 	for _, testCase := range bitLenTests {
@@ -78,6 +105,37 @@ func TestCalcInclusionProofNodeAddressesRejectsBadBitLen(t *testing.T) {
 
 	if err == nil {
 		t.Fatal("incorrectly accepted -ve maxBitLen")
+	}
+}
+
+func TestCalcConsistencyProofNodeAddresses(t *testing.T) {
+	for _, testCase := range consistencyTests {
+		proof, err := CalcConsistencyProofNodeAddresses(testCase.priorTreeSize, testCase.treeSize, 64)
+
+		if err != nil {
+			t.Fatalf("failed to calculate inclusion proof from %d to %d: %v", testCase.priorTreeSize, testCase.treeSize, err)
+		}
+
+		comparePaths(t, proof, testCase.expectedProof)
+	}
+}
+
+func TestCalcConsistencyProofNodeAddressesBadInputs(t *testing.T) {
+	for _, testCase := range consistencyTestsBad {
+		_, err := CalcConsistencyProofNodeAddresses(testCase.priorTreeSize, testCase.treeSize, 64)
+
+		if err == nil {
+			t.Fatalf("consistency path calculation accepted bad input: %v", testCase)
+		}
+	}
+}
+
+func TestCalcConsistencyProofNodeAddressesRejectsBadBitLen(t *testing.T) {
+	_, err := CalcConsistencyProofNodeAddresses(6, 7, -1)
+	_, err2 := CalcConsistencyProofNodeAddresses(6, 7, 0)
+
+	if err == nil || err2 == nil {
+		t.Fatalf("consistency path calculation accepted bad bitlen: %v %v", err, err2)
 	}
 }
 
