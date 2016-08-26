9729c0661cad006646bd7378fcdc57fd8b7d496b
Al Cutter <al-github@9600.org>
2016-08-26 12:21:44 +0100

    fix proto gen (#117)

--- gen.go
+++ gen.go
@@ -1,5 +1,5 @@
 package trillian
 
-//go:generate protoc --go_out=plugins=grpc:. github.com/google/trillian/*.proto
+//go:generate sh -c "cd $GOPATH/src && protoc --go_out=plugins=grpc:. github.com/google/trillian/*proto"
 
 //go:generate mockgen -self_package github.com/google/trillian -package trillian -destination mock_log_client.go github.com/google/trillian TrillianLogClient,TrillianLogServer,TrillianMapClient,TrillianMapServer
