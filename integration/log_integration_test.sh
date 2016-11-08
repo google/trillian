f8efbf95f2488af641c9bf3b8ca4ab6693497f7b
David Drysdale <drysdale@google.com>
2016-11-08 15:35:14 +0000

    Emit server logs if integration tests fail

--- integration/log_integration_test.sh
+++ integration/log_integration_test.sh
@@ -21,13 +21,26 @@ echo "Starting Log server on port ${PORT}"
 pushd ${TRILLIAN_ROOT} > /dev/null
 go build ${GOFLAGS} ./server/trillian_log_server/
 ./trillian_log_server --private_key_password=towel --private_key_file=${TESTDATA}/trillian-server-key.pem --port ${PORT} --signer_interval="1s" --sequencer_sleep_between_runs="1s" --batch_size=100 &
-trap "kill -INT %1" EXIT
+SERVER_PID=$!
+trap "kill -INT ${SERVER_PID}" EXIT
 popd > /dev/null
 sleep 2
 
 
 # Run the test(s):
 cd ${INTEGRATION_DIR}
+set +e
 go test -tags=integration -run ".*Log.*" --timeout=5m ./ --treeid ${TEST_TREE_ID} --log_server="localhost:${PORT}"
+RESULT=$?
+set -e
 
 echo "Stopping Log server on port ${PORT}"
+trap - EXIT
+kill -INT ${SERVER_PID}
+
+if [ $RESULT != 0 ]; then
+    sleep 1
+    echo "Server log:"
+    echo "--------------------"
+    cat /tmp/trillian_log_server.INFO
+fi
