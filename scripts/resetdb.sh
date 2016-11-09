90fd55479856f63824f547343d1fe0068a27ed65
David Drysdale <drysdale@google.com>
2016-11-09 09:49:39 +0000

    scripts: add resetdb.sh for convenience

---
+++ scripts/resetdb.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+echo "Completely wipe and reset database 'test'."
+read -p "Are you sure? " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    mysql -u root -e 'DROP DATABASE IF EXISTS test;'
+    mysql -u root -e 'CREATE DATABASE test;'
+    mysql -u root -e "GRANT ALL ON test.* TO 'test'@'localhost' IDENTIFIED BY 'zaphod';"
+    mysql -u root -D test < storage/mysql/storage.sql
+fi
+echo
