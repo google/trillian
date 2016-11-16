170a93040b3da6f1238153ea9767103ef4312818
Al Cutter <al@google.com>
2016-11-16 14:18:42 +0000

    Add missing package doc for integration.

---
+++ integration/doc.go
@@ -0,0 +1,4 @@
+// Package integration contains some integration tests which are intended to
+// serve as a way of checking that various top-level binaries work as intended,
+// as well as providing a simple example of how to run and use the various servers.
+package integration
