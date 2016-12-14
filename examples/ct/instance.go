222ff84f69c662dfb3dd9add50da6f83ed0aa8db
David Drysdale <drysdale@google.com>
2016-12-14 14:35:10 +0000

    examples/ct: get log config from a JSON file
    Prepare for multitenant by encapsulating per-log config stuff.

---
+++ examples/ct/instance.go
@@ -0,0 +1,89 @@
+package ct
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"time"
+
+	"github.com/google/trillian"
+	"github.com/google/trillian/crypto"
+	"github.com/google/trillian/util"
+)
+
+// LogConfig describes the configuration options for a log instance.
+type LogConfig struct {
+	LogID           int64
+	RootsPEMFile    string
+	PubKeyPEMFile   string
+	PrivKeyPEMFile  string
+	PrivKeyPassword string
+}
+
+// LogConfigFromFile creates a slice of LogConfig options from the given
+// filename, which should contain JSON encoded configuration data.
+func LogConfigFromFile(filename string) ([]LogConfig, error) {
+	if len(filename) == 0 {
+		return nil, fmt.Errorf("log config filename empty")
+	}
+	cfgData, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read log config: %v", err)
+	}
+	var cfg []LogConfig
+	if err := json.Unmarshal(cfgData, &cfg); err != nil {
+		return nil, fmt.Errorf("failed to parse config data: %v", err)
+	}
+	if len(cfg) == 0 {
+		return nil, errors.New("empty log config found")
+	}
+	return cfg, nil
+}
+
+// SetUpInstance sets up a log instance that uses the specified client to communicate
+// with the Trillian RPC back end.
+func (cfg LogConfig) SetUpInstance(client trillian.TrillianLogClient, deadline time.Duration) error {
+	// Check config validity.
+	if len(cfg.RootsPEMFile) == 0 {
+		return errors.New("need to specify RootsPEMFile")
+	}
+	if len(cfg.PubKeyPEMFile) == 0 {
+		return errors.New("need to specify PubKeyPEMFile")
+	}
+	if len(cfg.PrivKeyPEMFile) == 0 {
+		return errors.New("need to specify PrivKeyPEMFile")
+	}
+
+	// Load the trusted roots
+	roots := NewPEMCertPool()
+	if err := roots.AppendCertsFromPEMFile(cfg.RootsPEMFile); err != nil {
+		return fmt.Errorf("failed to read trusted roots: %v", err)
+	}
+
+	// Set up a key manager instance for this log.
+	km := crypto.NewPEMKeyManager()
+	privData, err := ioutil.ReadFile(cfg.PrivKeyPEMFile)
+	if err != nil {
+		return fmt.Errorf("failed to load private key file: %v", err)
+	}
+
+	if err := km.LoadPrivateKey(string(privData), cfg.PrivKeyPassword); err != nil {
+		return fmt.Errorf("failed to parse private key: %v", err)
+	}
+
+	pubData, err := ioutil.ReadFile(cfg.PubKeyPEMFile)
+	if err != nil {
+		return fmt.Errorf("failed to load public key file: %v", err)
+	}
+
+	if err := km.LoadPublicKey(string(pubData)); err != nil {
+		return fmt.Errorf("failed to parse public key: %v", err)
+	}
+
+	// Create and register the handlers using the RPC client we just set up
+	ctx := NewLogContext(cfg.LogID, roots, client, km, deadline, new(util.SystemTimeSource))
+	ctx.RegisterHandlers()
+
+	return nil
+}
