f557326f9d895d53201287a4afb37df4ce64236f
David Drysdale <drysdale@google.com>
2016-12-20 17:06:30 +0000

    gen.go: explicitly list top-level .proto files
    This forces a consistent order of processing, which is required
    because protoc-gen-go puts extra comments in the first file
    processed and we need this to always be the same file.

--- gen.go
+++ gen.go
@@ -1,3 +1,3 @@
 package trillian
 
-//go:generate sh -c "cd $GOPATH/src && protoc --go_out=plugins=grpc:. github.com/google/trillian/*proto"
+//go:generate sh -c "cd $GOPATH/src && protoc --go_out=plugins=grpc:. github.com/google/trillian/trillian_api.proto github.com/google/trillian/trillian.proto"
