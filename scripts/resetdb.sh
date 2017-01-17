2ff0cefe5c9b65e2fd8de0a58ab2832b76788b4d
Gary Belvin <gdb@google.com>
2017-01-17 16:21:43 +0000

    Override comment

--- scripts/resetdb.sh
+++ scripts/resetdb.sh
@@ -3,6 +3,7 @@ echo "Completely wipe and reset database 'test'."
 read -p "Are you sure? " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+    # A command line supplied -u will override the first argument.
     mysql -u root "$@" -e 'DROP DATABASE IF EXISTS test;'
     mysql -u root "$@" -e 'CREATE DATABASE test;'
     mysql -u root "$@" -e "GRANT ALL ON test.* TO 'test'@'localhost' IDENTIFIED BY 'zaphod';"
