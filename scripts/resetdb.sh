c9d6d1f602702457988dde1dbb6414f0930e4892
Gary Belvin <gdb@google.com>
2017-01-17 11:26:06 +0000

    Pass args to reset script
    Allow custom username / password in db setup script
    by passing command line arguments to mysql

--- scripts/resetdb.sh
+++ scripts/resetdb.sh
@@ -3,9 +3,9 @@ echo "Completely wipe and reset database 'test'."
 read -p "Are you sure? " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    mysql -u root -e 'DROP DATABASE IF EXISTS test;'
-    mysql -u root -e 'CREATE DATABASE test;'
-    mysql -u root -e "GRANT ALL ON test.* TO 'test'@'localhost' IDENTIFIED BY 'zaphod';"
-    mysql -u root -D test < storage/mysql/storage.sql
+    mysql -u root "$@" -e 'DROP DATABASE IF EXISTS test;'
+    mysql -u root "$@" -e 'CREATE DATABASE test;'
+    mysql -u root "$@" -e "GRANT ALL ON test.* TO 'test'@'localhost' IDENTIFIED BY 'zaphod';"
+    mysql -u root "$@" -D test < storage/mysql/storage.sql
 fi
 echo
