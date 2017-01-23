6ce4743aa1d07f2d0ed60119e9e96160220d2e87
Gary Belvin <gdb@google.com>
2017-01-23 11:53:29 +0000

    Add license header

--- integration/doc.go
+++ integration/doc.go
@@ -1,3 +1,17 @@
+// Copyright 2017 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 // Package integration contains some integration tests which are intended to
 // serve as a way of checking that various top-level binaries work as intended,
 // as well as providing a simple example of how to run and use the various servers.
