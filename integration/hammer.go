11ddf46e7bf3f15b4039bef4ddd9c0fba0d4d732
David Drysdale <drysdale@google.com>
2017-02-01 11:53:41 +0000

    integration/ct: move test operations to methods on test state

--- integration/hammer.go
+++ integration/hammer.go
@@ -32,6 +32,13 @@ const sctCount = 10
 // Maximum number of entries to request.
 const maxEntriesCount = uint64(10)
 
+// errSkip indicates that a test operation should be skipped.
+type errSkip struct{}
+
+func (e errSkip) Error() string {
+	return "test operation skipped"
+}
+
 // HammerConfig provides configuration for a stress/load test.
 type HammerConfig struct {
 	// Configuration for the log.
@@ -141,13 +148,28 @@ func (pc *pendingCerts) popIfMMDPassed(now time.Time) *submittedCert {
 	return submitted
 }
 
-// HammerCTLog performs load/stress operations according to given config.
-func HammerCTLog(cfg HammerConfig) error {
+// hammerState tracks the operations that have been performed during a test run, including
+// earlier SCTs/STHs for later checking.
+type hammerState struct {
+	cfg   *HammerConfig
+	pool  ClientPool
+	stats *wantStats
+	// STHs are arranged from later to earlier (so [0] is the most recent), and the
+	// discovery of new STHs will push older ones off the end.
+	sth [sthCount]*ct.SignedTreeHead
+	// Submitted certs also run from later to earlier, but the discovery of new SCTs
+	// does not affect the existing contents of the array, so if the array is full it
+	// keeps the same elements.  Instead, the oldest entry is removed (and a space
+	// created) when we are able to get an inclusion proof for it.
+	pending pendingCerts
+}
+
+func newHammerState(cfg *HammerConfig) (*hammerState, error) {
 	opts := jsonclient.Options{}
 	if cfg.LogCfg.PubKeyPEMFile != "" {
 		pubkey, err := ioutil.ReadFile(cfg.LogCfg.PubKeyPEMFile)
 		if err != nil {
-			return fmt.Errorf("failed to get public key contents: %v", err)
+			return nil, fmt.Errorf("failed to get public key contents: %v", err)
 		}
 		opts.PublicKey = string(pubkey)
 	}
@@ -155,207 +177,250 @@ func HammerCTLog(cfg HammerConfig) error {
 	for _, s := range strings.Split(cfg.Servers, ",") {
 		c, err := client.New("http://"+s+"/"+cfg.LogCfg.Prefix, nil, opts)
 		if err != nil {
-			return fmt.Errorf("failed to create LogClient instance: %v", err)
+			return nil, fmt.Errorf("failed to create LogClient instance: %v", err)
 		}
 		pool = append(pool, c)
 	}
+	state := hammerState{
+		cfg:   cfg,
+		pool:  pool,
+		stats: newWantStats(cfg.LogCfg.LogID),
+	}
+	return &state, nil
+}
+
+func (s *hammerState) addChain(ctx context.Context) error {
+	chain, err := makeCertChain(s.cfg.LeafChain, s.cfg.LeafCert, s.cfg.CACert, s.cfg.Signer)
+	if err != nil {
+		return fmt.Errorf("failed to make fresh cert: %v", err)
+	}
+	sct, err := s.pool.Pick().AddChain(ctx, chain)
+	if err != nil {
+		return fmt.Errorf("failed to add-chain: %v", err)
+	}
+	glog.V(2).Infof("%s: Uploaded cert, got SCT(time=%q)", s.cfg.LogCfg.Prefix, timeFromMS(sct.Timestamp))
+	if s.pending.canAppend(time.Now(), s.cfg.MMD) {
+		// Calculate leaf hash =  SHA256(0x00 | tls-encode(MerkleTreeLeaf))
+		submitted := submittedCert{precert: false, sct: sct}
+		leaf := ct.MerkleTreeLeaf{
+			Version:  ct.V1,
+			LeafType: ct.TimestampedEntryLeafType,
+			TimestampedEntry: &ct.TimestampedEntry{
+				Timestamp:  sct.Timestamp,
+				EntryType:  ct.X509LogEntryType,
+				X509Entry:  &(chain[0]),
+				Extensions: sct.Extensions,
+			},
+		}
+		submitted.integrateBy = timeFromMS(sct.Timestamp).Add(s.cfg.MMD)
+		submitted.leafData, err = tls.Marshal(leaf)
+		if err != nil {
+			return fmt.Errorf("failed to tls.Marshal leaf cert: %v", err)
+		}
+		submitted.leafHash = sha256.Sum256(append([]byte{merkletree.LeafPrefix}, submitted.leafData...))
+		s.pending.appendCert(&submitted)
+		glog.V(3).Infof("%s: Uploaded cert has leaf-hash %x", s.cfg.LogCfg.Prefix, submitted.leafHash)
+	}
+	return nil
+}
+
+func (s *hammerState) addPreChain(ctx context.Context) error {
+	prechain, tbs, err := makePrecertChain(s.cfg.LeafChain, s.cfg.LeafCert, s.cfg.CACert, s.cfg.Signer)
+	if err != nil {
+		return fmt.Errorf("failed to make fresh pre-cert: %v", err)
+	}
+	sct, err := s.pool.Pick().AddPreChain(ctx, prechain)
+	if err != nil {
+		return fmt.Errorf("failed to add-pre-chain: %v", err)
+	}
+	glog.V(2).Infof("%s: Uploaded pre-cert, got SCT(time=%q)", s.cfg.LogCfg.Prefix, timeFromMS(sct.Timestamp))
+	if s.pending.canAppend(time.Now(), s.cfg.MMD) {
+		// Calculate leaf hash =  SHA256(0x00 | tls-encode(MerkleTreeLeaf))
+		submitted := submittedCert{precert: true, sct: sct}
+		leaf := ct.MerkleTreeLeaf{
+			Version:  ct.V1,
+			LeafType: ct.TimestampedEntryLeafType,
+			TimestampedEntry: &ct.TimestampedEntry{
+				Timestamp: sct.Timestamp,
+				EntryType: ct.PrecertLogEntryType,
+				PrecertEntry: &ct.PreCert{
+					IssuerKeyHash:  sha256.Sum256(s.cfg.CACert.RawSubjectPublicKeyInfo),
+					TBSCertificate: tbs,
+				},
+				Extensions: sct.Extensions,
+			},
+		}
+		submitted.integrateBy = timeFromMS(sct.Timestamp).Add(s.cfg.MMD)
+		submitted.leafData, err = tls.Marshal(leaf)
+		if err != nil {
+			return fmt.Errorf("tls.Marshal(precertLeaf)=(nil,%v); want (_,nil)", err)
+		}
+		submitted.leafHash = sha256.Sum256(append([]byte{merkletree.LeafPrefix}, submitted.leafData...))
+		s.pending.appendCert(&submitted)
+		glog.V(3).Infof("%s: Uploaded pre-cert has leaf-hash %x", s.cfg.LogCfg.Prefix, submitted.leafHash)
+	}
+	return nil
+}
+
+func (s *hammerState) getSTH(ctx context.Context) error {
+	// Shuffle earlier STHs along.
+	for i := sthCount - 1; i > 0; i-- {
+		s.sth[i] = s.sth[i-1]
+	}
+	var err error
+	s.sth[0], err = s.pool.Pick().GetSTH(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get-sth: %v", err)
+	}
+	glog.V(2).Infof("%s: Got STH(time=%q, size=%d)", s.cfg.LogCfg.Prefix, timeFromMS(s.sth[0].Timestamp), s.sth[0].TreeSize)
+	return nil
+}
+
+func (s *hammerState) getSTHConsistency(ctx context.Context) error {
+	// Get current size, and pick an earlier size
+	sthNow, err := s.pool.Pick().GetSTH(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get-sth for current tree: %v", err)
+	}
+	which := rand.Intn(sthCount)
+	if s.sth[which] == nil || s.sth[which].TreeSize == 0 {
+		glog.V(3).Infof("%s: skipping get-sth-consistency as no earlier STH", s.cfg.LogCfg.Prefix)
+		return errSkip{}
+	}
+	if s.sth[which].TreeSize == sthNow.TreeSize {
+		glog.V(3).Infof("%s: skipping get-sth-consistency as same size (%d)", s.cfg.LogCfg.Prefix, sthNow.TreeSize)
+		return errSkip{}
+	}
+
+	proof, err := s.pool.Pick().GetSTHConsistency(ctx, s.sth[which].TreeSize, sthNow.TreeSize)
+	if err != nil {
+		return fmt.Errorf("failed to get-sth-consistency(%d, %d): %v", s.sth[which].TreeSize, sthNow.TreeSize, err)
+	}
+	if err := checkCTConsistencyProof(s.sth[which], sthNow, proof); err != nil {
+		return fmt.Errorf("get-sth-consistency(%d, %d) proof check failed: %v", s.sth[which].TreeSize, sthNow.TreeSize, err)
+	}
+	glog.V(2).Infof("%s: Got STH consistency proof (size=%d => %d) len %d",
+		s.cfg.LogCfg.Prefix, s.sth[which].TreeSize, sthNow.TreeSize, len(proof))
+	return nil
+}
+
+func (s *hammerState) getProofByHash(ctx context.Context) error {
+	submitted := s.pending.popIfMMDPassed(time.Now())
+	if submitted == nil {
+		// No SCT that is guaranteed to be integrated, so move on.
+		return errSkip{}
+	}
+	// Get an STH that should include this submitted [pre-]cert.
+	sth, err := s.pool.Pick().GetSTH(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get-sth for proof: %v", err)
+	}
+	// Get and check an inclusion proof.
+	rsp, err := s.pool.Pick().GetProofByHash(ctx, submitted.leafHash[:], sth.TreeSize)
+	if err != nil {
+		return fmt.Errorf("failed to get-proof-by-hash(size=%d): %v", sth.TreeSize, err)
+	}
+	if err := Verifier.VerifyInclusionProof(rsp.LeafIndex, int64(sth.TreeSize), rsp.AuditPath, sth.SHA256RootHash[:], submitted.leafData); err != nil {
+		return fmt.Errorf("failed to VerifyInclusionProof(%d, %d)=%v", rsp.LeafIndex, sth.TreeSize, err)
+	}
+	return nil
+}
+
+func (s *hammerState) getEntries(ctx context.Context) error {
+	if s.sth[0] == nil || s.sth[0].TreeSize == 0 {
+		glog.V(3).Infof("%s: skipping get-entries as no earlier STH", s.cfg.LogCfg.Prefix)
+		return errSkip{}
+	}
+	count := uint64(1 + rand.Intn(int(maxEntriesCount-1)))
+	if count > s.sth[0].TreeSize {
+		count = s.sth[0].TreeSize
+	}
+	// Entry indices are zero-based.
+	first := int64(s.sth[0].TreeSize - count)
+	last := int64(s.sth[0].TreeSize) - 1
+	entries, err := s.pool.Pick().GetEntries(ctx, first, last)
+	if err != nil {
+		return fmt.Errorf("failed to get-entries(%d,%d): %v", first, last, err)
+	}
+	if len(entries) < int(count) {
+		return fmt.Errorf("get-entries(%d,%d) returned %d entries; want %d", first, last, len(entries), count)
+	}
+	for i, entry := range entries {
+		leaf := entry.Leaf
+		ts := leaf.TimestampedEntry
+		if leaf.Version != 0 {
+			return fmt.Errorf("leaf[%d].Version=%v; want V1(0)", i, leaf.Version)
+		}
+		if leaf.LeafType != ct.TimestampedEntryLeafType {
+			return fmt.Errorf("leaf[%d].Version=%v; want TimestampedEntryLeafType", i, leaf.LeafType)
+		}
+		if ts.EntryType != ct.X509LogEntryType && ts.EntryType != ct.PrecertLogEntryType {
+			return fmt.Errorf("leaf[%d].ts.EntryType=%v; want {X509,Precert}LogEntryType", i, ts.EntryType)
+		}
+	}
+	glog.V(2).Infof("%s: Got entries [%d:%d+1]\n", s.cfg.LogCfg.Prefix, first, last)
+	return nil
+}
+
+func (s *hammerState) getRoots(ctx context.Context) error {
+	roots, err := s.pool.Pick().GetAcceptedRoots(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get-roots: %v", err)
+	}
+	glog.V(2).Infof("%s: Got roots (len=%d)", s.cfg.LogCfg.Prefix, len(roots))
+	return nil
+}
+
+// HammerCTLog performs load/stress operations according to given config.
+func HammerCTLog(cfg HammerConfig) error {
+	s, err := newHammerState(&cfg)
+	if err != nil {
+		return err
+	}
 	ctx := context.Background()
-
-	stats := newWantStats(cfg.LogCfg.LogID)
-	// STHs are arranged from later to earlier (so [0] is the most recent), and the
-	// discovery of new STHs will push older ones off the end.
-	sth := [sthCount]*ct.SignedTreeHead{}
-	// Submitted certs also run from later to earlier, but the discovery of new SCTs
-	// does not affect the existing contents of the array, so if the array is full it
-	// keeps the same elements.  Instead, the oldest entry is removed (and a space
-	// created) when we are able to get an inclusion proof for it.
-	var pending pendingCerts
-
 	for count := uint64(1); count < cfg.Operations; count++ {
 		ep := cfg.EPBias.Choose()
 		glog.V(3).Infof("perform %s operation", ep)
 		status := http.StatusOK
+		var err error
 		switch ep {
 		case ctfe.AddChainName:
-			chain, err := makeCertChain(cfg.LeafChain, cfg.LeafCert, cfg.CACert, cfg.Signer)
-			if err != nil {
-				return fmt.Errorf("failed to make fresh cert: %v", err)
-			}
-			sct, err := pool.Pick().AddChain(ctx, chain)
-			if err != nil {
-				return fmt.Errorf("failed to add-chain: %v", err)
-			}
-			glog.V(2).Infof("%s: Uploaded cert, got SCT(time=%q)", cfg.LogCfg.Prefix, timeFromMS(sct.Timestamp))
-			if pending.canAppend(time.Now(), cfg.MMD) {
-				// Calculate leaf hash =  SHA256(0x00 | tls-encode(MerkleTreeLeaf))
-				submitted := submittedCert{precert: false, sct: sct}
-				leaf := ct.MerkleTreeLeaf{
-					Version:  ct.V1,
-					LeafType: ct.TimestampedEntryLeafType,
-					TimestampedEntry: &ct.TimestampedEntry{
-						Timestamp:  sct.Timestamp,
-						EntryType:  ct.X509LogEntryType,
-						X509Entry:  &(chain[0]),
-						Extensions: sct.Extensions,
-					},
-				}
-				submitted.integrateBy = timeFromMS(sct.Timestamp).Add(cfg.MMD)
-				submitted.leafData, err = tls.Marshal(leaf)
-				if err != nil {
-					return fmt.Errorf("failed to tls.Marshal leaf cert: %v", err)
-				}
-				submitted.leafHash = sha256.Sum256(append([]byte{merkletree.LeafPrefix}, submitted.leafData...))
-				pending.appendCert(&submitted)
-				glog.V(3).Infof("%s: Uploaded cert has leaf-hash %x", cfg.LogCfg.Prefix, submitted.leafHash)
-			}
+			err = s.addChain(ctx)
 		case ctfe.AddPreChainName:
-			prechain, tbs, err := makePrecertChain(cfg.LeafChain, cfg.LeafCert, cfg.CACert, cfg.Signer)
-			if err != nil {
-				return fmt.Errorf("failed to make fresh pre-cert: %v", err)
-			}
-			sct, err := pool.Pick().AddPreChain(ctx, prechain)
-			if err != nil {
-				return fmt.Errorf("failed to add-pre-chain: %v", err)
-			}
-			glog.V(2).Infof("%s: Uploaded pre-cert, got SCT(time=%q)", cfg.LogCfg.Prefix, timeFromMS(sct.Timestamp))
-			if pending.canAppend(time.Now(), cfg.MMD) {
-				// Calculate leaf hash =  SHA256(0x00 | tls-encode(MerkleTreeLeaf))
-				submitted := submittedCert{precert: true, sct: sct}
-				leaf := ct.MerkleTreeLeaf{
-					Version:  ct.V1,
-					LeafType: ct.TimestampedEntryLeafType,
-					TimestampedEntry: &ct.TimestampedEntry{
-						Timestamp: sct.Timestamp,
-						EntryType: ct.PrecertLogEntryType,
-						PrecertEntry: &ct.PreCert{
-							IssuerKeyHash:  sha256.Sum256(cfg.CACert.RawSubjectPublicKeyInfo),
-							TBSCertificate: tbs,
-						},
-						Extensions: sct.Extensions,
-					},
-				}
-				submitted.integrateBy = timeFromMS(sct.Timestamp).Add(cfg.MMD)
-				submitted.leafData, err = tls.Marshal(leaf)
-				if err != nil {
-					return fmt.Errorf("tls.Marshal(precertLeaf)=(nil,%v); want (_,nil)", err)
-				}
-				submitted.leafHash = sha256.Sum256(append([]byte{merkletree.LeafPrefix}, submitted.leafData...))
-				pending.appendCert(&submitted)
-				glog.V(3).Infof("%s: Uploaded pre-cert has leaf-hash %x", cfg.LogCfg.Prefix, submitted.leafHash)
-			}
+			err = s.addPreChain(ctx)
 		case ctfe.GetSTHName:
-			// Shuffle earlier STHs along.
-			for i := sthCount - 1; i > 0; i-- {
-				sth[i] = sth[i-1]
-			}
-			var err error
-			sth[0], err = pool.Pick().GetSTH(ctx)
-			if err != nil {
-				return fmt.Errorf("failed to get-sth: %v", err)
-			}
-			glog.V(2).Infof("%s: Got STH(time=%q, size=%d)", cfg.LogCfg.Prefix, timeFromMS(sth[0].Timestamp), sth[0].TreeSize)
+			err = s.getSTH(ctx)
 		case ctfe.GetSTHConsistencyName:
-			// Get current size, and pick an earlier size
-			sthNow, err := pool.Pick().GetSTH(ctx)
-			if err != nil {
-				return fmt.Errorf("failed to get-sth for current tree: %v", err)
-			}
-			which := rand.Intn(sthCount)
-			if sth[which] == nil || sth[which].TreeSize == 0 {
-				glog.V(3).Infof("%s: skipping get-sth-consistency as no earlier STH", cfg.LogCfg.Prefix)
-				break
-			}
-			if sth[which].TreeSize == sthNow.TreeSize {
-				glog.V(3).Infof("%s: skipping get-sth-consistency as same size (%d)", cfg.LogCfg.Prefix, sthNow.TreeSize)
-				break
-			}
-
-			proof, err := pool.Pick().GetSTHConsistency(ctx, sth[which].TreeSize, sthNow.TreeSize)
-			if err != nil {
-				return fmt.Errorf("failed to get-sth-consistency(%d, %d): %v", sth[which].TreeSize, sthNow.TreeSize, err)
-			}
-			if err := checkCTConsistencyProof(sth[which], sthNow, proof); err != nil {
-				return fmt.Errorf("get-sth-consistency(%d, %d) proof check failed: %v", sth[which].TreeSize, sthNow.TreeSize, err)
-			}
-			glog.V(2).Infof("%s: Got STH consistency proof (size=%d => %d) len %d",
-				cfg.LogCfg.Prefix, sth[which].TreeSize, sthNow.TreeSize, len(proof))
+			err = s.getSTHConsistency(ctx)
 		case ctfe.GetProofByHashName:
-			submitted := pending.popIfMMDPassed(time.Now())
-			if submitted == nil {
-				// No SCT that is guaranteed to be integrated, so move on.
-				status = http.StatusFailedDependency
-				break
-			}
-			// Get an STH that should include this submitted [pre-]cert.
-			sth, err := pool.Pick().GetSTH(ctx)
-			if err != nil {
-				return fmt.Errorf("failed to get-sth for proof: %v", err)
-			}
-			// Get and check an inclusion proof.
-			rsp, err := pool.Pick().GetProofByHash(ctx, submitted.leafHash[:], sth.TreeSize)
-			if err != nil {
-				return fmt.Errorf("failed to get-proof-by-hash(size=%d): %v", sth.TreeSize, err)
-			}
-			if err := Verifier.VerifyInclusionProof(rsp.LeafIndex, int64(sth.TreeSize), rsp.AuditPath, sth.SHA256RootHash[:], submitted.leafData); err != nil {
-				return fmt.Errorf("failed to VerifyInclusionProof(%d, %d)=%v", rsp.LeafIndex, sth.TreeSize, err)
-			}
+			err = s.getProofByHash(ctx)
 		case ctfe.GetEntriesName:
-			if sth[0] == nil || sth[0].TreeSize == 0 {
-				glog.V(3).Infof("%s: skipping get-entries as no earlier STH", cfg.LogCfg.Prefix)
-				break
-			}
-			count := uint64(1 + rand.Intn(int(maxEntriesCount-1)))
-			if count > sth[0].TreeSize {
-				count = sth[0].TreeSize
-			}
-			// Entry indices are zero-based.
-			first := int64(sth[0].TreeSize - count)
-			last := int64(sth[0].TreeSize) - 1
-			entries, err := pool.Pick().GetEntries(ctx, first, last)
-			if err != nil {
-				return fmt.Errorf("failed to get-entries(%d,%d): %v", first, last, err)
-			}
-			if len(entries) < int(count) {
-				return fmt.Errorf("get-entries(%d,%d) returned %d entries; want %d", first, last, len(entries), count)
-			}
-			for i, entry := range entries {
-				leaf := entry.Leaf
-				ts := leaf.TimestampedEntry
-				if leaf.Version != 0 {
-					return fmt.Errorf("leaf[%d].Version=%v; want V1(0)", i, leaf.Version)
-				}
-				if leaf.LeafType != ct.TimestampedEntryLeafType {
-					return fmt.Errorf("leaf[%d].Version=%v; want TimestampedEntryLeafType", i, leaf.LeafType)
-				}
-				if ts.EntryType != ct.X509LogEntryType && ts.EntryType != ct.PrecertLogEntryType {
-					return fmt.Errorf("leaf[%d].ts.EntryType=%v; want {X509,Precert}LogEntryType", i, ts.EntryType)
-				}
-			}
-			glog.V(2).Infof("%s: Got entries [%d:%d+1]\n", cfg.LogCfg.Prefix, first, last)
+			err = s.getEntries(ctx)
 		case ctfe.GetRootsName:
-			roots, err := pool.Pick().GetAcceptedRoots(ctx)
-			if err != nil {
-				return fmt.Errorf("failed to get-roots: %v", err)
-			}
-			glog.V(2).Infof("%s: Got roots (len=%d)", cfg.LogCfg.Prefix, len(roots))
+			err = s.getRoots(ctx)
 		case ctfe.GetEntryAndProofName:
 			status = http.StatusNotImplemented
 		default:
 			return fmt.Errorf("internal error: unknown entrypoint %s selected", ep)
 		}
+		if _, ok := err.(errSkip); ok {
+			status = http.StatusFailedDependency
+		} else if err != nil {
+			return err
+		}
 		if status == http.StatusNotImplemented {
 			glog.V(2).Infof("%s: hammering entrypoint %s not yet implemented", cfg.LogCfg.Prefix, ep)
 		}
-		stats.done(ep, status)
+		s.stats.done(ep, status)
 
 		if count%emitInterval == 0 {
 			fmt.Printf("%10s:", cfg.LogCfg.Prefix)
-			fmt.Printf(" last-sth.size=%d", sth[0].TreeSize)
+			fmt.Printf(" last-sth.size=%d", s.sth[0].TreeSize)
 			fmt.Printf(" operations: total=%d", count)
 			statusOK := strconv.Itoa(http.StatusOK)
 			for _, ep := range ctfe.Entrypoints {
 				if cfg.EPBias.Bias[ep] > 0 {
-					fmt.Printf(" %s=%d/%d", ep, stats.HTTPRsps[ep][statusOK], stats.HTTPReq[ep])
+					fmt.Printf(" %s=%d/%d", ep, s.stats.HTTPRsps[ep][statusOK], s.stats.HTTPReq[ep])
 				}
 			}
 			fmt.Printf("\n")
