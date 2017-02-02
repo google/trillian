7dc89f8ac0c522e03434abf4166d82482f0f9ffc
Alan Parra <alanparra@google.com>
2017-02-02 15:43:02 +0000

    MySQL AdminStorage implementation
    Adds a new MySQL AdminStorage implementation, plus a generic AdminStorage
    tester, which may be used to test any AdminStorage implementation (not just the
    MySQL one).
    
    A few schema changes have been made. A follow up PR will wire the new storage in
    the current code and further add constraints to the schema, plus remove some
    redundant columns. Notably, this PR enables strict mode on MySQL, without which
    invalid enums generate warnings instead of errors (and get truncated to an
    empty string).
    
    A few general utilities have been added as well (tree ID generation, tree
    validation), which may be used by other AdminStorage implementations.

---
+++ storage/tree_validation.go
@@ -0,0 +1,51 @@
+// Copyright 2017 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package storage
+
+import (
+	"fmt"
+	"github.com/google/trillian"
+)
+
+const (
+	maxDisplayNameLength = 20
+	maxDescriptionLength = 200
+)
+
+// ValidateTreeForCreation returns nil if tree is valid for insertion, error
+// otherwise.
+// See the documentation on trillian.Tree for reference on which values are
+// valid.
+func ValidateTreeForCreation(tree *trillian.Tree) error {
+	switch {
+	case tree.TreeState != trillian.TreeState_ACTIVE:
+		return fmt.Errorf("invalid tree_state: %s", tree.TreeState)
+	case tree.TreeType == trillian.TreeType_UNKNOWN_TREE_TYPE:
+		return fmt.Errorf("invalid tree_type: %s", tree.TreeType)
+	case tree.HashStrategy == trillian.HashStrategy_UNKNOWN_HASH_STRATEGY:
+		return fmt.Errorf("invalid hash_strategy: %s", tree.HashStrategy)
+	case tree.HashAlgorithm == trillian.HashAlgorithm_NONE:
+		return fmt.Errorf("invalid hash_algorithm: %s", tree.HashStrategy)
+	case tree.SignatureAlgorithm == trillian.SignatureAlgorithm_ANONYMOUS:
+		return fmt.Errorf("invalid signature_algorithm: %s", tree.HashStrategy)
+	case tree.DuplicatePolicy == trillian.DuplicatePolicy_UNKNOWN_DUPLICATE_POLICY:
+		return fmt.Errorf("invalid duplicate_policy: %s", tree.HashStrategy)
+	case len(tree.DisplayName) > maxDisplayNameLength:
+		return fmt.Errorf("display_name too big, max length is %v: %v", maxDisplayNameLength, tree.HashStrategy)
+	case len(tree.Description) > maxDescriptionLength:
+		return fmt.Errorf("display_name too big, max length is %v: %v", maxDisplayNameLength, tree.HashStrategy)
+	}
+	return nil
+}
---
+++ storage/tree_validation_test.go
@@ -0,0 +1,100 @@
+// Copyright 2017 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package storage
+
+import (
+	"github.com/google/trillian"
+	"testing"
+)
+
+func TestValidateTreeForCreation(t *testing.T) {
+	valid1 := newTree()
+
+	valid2 := newTree()
+	valid2.TreeType = trillian.TreeType_MAP
+	valid2.DuplicatePolicy = trillian.DuplicatePolicy_DUPLICATES_ALLOWED
+
+	invalidState1 := newTree()
+	invalidState1.TreeState = trillian.TreeState_UNKNOWN_TREE_STATE
+	invalidState2 := newTree()
+	invalidState2.TreeState = trillian.TreeState_FROZEN
+	invalidState3 := newTree()
+	invalidState3.TreeState = trillian.TreeState_SOFT_DELETED
+	invalidState4 := newTree()
+	invalidState4.TreeState = trillian.TreeState_HARD_DELETED
+
+	invalidType := newTree()
+	invalidType.TreeType = trillian.TreeType_UNKNOWN_TREE_TYPE
+
+	invalidHashStrategy := newTree()
+	invalidHashStrategy.HashStrategy = trillian.HashStrategy_UNKNOWN_HASH_STRATEGY
+
+	invalidHashAlgorithm := newTree()
+	invalidHashAlgorithm.HashAlgorithm = trillian.HashAlgorithm_NONE
+
+	invalidSignatureAlgorithm := newTree()
+	invalidSignatureAlgorithm.SignatureAlgorithm = trillian.SignatureAlgorithm_ANONYMOUS
+
+	invalidDuplicatePolicy := newTree()
+	invalidDuplicatePolicy.DuplicatePolicy = trillian.DuplicatePolicy_UNKNOWN_DUPLICATE_POLICY
+
+	invalidDisplayName := newTree()
+	invalidDisplayName.DisplayName = "A Very Long Display Name That Clearly Won't Fit But At Least Mentions Llamas Somewhere"
+
+	invalidDescription := newTree()
+	invalidDescription.Description = `
+		A Very Long Description That Clearly Won't Fit, Also Mentions Llamas, For Some Reason Has Only Capitalized Words And Keeps Repeating Itself.
+		A Very Long Description That Clearly Won't Fit, Also Mentions Llamas, For Some Reason Has Only Capitalized Words And Keeps Repeating Itself.
+		`
+
+	tests := []struct {
+		tree    *trillian.Tree
+		wantErr bool
+	}{
+		{tree: valid1},
+		{tree: valid2},
+		{tree: invalidState1, wantErr: true},
+		{tree: invalidState2, wantErr: true},
+		{tree: invalidState3, wantErr: true},
+		{tree: invalidState4, wantErr: true},
+		{tree: invalidType, wantErr: true},
+		{tree: invalidHashStrategy, wantErr: true},
+		{tree: invalidHashAlgorithm, wantErr: true},
+		{tree: invalidSignatureAlgorithm, wantErr: true},
+		{tree: invalidDuplicatePolicy, wantErr: true},
+		{tree: invalidDisplayName, wantErr: true},
+		{tree: invalidDescription, wantErr: true},
+	}
+	for i, test := range tests {
+		err := ValidateTreeForCreation(test.tree)
+		if hasErr := err != nil; hasErr != test.wantErr {
+			t.Errorf("%v: ValidateTreeForCreation() = %v, wantErr = %v", i, err, test.wantErr)
+		}
+	}
+}
+
+// newTree returns a valid tree for tests.
+func newTree() *trillian.Tree {
+	return &trillian.Tree{
+		TreeState:          trillian.TreeState_ACTIVE,
+		TreeType:           trillian.TreeType_LOG,
+		HashStrategy:       trillian.HashStrategy_RFC_6962,
+		HashAlgorithm:      trillian.HashAlgorithm_SHA256,
+		SignatureAlgorithm: trillian.SignatureAlgorithm_ECDSA,
+		DuplicatePolicy:    trillian.DuplicatePolicy_DUPLICATES_NOT_ALLOWED,
+		DisplayName:        "Llamas Log",
+		Description:        "Registry of publicly-owned llamas",
+	}
+}
