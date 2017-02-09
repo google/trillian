7b6922abd8ca929be53a8581d4e5b3949eea5720
Alan Parra <alanparra@google.com>
2017-02-09 18:24:26 +0000

    Fix integration scripts

--- scripts/createmap.sh
+++ scripts/createmap.sh
@@ -5,4 +5,4 @@ fi
 TESTDBOPTS="-u test --password=zaphod -D test"
 TREE_ID=$1
 # Create a new Map storage row for the given tree ID.
-mysql ${TESTDBOPTS} -e "INSERT INTO Trees VALUES (${TREE_ID}, 1, 'MAP', 'SHA256', 'SHA256', false)"
+mysql ${TESTDBOPTS} -e "INSERT INTO Trees(TreeId, KeyId, TreeType, LeafHasherType, TreeHasherType, AllowsDuplicateLeaves) VALUES(${TREE_ID}, 1, 'MAP', 'SHA256', 'SHA256', false)"
