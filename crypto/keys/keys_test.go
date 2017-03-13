c567dd19c71f9acceb88bf8be65cc63fb8bacce3
Rob Percival <robpercival@google.com>
2017-03-13 15:54:45 +0000

    Refactor keys_test.go into tabular tests
    Improves coverage of TestSignatureAlgorithm.

--- crypto/keys/keys_test.go
+++ crypto/keys/keys_test.go
@@ -17,58 +17,128 @@ package keys
 import (
 	"crypto"
 	"crypto/ecdsa"
+	"crypto/rand"
+	"crypto/rsa"
 	"encoding/asn1"
+	"fmt"
 	"math/big"
-	"math/rand"
 	"testing"
 
 	"github.com/google/trillian/crypto/sigpb"
 	"github.com/google/trillian/testonly"
 )
 
-type ecdsaSig struct {
-	R, S *big.Int
+const (
+	ecdsaPublicKey = `
+-----BEGIN PUBLIC KEY-----
+MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEvuynpVdR+5xSNaVBb//1fqO6Nb/nC+WvRQ4bALzy4G+QbByvO1Qpm2eUzTdDUnsLN5hp3pIXYAmtjvjY1fFZEg==
+-----END PUBLIC KEY-----`
+	rsaPublicKey = `
+-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsMB4reLZhs+2ReYX01nZpqLBQ9uhcZvBmzH54RsZDTb5khw+luSXKbLKXxdbQfrsxURbeVdugDNnV897VI43znuiKJ19Y/XS3N5Z7Q97/GOxOxGFObP0DovCAPblxAMaQBb+U9jkVt/4bHcNIOTZl/lXgX+yp58lH5uPfDwav/hVNg7QkAW3BxQZ5wiLTTZUILoTMjax4R24pULlg/Wt/rT4bDj8rxUgYR60MuO93jdBtNGwmzdCYyk4cEmrPEgCueRC6jFafUzlLjvuX89ES9n98LxX+gBANA7RpVPkJd0kfWFHO1JRUEJr++WjU3x4la2Xs4tUNX4QBSJP4XEOXwIDAQAB
+-----END PUBLIC KEY-----`
+	dsaPublicKey = `
+-----BEGIN PUBLIC KEY-----
+MIIDRjCCAjkGByqGSM44BAEwggIsAoIBAQDgLI6pXvqpcOY33lzeZrjUBHxphiz0I9VKF9vGpWymNfBptQ75bpQFe16jBjaOGwDImASHTp53XskQJLOXC4bZxoRUHsm8bHQVZHQhYgxn8ZDQX/40zOR1d73y1TXSiULo6rDKVlM+fFcm33tGv+ZOdfaIhW17c5jvDAy6UWqQakasvL+kfiejIDGHjLVFWwX0vLCG+pAomgO6snQHGcPhDO9uxEYPd9on7YTgBrpa2IcXk5jFeY8xOxMnMwoBojRvH97+ivdBR1yW8f+4FAGg5o1eFV5ZqoUAF8GO3BBEwluMGNeT7gMgl4PO8N8xBxJulHd3tLW5qkW0cBPwkbzzAiEAvdYeMPamsFAyd7s07dt78wxXyHGrwVl2AcQBo0QTATkCggEASH9Rp+EjNkL7uCqGJ78P4tjJM+2+xaEhZpJ/kTzq6DtdFhu5Rov6lN5NnZKPSUNYr9Vkmu88ru0iND1N37z0rJpImksXKxCv0AwBkwtqCwf9jjkTrZiGRzP8xf789wK+uG7Uud20ml9QzXKr9Af9WrRx3DtCq44PBaIlhPvpZS9znCZsuUZqYZFW3/oD4EhwPgVLSWeulh1t33ku3mYQwVS8ZTdJGPyFRoD1dcQ4EchR4ce0u0nTXlqErWhfnmb9msF6dFCV0Mx5yrqxkEHbJ/vZgB4zAdOke7XiJsWqIok/7IJpJuVOvkY9NHgBdlq3xU180+pEo2NrGm4pbrGm1wOCAQUAAoIBAAGbucHEfgtcu++OQQjYqneukv4zqcP/PCJTP+GuXen6SH25V2ZlHC88lG6qdZVBPWZidAb9BSoUQpW7BzauKRqH7rKOsIeqvEPCiWBKA781Zi5HAWGhC4INJJx54Q66F54DkGlTRVFkXlGpAIudhfAIG//MyO9TIsLSgRyqjKWVm+/XhWDIT5iMJZZ/IgmbICueaa7go8poHuTTyUDPHPIeL5d9Aru7qD4JtX+UVy6GYKhWx/guv+A7zyJ8d1kMLsmUAro80DLPDoais2I8YPpbu+xTSLLswIYddDdwg3P8mMAGzuWY/ZLumwpRr/fbI+t2Sm9KKGNGkGGIKAg43cs=
+-----END PUBLIC KEY-----`
+)
+
+func verifyECDSA(key *ecdsa.PublicKey, digest []byte, signature []byte) error {
+	// s is the decoded signature.
+	var s struct {
+		R, S *big.Int
+	}
+
+	_, err := asn1.Unmarshal(signature, &s)
+	if err != nil {
+		return fmt.Errorf("failed to unmarshal signature as ASN.1: %v", err)
+	}
+
+	if !ecdsa.Verify(key, digest, s.R, s.S) {
+		return fmt.Errorf("signature failed verification")
+	}
+
+	return nil
 }
 
-func TestLoadECDSAKeyAndSign(t *testing.T) {
-	// Obviously in real code we wouldn't use a fixed seed
-	randSource := rand.New(rand.NewSource(42))
+func TestLoadPrivateKeyAndSign(t *testing.T) {
 	hasher := crypto.SHA256
+	digest := []byte("hello")
 
-	k, err := NewFromPrivatePEM(testonly.DemoPrivateKey, testonly.DemoPrivateKeyPass)
-	if err != nil {
-		t.Fatalf("Failed to load key: %v", err)
+	tests := []struct {
+		name    string
+		keyPEM  string
+		keyPass string
+	}{
+		{
+			name:    "ECDSA",
+			keyPEM:  testonly.DemoPrivateKey,
+			keyPass: testonly.DemoPrivateKeyPass,
+		},
 	}
 
-	signed, err := k.Sign(randSource, []byte("hello"), hasher)
+	for _, test := range tests {
+		k, err := NewFromPrivatePEM(test.keyPEM, test.keyPass)
+		if err != nil {
+			t.Errorf("%s: failed to load key: %v", test.name, err)
+			continue
+		}
 
-	if err != nil {
-		t.Fatalf("Failed to sign: %v", err)
-	}
+		signature, err := k.Sign(rand.Reader, digest, hasher)
+		if err != nil {
+			t.Errorf("%s: failed to sign: %v", test.name, err)
+			continue
+		}
 
-	// Do a round trip by verifying the signature using the public key
-	var signature ecdsaSig
-	_, err = asn1.Unmarshal(signed, &signature)
+		// Do a round trip by verifying the signature using the public key.
+		switch publicKey := k.Public().(type) {
+		case *ecdsa.PublicKey:
+			if err := verifyECDSA(publicKey, digest, signature); err != nil {
+				t.Errorf("%s: %v", test.name, err)
+			}
+		case *rsa.PublicKey:
+			if err := rsa.VerifyPKCS1v15(publicKey, hasher, digest, signature); err != nil {
+				t.Errorf("%s: %v", test.name, err)
+			}
+		default:
+			t.Errorf("Unsupported public key type: %T", publicKey)
+		}
 
-	if err != nil {
-		t.Fatal("Failed to unmarshal signature as asn.1")
-	}
-
-	publicKey := k.Public().(*ecdsa.PublicKey)
-
-	if !ecdsa.Verify(publicKey, []byte("hello"), signature.R, signature.S) {
-		t.Fatal("Signature did not verify on round trip test")
 	}
 }
 
 func TestSignatureAlgorithm(t *testing.T) {
-	privateKey, err := NewFromPrivatePEM(testonly.DemoPrivateKey, testonly.DemoPrivateKeyPass)
-	if err != nil {
-		t.Fatalf("Failed to load key: %v", err)
+	tests := []struct {
+		name   string
+		keyPEM string
+		want   sigpb.DigitallySigned_SignatureAlgorithm
+	}{
+		{
+			name:   "ECDSA",
+			keyPEM: ecdsaPublicKey,
+			want:   sigpb.DigitallySigned_ECDSA,
+		},
+		{
+			name:   "RSA",
+			keyPEM: rsaPublicKey,
+			want:   sigpb.DigitallySigned_RSA,
+		},
+		{
+			name:   "DSA",
+			keyPEM: dsaPublicKey,
+			want:   sigpb.DigitallySigned_ANONYMOUS,
+		},
 	}
 
-	publicKey := privateKey.Public()
-	if got, want := SignatureAlgorithm(publicKey), sigpb.DigitallySigned_ECDSA; got != want {
-		t.Errorf("SignatureAlgorithm(%v) = %v, want %v", publicKey, got, want)
+	for _, test := range tests {
+		key, err := NewFromPublicPEM(test.keyPEM)
+		if err != nil {
+			t.Errorf("%s: Failed to load key: %v", test.name, err)
+			continue
+		}
+
+		if got := SignatureAlgorithm(key); got != test.want {
+			t.Errorf("%s: SignatureAlgorithm(%v) = %v, want %v", test.name, key, got, test.want)
+		}
 	}
 }
