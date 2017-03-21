02560f8beba6d23047fc16e9d5042c3803c73db1
Martin <Martin2112@users.noreply.github.com>
2017-03-21 10:03:08 +0000

    Add a custom leaf data prefix to the log integration test (#453)
    * Add a custom leaf prefix option to integration test
    
    * Fix bug reading back when using custom leaf prefix

--- integration/log.go
+++ integration/log.go
@@ -44,6 +44,7 @@ type TestParameters struct {
 	sequencingWaitTotal time.Duration
 	sequencingPollWait  time.Duration
 	rpcRequestDeadline  time.Duration
+	customLeafPrefix    string
 }
 
 // DefaultTestParameters builds a TestParameters object for a normal
@@ -62,6 +63,7 @@ func DefaultTestParameters(treeID int64) TestParameters {
 		sequencingWaitTotal: 10 * time.Second * 60,
 		sequencingPollWait:  time.Second * 5,
 		rpcRequestDeadline:  time.Second * 10,
+		customLeafPrefix:    "",
 	}
 }
 
@@ -186,14 +188,14 @@ func queueLeaves(client trillian.TrillianLogClient, params TestParameters) error
 		// Leaf data based on the sequence number so we can check the hashes
 		leafNumber := params.startLeaf + l
 
-		data := []byte(fmt.Sprintf("Leaf %d", leafNumber))
+		data := []byte(fmt.Sprintf("%sLeaf %d", params.customLeafPrefix, leafNumber))
 		idHash := sha256.Sum256(data)
 
 		leaf := &trillian.LogLeaf{
 			LeafIdentityHash: idHash[:],
 			MerkleLeafHash:   testonly.Hasher.HashLeaf(data),
 			LeafValue:        data,
-			ExtraData:        []byte(fmt.Sprintf("Extra %d", leafNumber)),
+			ExtraData:        []byte(fmt.Sprintf("%sExtra %d", params.customLeafPrefix, leafNumber)),
 		}
 		leaves = append(leaves, leaf)
 
@@ -255,7 +257,7 @@ func readbackLogEntries(logID int64, client trillian.TrillianLogClient, params T
 	leafDataPresenceMap := make(map[string]bool)
 
 	for l := int64(0); l < params.leafCount; l++ {
-		leafDataPresenceMap[fmt.Sprintf("Leaf %d", l+params.startLeaf)] = true
+		leafDataPresenceMap[fmt.Sprintf("%sLeaf %d", params.customLeafPrefix, l+params.startLeaf)] = true
 	}
 
 	for currentLeaf < params.leafCount {
