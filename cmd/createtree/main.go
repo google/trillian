41f4800b23ab8b446da8d5d4cc6050fab8e70872
Roland Shoemaker <roland@letsencrypt.org>
2017-04-14 14:02:55 -0700

    Don't require a PEM password to create a tree

--- cmd/createtree/main.go
+++ cmd/createtree/main.go
@@ -146,13 +146,9 @@ func newPK(opts *createOpts) (*any.Any, error) {
 		if _, err := os.Stat(path); err != nil {
 			return nil, fmt.Errorf("error reading PEM key file at %v: %v", path, err)
 		}
-		pass := opts.pemKeyPass
-		if pass == "" {
-			return nil, errors.New("empty PEM key password")
-		}
 		pemKey := &trillian.PEMKeyFile{
 			Path:     path,
-			Password: pass,
+			Password: opts.pemKeyPass,
 		}
 		return ptypes.MarshalAny(pemKey)
 	default:
