4a4eb07e65a5d1d47cd0a3f292b67ed0baf8e6d5
David Drysdale <drysdale@google.com>
2017-04-21 17:39:08 +0100

    util: Add package comments

--- util/etcd/election.go
+++ util/etcd/election.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package etcd holds an etcd-specific implementation of the
+// util.MasterElection interface.
 package etcd
 
 import (
