ac807369bb77bc9eb4a96b26ad03daa028db79a3
Alan Parra <codingllama@users.noreply.github.com>
2017-04-26 10:27:36 +0100

    Document user-visible characteristic of TrillianErrors (#540)
    Issue #508

--- errors/doc.go
+++ errors/doc.go
@@ -14,7 +14,15 @@
 
 // Package errors defines an error representation that associates an error
 // message to an error code.
+//
 // It's meant to allow translation to other kinds of errors (e.g., gRPC errors)
 // without information loss, while at the same time keeping the implementation
 // independent of specific libraries.
+//
+// Errors created by this package are meant to be user-visible, therefore care
+// must be taken to ensure that both messages and error codes are chosen
+// according to the perspective of the RPC caller. For example, a function that
+// reads a file may be tempted to return an error with a NotFound code, but from
+// the perspective of the RPC caller, FailedPrecondition would be clearer (as it
+// indicates a bad argument on the request).
 package errors
