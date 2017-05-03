a062667671ed0dcebaabe6b5826a4e4059e93209
David Drysdale <drysdale@google.com>
2017-05-03 15:50:15 +0100

    integration: split CT parts from base log parts
    Use the base log scripts for log_integration_test.sh.
    Also clean up some variable use and naming infelicities,
    and some indentation.

--- integration/ct_stop_test.sh
+++ integration/ct_stop_test.sh
@@ -1,28 +1,14 @@
 #!/bin/bash
 # Close down a set of running processes for a CT test. Assumes the following
-# variables are set:
-#  - RPC_SERVER_PIDS : bash array of RPC server pids
-#  - LB_SERVER_PID   : RPC load balancer pid
+# variables are set, in addition to those needed by log_stop_test.sh:
 #  - CT_SERVER_PIDS  : bash array of CT HTTP server pids
 set -e
 INTEGRATION_DIR="$( cd "$( dirname "$0" )" && pwd )"
 . "${INTEGRATION_DIR}"/common.sh
 
-for pid in "${HTTP_SERVER_PIDS[@]}"; do
-    echo "Stopping CT HTTP server (pid ${pid})"
-    killPid ${pid}
+for pid in "${CT_SERVER_PIDS[@]}"; do
+  echo "Stopping CT HTTP server (pid ${pid})"
+  killPid ${pid}
 done
-for pid in "${LOG_SIGNER_PIDS[@]}"; do
-    echo "Stopping Log signer (pid ${pid})"
-    killPid ${pid}
-done
-echo "Stopping Log RPC load balancer (pid ${LB_SERVER_PID})"
-killPid ${LB_SERVER_PID}
-for pid in "${RPC_SERVER_PIDS[@]}"; do
-    echo "Stopping Log RPC server (pid ${pid})"
-    killPid ${pid}
-done
-if [[ "${ETCD_PID}" != "" ]]; then
-    echo "Stopping local etcd server (pid ${ETCD_PID})"
-    killPid ${ETCD_PID}
-fi
+
+. "${INTEGRATION_DIR}"/log_stop_test.sh
--- integration/log_integration_test.sh
+++ integration/log_integration_test.sh
@@ -3,63 +3,46 @@ set -e
 INTEGRATION_DIR="$( cd "$( dirname "$0" )" && pwd )"
 . "${INTEGRATION_DIR}"/common.sh
 
-echo "Building code"
+echo "Launching core Trillian log components"
+. "${INTEGRATION_DIR}"/log_prep_test.sh 1 1
+
+# Cleanup for the Trillian components
+TO_DELETE="${TO_DELETE} ${ETCD_DB_DIR}"
+TO_KILL+=(${LOG_SIGNER_PIDS[@]})
+TO_KILL+=(${LB_SERVER_PID})
+TO_KILL+=(${RPC_SERVER_PIDS[@]})
+TO_KILL+=(${ETCD_PID})
+
+echo "Provision log"
 go build ${GOFLAGS} ./cmd/createtree/
-go build ${GOFLAGS} ./server/trillian_log_server/
-go build ${GOFLAGS} ./server/trillian_log_signer/
-
-yes | "${SCRIPTS_DIR}"/resetdb.sh
-
-RPC_PORT=$(pickUnusedPort)
-
-echo "Starting Log RPC server on localhost:${RPC_PORT}"
-pushd "${TRILLIAN_ROOT}" > /dev/null
-./trillian_log_server --rpc_endpoint="localhost:${RPC_PORT}" --http_endpoint='' &
-RPC_SERVER_PID=$!
-popd > /dev/null
-waitForServerStartup ${RPC_PORT}
-
 TEST_TREE_ID=$(./createtree \
-  --admin_server="localhost:${RPC_PORT}" \
+  --admin_server="${RPC_SERVERS}" \
   --pem_key_path=testdata/log-rpc-server.privkey.pem \
   --pem_key_password=towel \
   --signature_algorithm=ECDSA)
 echo "Created tree ${TEST_TREE_ID}"
 
-# Ensure we kill the RPC server once we're done.
-TO_KILL+=(${RPC_SERVER_PID})
-waitForServerStartup ${RPC_PORT}
-
-echo "Starting Log signer"
-pushd "${TRILLIAN_ROOT}" > /dev/null
-./trillian_log_signer --sequencer_interval="1s" --batch_size=100 --http_endpoint='' --force_master &
-LOG_SIGNER_PID=$!
-TO_KILL+=(${LOG_SIGNER_PID})
-popd > /dev/null
-
-# Run the test(s):
-cd "${INTEGRATION_DIR}"
+echo "Running test"
+pushd "${INTEGRATION_DIR}"
 set +e
-go test -run ".*LiveLog.*" --timeout=5m ./ --treeid ${TEST_TREE_ID} --log_rpc_server="localhost:${RPC_PORT}"
+go test -run ".*LiveLog.*" --timeout=5m ./ --treeid ${TEST_TREE_ID} --log_rpc_server="${RPC_SERVERS}"
 RESULT=$?
 set -e
+popd
 
-echo "Stopping Log signer (pid ${LOG_SIGNER_PID})"
-killPid ${LOG_SIGNER_PID}
-echo "Stopping Log RPC server (pid ${RPC_SERVER_PID})"
-killPid ${RPC_SERVER_PID}
+. "${INTEGRATION_DIR}"/log_stop_test.sh
 TO_KILL=()
 
 if [ $RESULT != 0 ]; then
-    sleep 1
-    if [ "$TMPDIR" == "" ]; then
-        TMPDIR=/tmp
-    fi
-    echo "Server log:"
-    echo "--------------------"
-    cat "${TMPDIR}"/trillian_log_server.INFO
-    echo "Signer log:"
-    echo "--------------------"
-    cat "${TMPDIR}"/trillian_log_signer.INFO
-    exit $RESULT
+  sleep 1
+  if [ "$TMPDIR" == "" ]; then
+    TMPDIR=/tmp
+  fi
+  echo "Server log:"
+  echo "--------------------"
+  cat "${TMPDIR}"/trillian_log_server.INFO
+  echo "Signer log:"
+  echo "--------------------"
+  cat "${TMPDIR}"/trillian_log_signer.INFO
+  exit $RESULT
 fi
