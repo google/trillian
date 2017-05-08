e7d6c01d57f39ea3c3783822a954610038465b0c
David Drysdale <drysdale@google.com>
2017-05-08 12:48:14 +0100

    examples/ct: round-robin across multiple backends
    Use a grpc.RoundRobin balancer with a FixedBackendResolver that
    just returns the set of configured backends.

--- examples/ct/ct_server/main.go
+++ examples/ct/ct_server/main.go
@@ -33,7 +33,7 @@ import (
 // Global flags that affect all log instances.
 var serverHostFlag = flag.String("host", "localhost", "Address to serve CT log requests on")
 var serverPortFlag = flag.Int("port", 6962, "Port to serve CT log requests on")
-var rpcBackendFlag = flag.String("log_rpc_server", "localhost:8090", "Backend Log RPC server to use")
+var rpcBackendFlag = flag.String("log_rpc_server", "localhost:8090", "Backend Log RPC servers to use, comma separated")
 var rpcDeadlineFlag = flag.Duration("rpc_deadline", time.Second*10, "Deadline for backend RPC requests")
 var logConfigFlag = flag.String("log_config", "", "File holding log config in JSON")
 var maxGetEntriesFlag = flag.Int64("maxGetEntriesAllowed", 0, "Max number of entries we allow in a get-entries request (default 50)")
@@ -57,7 +57,9 @@ func main() {
 	// TODO(Martin2112): Support TLS and other stuff for RPC client and http server, this is just to
 	// get started. Uses a blocking connection so we don't start serving before we're connected
 	// to backend.
-	conn, err := grpc.Dial(*rpcBackendFlag, grpc.WithInsecure(), grpc.WithBlock())
+	var res util.FixedBackendResolver
+	bal := grpc.RoundRobin(res)
+	conn, err := grpc.Dial(*rpcBackendFlag, grpc.WithInsecure(), grpc.WithBlock(), grpc.WithBalancer(bal))
 	if err != nil {
 		glog.Exitf("Could not connect to rpc server: %v", err)
 	}
