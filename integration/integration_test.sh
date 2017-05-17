efc548fdb37013f5f078f6b17bba92f4fc590f8c
David Drysdale <drysdale@google.com>
2017-05-17 17:19:47 +0100

    integration: drop CT integration tests
    These are moving across to the repo:
      https://github.com/google/certificate-transparency-go
    in subdir trillian/integration/

--- integration/integration_test.sh
+++ integration/integration_test.sh
@@ -6,5 +6,3 @@ INTEGRATION_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 run_test "Map integration test" "${INTEGRATION_DIR}/map_integration_test.sh"
 run_test "Log integration test" "${INTEGRATION_DIR}/log_integration_test.sh"
-run_test "CT integration test" "${INTEGRATION_DIR}/ct_integration_test.sh" 1 1 1
-run_test "CT multi-server integration test" "${INTEGRATION_DIR}/ct_integration_test.sh" 3 3 3
