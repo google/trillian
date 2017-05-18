4c6cc8535f5390b779d02d30cf66a584590755d9
David Drysdale <drysdale@google.com>
2017-05-18 17:19:52 +0100

    scripts: drop unused scripts

--- scripts/wipemap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <logid>"
-fi
-TESTDBOPTS="-u test --password=zaphod -D test"
-TREE_ID=$1
-# Wipe all Map storage rows for the given tree ID.
-mysql ${TESTDBOPTS} -e "DELETE FROM Trees WHERE TreeId = ${TREE_ID}"
