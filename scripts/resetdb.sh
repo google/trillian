2a4364576ae60f00d6e0bc41695301bf5814b3fd
Rob Percival <robpercival@google.com>
2017-05-19 16:33:59 +0100

    Change order of mysql arguments in scripts/resetdb.sh
    Some flags need to appear first, e.g. "--defaults-extra-file".

--- scripts/resetdb.sh
+++ scripts/resetdb.sh
@@ -3,10 +3,11 @@ echo "Completely wipe and reset database 'test'."
 read -p "Are you sure? " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    # A command line supplied -u will override the first argument.
-    mysql -u root "$@" -e 'DROP DATABASE IF EXISTS test;'
-    mysql -u root "$@" -e 'CREATE DATABASE test;'
-    mysql -u root "$@" -e "GRANT ALL ON test.* TO 'test'@'localhost' IDENTIFIED BY 'zaphod';"
-    mysql -u root "$@" -D test < $(go env GOPATH)/src/github.com/google/trillian/storage/mysql/storage.sql
+    # User-supplied arguments must be first. This is because some flags, such
+    # as --defaults-extra-file, must be at the start.
+    mysql "$@" -u root -e 'DROP DATABASE IF EXISTS test;'
+    mysql "$@" -u root -e 'CREATE DATABASE test;'
+    mysql "$@" -u root -e "GRANT ALL ON test.* TO 'test'@'localhost' IDENTIFIED BY 'zaphod';"
+    mysql "$@" -u root -D test < $(go env GOPATH)/src/github.com/google/trillian/storage/mysql/storage.sql
 fi
 echo
