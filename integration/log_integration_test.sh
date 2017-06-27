40df460c11e8a45d7f675701570318f5b202386c
Roland Shoemaker <roland@letsencrypt.org>
2017-06-26 22:13:36 -0700

    Use consistent naming scheme for test keys

--- integration/log_integration_test.sh
+++ integration/log_integration_test.sh
@@ -15,7 +15,7 @@ TO_KILL+=(${ETCD_PID})
 if [[ "${WITH_PKCS11}" == "true" ]]; then
   echo 0:${TMPDIR}/softhsm-slot0.db > ${SOFTHSM_CONF}
   softhsm --slot 0 --init-token --label log --pin 1234 --so-pin 5678
-  softhsm --slot 0 --import testdata/log-rpc-server-pkcs11.pem --label log_key --pin 1234 --id BEEF
+  softhsm --slot 0 --import testdata/log-rpc-server-pkcs11.privkey.pem --label log_key --pin 1234 --id BEEF
   KEY_ARGS="--private_key_format PKCS11ConfigFile --pkcs11_config_path testdata/pkcs11-conf.json --signature_algorithm=RSA"
 else
   KEY_ARGS="--pem_key_path=testdata/log-rpc-server.privkey.pem --pem_key_password=towel --signature_algorithm=ECDSA"
