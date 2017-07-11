249f236d8c29be6ba8ba950b7a04ae55b802ab2b
Gary Belvin <gdbelvin@gmail.com>
2017-07-11 18:05:57 +0100

    NewMapEnv() for map integration test matrix (#732)
    * NewMapEnv() for map integration test matrix
    
    This PR creates a MapEnv, similar to LogEnv to support pure go
    integration tests for the map.
    
    This supports targeted testing of round-trip AddMapLeaf /
    VerifyMapInclusionProof operations for each of the map hashers.
    
    At this time, the TEST_MAP_HASHER is the only one that is known to work.
    
    Also included:
    - Added function names along error return paths in the map.
    
    * cleanup from review
    
    * second round of edits

--- server/map_rpc_server.go
+++ server/map_rpc_server.go
@@ -57,13 +57,13 @@ func (t *TrillianMapServer) GetLeaves(ctx context.Context, req *trillian.GetMapL
 
 	tree, hasher, err := t.getTreeAndHasher(ctx, mapID, true /* readonly */)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("could not get map %v: %v", mapID, err)
 	}
 	ctx = trees.NewContext(ctx, tree)
 
 	tx, err := t.registry.MapStorage.SnapshotForTree(ctx, mapID)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("could not create database snapshot: %v", err)
 	}
 	defer tx.Close()
 
@@ -72,13 +72,13 @@ func (t *TrillianMapServer) GetLeaves(ctx context.Context, req *trillian.GetMapL
 		// need to know the newest published revision
 		r, err := tx.LatestSignedMapRoot(ctx)
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("could not fetch the latest SignedMapRoot: %v", err)
 		}
 		root = &r
 	} else {
 		r, err := tx.GetSignedMapRoot(ctx, req.Revision)
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("could not fetch SignedMapRoot %v: %v", req.Revision, err)
 		}
 		root = &r
 	}
@@ -96,7 +96,7 @@ func (t *TrillianMapServer) GetLeaves(ctx context.Context, req *trillian.GetMapL
 		// Fetch the leaf if it exists.
 		leaves, err := tx.Get(ctx, root.MapRevision, [][]byte{index})
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("could not fetch leaf %x: %v", index, err)
 		}
 		var leaf *trillian.MapLeaf
 		if len(leaves) == 1 {
@@ -113,7 +113,7 @@ func (t *TrillianMapServer) GetLeaves(ctx context.Context, req *trillian.GetMapL
 		// Fetch the proof regardless of whether the leaf exists.
 		proof, err := smtReader.InclusionProof(ctx, root.MapRevision, index)
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("could not get inclusion proof for leaf %x: %v", index, err)
 		}
 
 		inclusions = append(inclusions, &trillian.MapLeafInclusion{
@@ -124,7 +124,7 @@ func (t *TrillianMapServer) GetLeaves(ctx context.Context, req *trillian.GetMapL
 	glog.Infof("%v: wanted %v leaves, found %v", mapID, len(req.Index), found)
 
 	if err := tx.Commit(); err != nil {
-		return nil, err
+		return nil, fmt.Errorf("could not commit db transaction: %v", err)
 	}
 
 	return &trillian.GetMapLeavesResponse{
---
+++ testonly/integration/mapenv.go
@@ -0,0 +1,110 @@
+// Copyright 2017 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package integration
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/google/trillian"
+	"github.com/google/trillian/crypto/keys"
+	"github.com/google/trillian/extension"
+	"github.com/google/trillian/quota"
+	"github.com/google/trillian/server"
+	"github.com/google/trillian/server/admin"
+	"github.com/google/trillian/storage/mysql"
+
+	"google.golang.org/grpc"
+)
+
+// MapEnv is a map server and connected client.
+type MapEnv struct {
+	registry  extension.Registry
+	mapServer *server.TrillianMapServer
+
+	// Objects that need Close(), in order of creation.
+	DB         *sql.DB
+	grpcServer *grpc.Server
+	clientConn *grpc.ClientConn
+
+	// Public fields
+	MapClient   trillian.TrillianMapClient
+	AdminClient trillian.TrillianAdminClient
+}
+
+// NewMapEnv creates a fresh DB, map server, and client.
+func NewMapEnv(ctx context.Context, testID string) (*MapEnv, error) {
+	db, err := GetTestDB(testID)
+	if err != nil {
+		return nil, err
+	}
+
+	registry := extension.Registry{
+		AdminStorage:  mysql.NewAdminStorage(db),
+		MapStorage:    mysql.NewMapStorage(db),
+		QuotaManager:  quota.Noop(),
+		SignerFactory: &keys.DefaultSignerFactory{},
+	}
+
+	ret, err := NewMapEnvWithRegistry(ctx, testID, registry)
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+	ret.DB = db
+	return ret, nil
+}
+
+// NewMapEnvWithRegistry uses the passed in Registry to create a map server and
+// client.  testID should be unique to each unittest package so as to allow
+// parallel tests.
+func NewMapEnvWithRegistry(ctx context.Context, testID string, registry extension.Registry) (*MapEnv, error) {
+	addr, lis, err := listen()
+	if err != nil {
+		return nil, err
+	}
+
+	// Create Map Server.
+	grpcServer := grpc.NewServer()
+	mapServer := server.NewTrillianMapServer(registry)
+	trillian.RegisterTrillianMapServer(grpcServer, mapServer)
+	trillian.RegisterTrillianAdminServer(grpcServer, admin.New(registry))
+	go grpcServer.Serve(lis)
+
+	// Connect to the server.
+	cc, err := grpc.Dial(addr, grpc.WithInsecure())
+	if err != nil {
+		grpcServer.Stop()
+		return nil, err
+	}
+
+	return &MapEnv{
+		registry:    registry,
+		mapServer:   mapServer,
+		grpcServer:  grpcServer,
+		clientConn:  cc,
+		MapClient:   trillian.NewTrillianMapClient(cc),
+		AdminClient: trillian.NewTrillianAdminClient(cc),
+	}, nil
+}
+
+// Close shuts down the server.
+func (env *MapEnv) Close() {
+	env.clientConn.Close()
+	env.grpcServer.GracefulStop()
+	if env.DB != nil {
+		env.DB.Close()
+	}
+}
