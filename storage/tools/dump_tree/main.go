333606ef999911c203fdbd7450ce272d0f54abae
Martin Smith <mhs@google.com>
2017-07-13 14:29:59 +0100

    Improve a flag name and add usage examples.

--- storage/tools/dump_tree/main.go
+++ storage/tools/dump_tree/main.go
@@ -17,6 +17,20 @@
 // SubTree protos for examination and debugging. It does not require any actual storage
 // to be configured.
 //
+// Examples of some usages:
+//
+// Print a summary of the storage protos in a tree of size 1044, rebuilding internal nodes:
+// dump_tree -tree_size 1044 -summary
+//
+// Print all versions of all raw subtree protos for a tree of size 58:
+// dump_tree -tree_size 58 -latest_version=false
+//
+// Print the latest revision of each subtree proto for a tree of size 127 with hex keys:
+// dump_tree -tree_size 127
+//
+// Print out the nodes by level using the NodeReader API for a tree of size 11:
+// dump_tree -tree_size 11 -traverse
+//
 // The format for recordio output is as defined in:
 // https://github.com/google/or-tools/blob/master/ortools/base/recordio.h
 // This program always outputs uncompressed records.
@@ -65,7 +79,7 @@ var (
 	treeSizeFlag        = flag.Int("tree_size", 871, "The number of leaves to be added to the tree")
 	batchSizeFlag       = flag.Int("batch_size", 50, "The batch size for sequencing")
 	leafDataFormatFlag  = flag.String("leaf_format", "Leaf %d", "The format string for leaf data")
-	latestRevisionFlag  = flag.Bool("latest_revision", true, "If true outputs only the latest revision per subtree")
+	latestRevisionFlag  = flag.Bool("latest_version", true, "If true outputs only the latest revision per subtree")
 	summaryFlag         = flag.Bool("summary", false, "If true outputs a brief summary per subtree, false dumps the whole proto")
 	hexKeysFlag         = flag.Bool("hex_keys", false, "If true shows proto keys as hex rather than base64")
 	leafHashesFlag      = flag.Bool("leaf_hashes", false, "If true the summary output includes leaf hashes")
