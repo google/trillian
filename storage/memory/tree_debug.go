d7b228506fe2670ba1bf4689326411bd232e1072
Martin <Martin2112@users.noreply.github.com>
2017-07-14 15:29:45 +0100

    Add some storage development / debugging tools (#743)
    * Add initial subtree dumper
    
    * Add option to dump latest subtree versions only
    
    And fix up import order.
    
    * Add a summary output option
    
    * Tweak output to more readable text proto format
    
    * Add recordio format output
    
    * Tweak summary output format
    
    * Add sth check + option to rebuild internal nodes.
    
    * Optionally dump the proto leaf hashes
    
    * Add a hashing utility to compute leaf / node hashes
    
    * Add a tree traversal option
    
    Something is not working, needs debugging. It could be this or memory
    tree storage.
    
    * Added leaf dump, to print what's in the tree
    
    * Added option to convert proto keys from base64 to hex
    
    * Reorganize code to make gocyclo happier
    
    * Adjust tree traversal so it handles non-perfect trees
    
    * Improve a flag name and add usage examples.
    
    * A big stack of review fixes
    
    * Add missing fixes.

---
+++ storage/memory/tree_debug.go
@@ -0,0 +1,53 @@
+// Copyright 2017 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package memory
+
+import (
+	"github.com/golang/glog"
+	"github.com/google/btree"
+	"github.com/google/trillian/storage"
+	"github.com/google/trillian/storage/storagepb"
+)
+
+// This file contains utilities that are not part of the Storage API contracts but may
+// be useful for development or debugging.
+
+// Dump ascends the tree, logging the items contained.
+func Dump(t *btree.BTree) {
+	t.Ascend(func(i btree.Item) bool {
+		glog.Infof("%#v", i)
+		return true
+	})
+}
+
+// DumpSubtrees will traverse the the BTree and execute a callback on each subtree proto
+// that it contains. The traversal will be 'in order' according to the BTree keys, which
+// may not be useful at the application level.
+func DumpSubtrees(ls storage.LogStorage, treeID int64, callback func(string, *storagepb.SubtreeProto)) {
+	m := ls.(*memoryLogStorage)
+	tree := m.trees[treeID]
+	pi := subtreeKey(treeID, 0, storage.NewEmptyNodeID(64))
+
+	tree.store.AscendGreaterOrEqual(pi, func(bi btree.Item) bool {
+		i := bi.(*kv)
+
+		if _, ok := i.v.(*storagepb.SubtreeProto); !ok {
+			// Then we've finished iterating over subtrees
+			return false
+		}
+		callback(i.k, i.v.(*storagepb.SubtreeProto))
+		return true
+	})
+}
