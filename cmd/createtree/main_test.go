209702d99ea4b18b35436efe5887112f84322e00
Rob Percival <robpercival@google.com>
2017-07-26 18:12:29 +0100

    Refactor cmd/createtree to use flags directly
    The createOpts struct makes it difficult to have some flags/features
    behind build tags. This change also breaks out the key-format-specific
    test cases into their own files, so that they can be placed behind a
    build tag if necessary (e.g. to remove the PKCS#11 dependency).

--- cmd/createtree/main_test.go
+++ cmd/createtree/main_test.go
@@ -16,9 +16,9 @@ package main
 
 import (
 	"errors"
+	"flag"
 	"fmt"
 	"net"
-	"os"
 	"testing"
 	"time"
 
@@ -27,15 +27,34 @@ import (
 	"github.com/golang/protobuf/ptypes/any"
 	"github.com/golang/protobuf/ptypes/empty"
 	"github.com/google/trillian"
-	"github.com/google/trillian/crypto/keys"
 	"github.com/google/trillian/crypto/keyspb"
 	"github.com/google/trillian/crypto/sigpb"
+	"github.com/google/trillian/util/flagsaver"
 	"github.com/kylelemons/godebug/pretty"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 )
 
-func marshalAny(p proto.Message) *any.Any {
+// defaultTree reflects all flag defaults with the addition of a valid private key.
+var defaultTree = &trillian.Tree{
+	TreeState:          trillian.TreeState_ACTIVE,
+	TreeType:           trillian.TreeType_LOG,
+	HashStrategy:       trillian.HashStrategy_RFC6962_SHA256,
+	HashAlgorithm:      sigpb.DigitallySigned_SHA256,
+	SignatureAlgorithm: sigpb.DigitallySigned_RSA,
+	PrivateKey:         mustMarshalAny(&empty.Empty{}),
+	MaxRootDuration:    ptypes.DurationProto(0 * time.Millisecond),
+}
+
+type testCase struct {
+	desc      string
+	setFlags  func()
+	createErr error
+	wantErr   bool
+	wantTree  *trillian.Tree
+}
+
+func mustMarshalAny(p proto.Message) *any.Any {
 	anyKey, err := ptypes.MarshalAny(p)
 	if err != nil {
 		panic(err)
@@ -43,155 +62,95 @@ func marshalAny(p proto.Message) *any.Any {
 	return anyKey
 }
 
-func TestRun(t *testing.T) {
-	err := os.Chdir("../..")
-	if err != nil {
-		t.Fatalf("Unable to change working directory to ../..: %s", err)
-	}
-
-	pemPath, pemPassword := "testdata/log-rpc-server.privkey.pem", "towel"
-	pemSigner, err := keys.NewFromPrivatePEMFile(pemPath, pemPassword)
-	if err != nil {
-		t.Fatalf("NewFromPrivatePEM(): %v", err)
-	}
-	pemDer, err := keys.MarshalPrivateKey(pemSigner)
-	if err != nil {
-		t.Fatalf("MarshalPrivateKey(): %v", err)
-	}
-	anyPrivKey, err := ptypes.MarshalAny(&keyspb.PrivateKey{Der: pemDer})
-	if err != nil {
-		t.Fatalf("MarshalAny(%v): %v", pemDer, err)
-	}
-
-	// defaultTree reflects all flag defaults with the addition of a valid pk
-	defaultTree := &trillian.Tree{
-		TreeState:          trillian.TreeState_ACTIVE,
-		TreeType:           trillian.TreeType_LOG,
-		HashStrategy:       trillian.HashStrategy_RFC6962_SHA256,
-		HashAlgorithm:      sigpb.DigitallySigned_SHA256,
-		SignatureAlgorithm: sigpb.DigitallySigned_RSA,
-		PrivateKey:         anyPrivKey,
-		MaxRootDuration:    ptypes.DurationProto(0 * time.Millisecond),
-	}
-
-	server, lis, stopFn, err := startFakeServer()
-	if err != nil {
-		t.Fatalf("Error starting fake server: %v", err)
-	}
-	defer stopFn()
-	server.generatedKey = anyPrivKey
-
-	validOpts := newOptsFromFlags()
-	validOpts.addr = lis.Addr().String()
-
+func TestCreateTree(t *testing.T) {
 	nonDefaultTree := *defaultTree
 	nonDefaultTree.TreeType = trillian.TreeType_MAP
 	nonDefaultTree.SignatureAlgorithm = sigpb.DigitallySigned_ECDSA
 	nonDefaultTree.DisplayName = "Llamas Map"
 	nonDefaultTree.Description = "For all your digital llama needs!"
 
-	nonDefaultOpts := *validOpts
-	nonDefaultOpts.treeType = nonDefaultTree.TreeType.String()
-	nonDefaultOpts.sigAlgorithm = nonDefaultTree.SignatureAlgorithm.String()
-	nonDefaultOpts.displayName = nonDefaultTree.DisplayName
-	nonDefaultOpts.description = nonDefaultTree.Description
-
-	emptyAddr := *validOpts
-	emptyAddr.addr = ""
-
-	invalidEnumOpts := *validOpts
-	invalidEnumOpts.treeType = "LLAMA!"
-
-	privateKeyOpts := *validOpts
-	privateKeyOpts.privateKeyType = "PrivateKey"
-	privateKeyOpts.pemKeyPath = pemPath
-	privateKeyOpts.pemKeyPass = pemPassword
-
-	emptyKeyTypeOpts := privateKeyOpts
-	emptyKeyTypeOpts.privateKeyType = ""
-
-	invalidKeyTypeOpts := privateKeyOpts
-	invalidKeyTypeOpts.privateKeyType = "LLAMA!!"
-
-	emptyPEMPath := privateKeyOpts
-	emptyPEMPath.pemKeyPath = ""
-
-	emptyPEMPass := privateKeyOpts
-	emptyPEMPass.pemKeyPass = ""
-
-	pemKeyOpts := privateKeyOpts
-	pemKeyOpts.privateKeyType = "PEMKeyFile"
-	pemKeyTree := *defaultTree
-	pemKeyTree.PrivateKey, err = ptypes.MarshalAny(&keyspb.PEMKeyFile{
-		Path:     pemPath,
-		Password: pemPassword,
+	runTest(t, []*testCase{
+		{
+			desc:     "validOpts",
+			wantTree: defaultTree,
+		},
+		{
+			desc: "nonDefaultOpts",
+			setFlags: func() {
+				*treeType = nonDefaultTree.TreeType.String()
+				*signatureAlgorithm = nonDefaultTree.SignatureAlgorithm.String()
+				*displayName = nonDefaultTree.DisplayName
+				*description = nonDefaultTree.Description
+			},
+			wantTree: &nonDefaultTree,
+		},
+		{
+			desc:     "defaultOptsOnly",
+			setFlags: resetFlags,
+			wantErr:  true,
+		},
+		{
+			desc:     "emptyAddr",
+			setFlags: func() { *adminServerAddr = "" },
+			wantErr:  true,
+		},
+		{
+			desc:     "invalidEnumOpts",
+			setFlags: func() { *treeType = "LLAMA!" },
+			wantErr:  true,
+		},
+		{
+			desc:     "invalidKeyTypeOpts",
+			setFlags: func() { *privateKeyFormat = "LLAMA!!" },
+			wantErr:  true,
+		},
+		{
+			desc:      "createErr",
+			createErr: errors.New("create tree failed"),
+			wantErr:   true,
+		},
 	})
+}
+
+func runTest(t *testing.T, tests []*testCase) {
+	server, lis, stopFn, err := startFakeServer()
 	if err != nil {
-		t.Fatalf("MarshalAny(PEMKeyFile): %v", err)
+		t.Fatalf("Error starting fake server: %v", err)
 	}
-
-	pkcs11Opts := *validOpts
-	pkcs11Opts.privateKeyType = "PKCS11ConfigFile"
-	pkcs11Opts.pkcs11ConfigPath = "testdata/pkcs11-conf.json"
-	pkcs11Tree := *defaultTree
-	pkcs11Tree.PrivateKey, err = ptypes.MarshalAny(&keyspb.PKCS11Config{
-		TokenLabel: "log",
-		Pin:        "1234",
-		PublicKey: `-----BEGIN PUBLIC KEY-----
-MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC7/tWwqUXZJaNfnpvnqiaeNMkn
-hKusCsyAidrHxvuL+t54XFCHJwsB3wIlQZ4mMwb8mC/KRYhCqECBEoCAf/b0m3j/
-ASuEPLyYOrz/aEs3wP02IZQLGmihmjMk7T/ouNCuX7y1fTjX3GeVQ06U/EePwZFC
-xToc6NWBri0N3VVsswIDAQAB
------END PUBLIC KEY-----
-`,
-	})
-	if err != nil {
-		t.Fatalf("MarshalAny(PKCS11Config): %v", err)
-	}
-
-	emptyPKCS11Path := pkcs11Opts
-	emptyPKCS11Path.pkcs11ConfigPath = ""
-
-	tests := []struct {
-		desc      string
-		opts      *createOpts
-		createErr error
-		wantErr   bool
-		wantTree  *trillian.Tree
-	}{
-		{desc: "validOpts", opts: validOpts, wantTree: defaultTree},
-		{desc: "nonDefaultOpts", opts: &nonDefaultOpts, wantTree: &nonDefaultTree},
-		{desc: "defaultOptsOnly", opts: newOptsFromFlags(), wantErr: true}, // No mandatory opts provided
-		{desc: "emptyAddr", opts: &emptyAddr, wantErr: true},
-		{desc: "invalidEnumOpts", opts: &invalidEnumOpts, wantErr: true},
-		{desc: "emptyKeyTypeOpts", opts: &emptyKeyTypeOpts, wantErr: true},
-		{desc: "invalidKeyTypeOpts", opts: &invalidKeyTypeOpts, wantErr: true},
-		{desc: "emptyPEMPath", opts: &emptyPEMPath, wantErr: true},
-		{desc: "emptyPEMPass", opts: &emptyPEMPass, wantErr: true},
-		{desc: "PrivateKey", opts: &privateKeyOpts, wantTree: defaultTree},
-		{desc: "PEMKeyFile", opts: &pemKeyOpts, wantTree: &pemKeyTree},
-		{desc: "createErr", opts: validOpts, createErr: errors.New("create tree failed"), wantErr: true},
-		{desc: "PKCS11Config", opts: &pkcs11Opts, wantTree: &pkcs11Tree},
-		{desc: "emptyPKCS11Path", opts: &emptyPKCS11Path, wantErr: true},
-	}
+	defer stopFn()
+	server.generatedKey = defaultTree.PrivateKey
 
 	ctx := context.Background()
 	for _, test := range tests {
-		server.err = test.createErr
+		t.Run(test.desc, func(t *testing.T) {
+			defer flagsaver.Save().Restore()
+			*adminServerAddr = lis.Addr().String()
+			if test.setFlags != nil {
+				test.setFlags()
+			}
 
-		tree, err := createTree(ctx, test.opts)
-		switch hasErr := err != nil; {
-		case hasErr != test.wantErr:
-			t.Errorf("%v: createTree() returned err = '%v', wantErr = %v", test.desc, err, test.wantErr)
-			continue
-		case hasErr:
-			continue
-		}
+			server.err = test.createErr
 
-		if !proto.Equal(tree, test.wantTree) {
-			t.Errorf("%v: post-createTree diff -got +want:\n%v", test.desc, pretty.Compare(tree, test.wantTree))
-		}
+			tree, err := createTree(ctx)
+			switch hasErr := err != nil; {
+			case hasErr != test.wantErr:
+				t.Errorf("createTree() returned err = '%v', wantErr = %v", err, test.wantErr)
+				return
+			case hasErr:
+				return
+			}
+
+			if !proto.Equal(tree, test.wantTree) {
+				t.Errorf("post-createTree diff -got +want:\n%v", pretty.Compare(tree, test.wantTree))
+			}
+		})
 	}
+}
+
+func resetFlags() {
+	flag.Visit(func(f *flag.Flag) {
+		f.Value.Set(f.DefValue)
+	})
 }
 
 // fakeAdminServer that implements CreateTree.
---
+++ cmd/createtree/pem_test.go
@@ -0,0 +1,111 @@
+// Copyright 2017 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"testing"
+
+	"github.com/google/trillian/crypto/keys"
+	"github.com/google/trillian/crypto/keyspb"
+)
+
+func TestWithPEMKeyFile(t *testing.T) {
+	pemPath, pemPassword := "../../testdata/log-rpc-server.privkey.pem", "towel"
+
+	wantTree := *defaultTree
+	wantTree.PrivateKey = mustMarshalAny(&keyspb.PEMKeyFile{
+		Path:     pemPath,
+		Password: pemPassword,
+	})
+
+	runTest(t, []*testCase{
+		{
+			desc: "empty pemKeyPath",
+			setFlags: func() {
+				*privateKeyFormat = "PEMKeyFile"
+				*pemKeyPath = ""
+				*pemKeyPassword = pemPassword
+			},
+			wantErr: true,
+		},
+		{
+			desc: "empty pemKeyPass",
+			setFlags: func() {
+				*privateKeyFormat = "PEMKeyFile"
+				*pemKeyPath = pemPath
+				*pemKeyPassword = ""
+			},
+			wantErr: true,
+		},
+		{
+			desc: "valid pemKeyPath and pemKeyPass",
+			setFlags: func() {
+				*privateKeyFormat = "PEMKeyFile"
+				*pemKeyPath = pemPath
+				*pemKeyPassword = pemPassword
+			},
+			wantTree: &wantTree,
+		},
+	})
+}
+
+func TestWithPrivateKey(t *testing.T) {
+	pemPath, pemPassword := "../../testdata/log-rpc-server.privkey.pem", "towel"
+
+	key, err := keys.NewFromPrivatePEMFile(pemPath, pemPassword)
+	if err != nil {
+		t.Fatalf("Error reading test private key file: %v", err)
+	}
+
+	keyDER, err := keys.MarshalPrivateKey(key)
+	if err != nil {
+		t.Fatalf("Error marshaling test private key to DER: %v", err)
+	}
+
+	wantTree := *defaultTree
+	wantTree.PrivateKey = mustMarshalAny(&keyspb.PrivateKey{
+		Der: keyDER,
+	})
+
+	runTest(t, []*testCase{
+		{
+			desc: "empty pemKeyPath",
+			setFlags: func() {
+				*privateKeyFormat = "PrivateKey"
+				*pemKeyPath = ""
+				*pemKeyPassword = pemPassword
+			},
+			wantErr: true,
+		},
+		{
+			desc: "empty pemKeyPass",
+			setFlags: func() {
+				*privateKeyFormat = "PrivateKey"
+				*pemKeyPath = pemPath
+				*pemKeyPassword = ""
+			},
+			wantErr: true,
+		},
+		{
+			desc: "valid pemKeyPath and pemKeyPass",
+			setFlags: func() {
+				*privateKeyFormat = "PrivateKey"
+				*pemKeyPath = pemPath
+				*pemKeyPassword = pemPassword
+			},
+			wantTree: &wantTree,
+		},
+	})
+}
