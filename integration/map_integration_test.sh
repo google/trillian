6f2fca6d9caa4ff296802b90014c3eefb15e0002
David Drysdale <drysdale@google.com>
2017-09-06 15:39:01 +0100

    integration: function for map test setup/teardown

--- integration/map_integration_test.sh
+++ integration/map_integration_test.sh
@@ -3,28 +3,18 @@ set -e
 INTEGRATION_DIR="$( cd "$( dirname "$0" )" && pwd )"
 . "${INTEGRATION_DIR}"/functions.sh
 
-echo "Building code"
-go build ${GOFLAGS} github.com/google/trillian/server/trillian_map_server/
-
-yes | "${TRILLIAN_PATH}/scripts/resetdb.sh"
-
-RPC_PORT=$(pick_unused_port)
-
-echo "Starting Map RPC server on localhost:${RPC_PORT}"
-./trillian_map_server --rpc_endpoint="localhost:${RPC_PORT}" http_endpoint='' &
-RPC_SERVER_PID=$!
-wait_for_server_startup ${RPC_PORT}
-TO_KILL+=(${RPC_SERVER_PID})
+echo "Launching core Trillian map components"
+map_prep_test 1
+TO_KILL+=(${RPC_SERVER_PIDS[@]})
 
 echo "Running test"
 cd "${INTEGRATION_DIR}"
 set +e
-go test ${GOFLAGS} --timeout=5m ./maptest  --map_rpc_server="localhost:${RPC_PORT}" 
+go test ${GOFLAGS} --timeout=5m ./maptest  --map_rpc_server="${RPC_SERVER_1}"
 RESULT=$?
 set -e
 
-echo "Stopping Map RPC server (pid ${RPC_SERVER_PID})"
-kill_pid ${RPC_SERVER_PID}
+map_stop_test
 TO_KILL=()
 
 if [ $RESULT != 0 ]; then
