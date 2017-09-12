a6546d092307f6e0d396068066033b434203824d
Martin <Martin2112@users.noreply.github.com>
2017-09-12 16:27:41 +0100

    Allow log server to return consistency proofs where first=second (#819)
    * Allow log server to return proofs where first=second
    
    This is for additional compatiblity with the older C++ log server code.
    
    * lint fix
    
    * Fix typo
    
    * Fix inconsistent error message.

--- server/validate.go
+++ server/validate.go
@@ -51,8 +51,8 @@ func validateGetConsistencyProofRequest(req *trillian.GetConsistencyProofRequest
 	if req.SecondTreeSize <= 0 {
 		return status.Errorf(codes.InvalidArgument, "GetConsistencyProofRequest.SecondTreeSize: %v, want > 0", req.SecondTreeSize)
 	}
-	if req.SecondTreeSize <= req.FirstTreeSize {
-		return status.Errorf(codes.InvalidArgument, "GetConsistencyProofRequest.FirstTreeSize: %v < GetConsistencyProofRequest.SecondTreeSize: %v, want > ", req.FirstTreeSize, req.SecondTreeSize)
+	if req.SecondTreeSize < req.FirstTreeSize {
+		return status.Errorf(codes.InvalidArgument, "GetConsistencyProofRequest.FirstTreeSize: %v < GetConsistencyProofRequest.SecondTreeSize: %v, want >= ", req.FirstTreeSize, req.SecondTreeSize)
 	}
 	return nil
 }
