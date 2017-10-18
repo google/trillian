20acd11a6e30b61cf806d426ab2ab46a226425a6
Paul Hadfield <hadfieldp@google.com>
2017-10-18 14:31:12 +0100

    Test TrillianMapServer.GetSignedMapRoot

--- server/map_rpc_server_test.go
+++ server/map_rpc_server_test.go
@@ -1,4 +1,4 @@
-// Copyright 2017 Google Inc. All Rights Reserved.
+// Copyright 2016 Google Inc. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,13 +15,17 @@
 package server
 
 import (
+	"context"
 	"errors"
 	"testing"
 
 	"github.com/golang/mock/gomock"
+	"github.com/golang/protobuf/proto"
+	"github.com/google/trillian"
 	"github.com/google/trillian/extension"
 	"github.com/google/trillian/storage"
 	stestonly "github.com/google/trillian/storage/testonly"
+	"github.com/kylelemons/godebug/pretty"
 )
 
 const (
@@ -46,13 +50,80 @@ func TestIsHealthy(t *testing.T) {
 	 	mockStorage.EXPECT().CheckDatabaseAccessible(gomock.Any()).Return(test.accessibleErr)
 
 		server := NewTrillianMapServer(extension.Registry{
-			AdminStorage: mockAdminStorageForMap(ctrl, mapID1),
+			AdminStorage: mockAdminStorageForMap(ctrl, 123),
 			MapStorage:   mockStorage,
 		})
 
 		err := server.IsHealthy()
 		if gotErr := err != nil; gotErr != test.wantErr {
 			t.Errorf("%s: IsHealthy() err? %t want? %t (err=%v)", test.desc, gotErr, test.wantErr, err)
+		}
+	}
+}
+
+func TestGetSignedMapRoot(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	ctx := context.Background()
+
+	tests := []struct {
+		desc string
+		req *trillian.GetSignedMapRootRequest
+		mapRoot trillian.SignedMapRoot
+		snapShErr, lsmrErr error
+		wantErr bool
+	}{
+		{
+			desc: "Unknown map",
+			req: &trillian.GetSignedMapRootRequest{},
+			snapShErr: errors.New("unknown map"),
+			wantErr: true,
+		},
+		{
+			desc: "Map is empty, head at revision 0",
+			req: &trillian.GetSignedMapRootRequest{MapId: mapID1},
+			lsmrErr: errors.New("sql: no rows in result set"),
+			wantErr: true,
+		},
+		{
+			desc: "Map has leaves, head > revision 0",
+			req: &trillian.GetSignedMapRootRequest{MapId: mapID1},
+			mapRoot: trillian.SignedMapRoot{
+			},
+		},
+	}
+
+	for _, test := range tests {
+		mockStorage := storage.NewMockMapStorage(ctrl)
+		mockTx := storage.NewMockMapTreeTX(ctrl)
+
+		mockStorage.EXPECT().SnapshotForTree(gomock.Any(), test.req.MapId).Return(mockTx, test.snapShErr)
+		if test.snapShErr == nil {
+			mockTx.EXPECT().LatestSignedMapRoot(gomock.Any()).Return(test.mapRoot, test.lsmrErr)
+			if test.lsmrErr == nil {
+				mockTx.EXPECT().Commit().Return(nil)
+			}
+			mockTx.EXPECT().Close().Return(nil)
+			mockTx.EXPECT().IsOpen().AnyTimes().Return(false)
+		}
+
+		server := NewTrillianMapServer(extension.Registry{
+			AdminStorage: mockAdminStorageForMap(ctrl, mapID1),
+			MapStorage:   mockStorage,
+		})
+
+		smrResp, err := server.GetSignedMapRoot(ctx, test.req)
+
+		if gotErr := err != nil; gotErr != test.wantErr {
+			t.Errorf("%s: GetSignedMapRoot()=_, err? %t want? %t (err=%v)", test.desc, gotErr, test.wantErr, err)
+		}
+		if err != nil {
+			continue
+		}
+		want := &trillian.GetSignedMapRootResponse{MapRoot: &test.mapRoot}
+		if got := smrResp; !proto.Equal(got, want) {
+			diff := pretty.Compare(got, want)
+			t.Errorf("%s: GetSignedMapRoot() got != want, diff:\n%v", test.desc, diff)
 		}
 	}
 }
