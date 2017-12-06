183fdf983c406c8bd125efb9e0eb826d9277eae8
Paul Hadfield <hadfieldp@google.com>
2017-12-06 12:25:16 +0000

    Require revision >=0 in GetMapLeavesByRevision and GetSignedMapRootByRevision

--- server/map_rpc_server.go
+++ server/map_rpc_server.go
@@ -120,10 +120,13 @@ func (t *TrillianMapServer) GetLeaves(ctx context.Context, req *trillian.GetMapL
 
 // GetLeavesByRevision implements the GetLeavesByRevision RPC method.
 func (t *TrillianMapServer) GetLeavesByRevision(ctx context.Context, req *trillian.GetMapLeavesByRevisionRequest) (*trillian.GetMapLeavesResponse, error) {
-        return t.getLeavesByRevision(ctx, req.MapId, req.Index, int64(req.Revision))
+        return t.getLeavesByRevision(ctx, req.MapId, req.Index, req.Revision)
 }
 
 func (t *TrillianMapServer) getLeavesByRevision(ctx context.Context, mapID int64, indices [][]byte, revision int64) (*trillian.GetMapLeavesResponse, error) {
+	if revision < 0 {
+		return nil, fmt.Errorf("map revision %d must be >= 0", revision)
+	}
 	if err := t.Init(ctx, mapID); err != nil {
 		return nil, err
 	}
@@ -345,6 +348,9 @@ func (t *TrillianMapServer) GetSignedMapRoot(ctx context.Context, req *trillian.
 // GetSignedMapRootByRevision implements the GetSignedMapRootByRevision RPC
 // method.
 func (t *TrillianMapServer) GetSignedMapRootByRevision(ctx context.Context, req *trillian.GetSignedMapRootByRevisionRequest) (*trillian.GetSignedMapRootResponse, error) {
+	if req.Revision < 0 {
+		return nil, fmt.Errorf("map revision %d must be >= 0", req.Revision)
+	}
 	if err := t.Init(ctx, req.MapId); err != nil {
 		return nil, err
 	}
