bd887aa5466cb4a7379729a85e58591298394f43
Martin Smith <mhs@google.com>
2018-01-29 14:11:56 +0000

    Add a command for freezing trees for use by integration tests
    Refactor of createtree to allow sharing of test infrastructure.

---
+++ cmd/freezetree/main.go
@@ -0,0 +1,99 @@
+// Copyright 2017 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package main contains the implementation and entry point for the freezetree
+// command.
+//
+// Example usage:
+// $ ./freezetree --admin_server=host:port --tree_id=123456789
+//
+// The output is minimal to allow for easy usage in automated scripts.
+package main
+
+import (
+	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"time"
+
+	"github.com/golang/glog"
+	"github.com/google/trillian"
+	"google.golang.org/genproto/protobuf/field_mask"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+var (
+	adminServerAddr = flag.String("admin_server", "", "Address of the gRPC Trillian Admin Server (host:port)")
+	rpcDeadline     = flag.Duration("rpc_deadline", time.Second*10, "Deadline for RPC requests")
+	treeID          = flag.Int64("tree_id", 0, "The ID of the tree to be set to a frozen state")
+)
+
+func freezeTree(ctx context.Context) (*trillian.Tree, error) {
+	if *adminServerAddr == "" {
+		return nil, errors.New("empty --admin_server, please provide the Admin server host:port")
+	}
+
+	// We only want to update the state of the tree, which means we need a field
+	// mask on the request.
+	treeStateMask := &field_mask.FieldMask{
+		Paths: []string{"tree_state"},
+	}
+
+	req := &trillian.UpdateTreeRequest{
+		Tree: &trillian.Tree{
+			TreeId:    *treeID,
+			TreeState: trillian.TreeState_FROZEN,
+		},
+		UpdateMask: treeStateMask,
+	}
+
+	conn, err := grpc.Dial(*adminServerAddr, grpc.WithInsecure())
+	if err != nil {
+		return nil, fmt.Errorf("failed to dial %v: %v", *adminServerAddr, err)
+	}
+	defer conn.Close()
+
+	client := trillian.NewTrillianAdminClient(conn)
+	for {
+		tree, err := client.UpdateTree(ctx, req)
+		if err == nil {
+			return tree, nil
+		}
+		if s, ok := status.FromError(err); ok && s.Code() == codes.Unavailable {
+			glog.Errorf("Admin server unavailable, trying again: %v", err)
+			time.Sleep(100 * time.Millisecond)
+			continue
+		}
+		return nil, fmt.Errorf("failed to UpdateTree(%+v): %T %v", req, err, err)
+	}
+}
+
+func main() {
+	flag.Parse()
+
+	ctx, cancel := context.WithTimeout(context.Background(), *rpcDeadline)
+	defer cancel()
+	tree, err := freezeTree(ctx)
+	if err != nil {
+		glog.Exitf("Failed to freeze tree: %v", err)
+	}
+
+	// DO NOT change the output format, scripts are meant to depend on it.
+	// If you really want to change it, provide an output_format flag and
+	// keep the default as-is.
+	fmt.Println(tree.TreeState)
+}
---
+++ cmd/freezetree/main_test.go
@@ -0,0 +1,121 @@
+// Copyright 2017 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+	"errors"
+	"flag"
+	"testing"
+
+	"github.com/google/trillian"
+	"github.com/google/trillian/cmd/testonly"
+	"github.com/google/trillian/util/flagsaver"
+)
+
+type testCase struct {
+	desc      string
+	setFlags  func()
+	updateErr error
+	wantErr   bool
+	wantState trillian.TreeState
+}
+
+func TestFreezeTree(t *testing.T) {
+
+	runTest(t, []*testCase{
+		{
+			// We don't set the treeID in runTest so this should fail.
+			desc:    "missingTreeID",
+			wantErr: true,
+		},
+		{
+			desc: "mandatoryOptsNotSet",
+			// Undo the flags set by runTest, so that mandatory options are no longer set.
+			setFlags: resetFlags,
+			wantErr:  true,
+		},
+		{
+			desc:      "validUpdate",
+			setFlags:  func() { *treeID = 12345 },
+			wantState: trillian.TreeState_FROZEN,
+		},
+		{
+			desc:     "unknownTree",
+			setFlags: func() { *treeID = 123456 },
+			wantErr:  true,
+		},
+		{
+			desc:     "emptyAddr",
+			setFlags: func() { *adminServerAddr = "" },
+			wantErr:  true,
+		},
+		{
+			desc:      "updateErr",
+			updateErr: errors.New("update tree failed"),
+			wantErr:   true,
+		},
+	})
+}
+
+// runTest executes the updateTree command against a fake TrillianAdminServer
+// for each of the provided tests, and checks that the tree in the request is
+// as expected, or an expected error occurs.
+// Prior to each test case, it:
+// 1. Resets all flags to their original values.
+// 2. Sets the adminServerAddr flag to point to the fake server.
+// 3. Calls the test's setFlags func (if provided) to allow it to change flags specific to the test.
+func runTest(t *testing.T, tests []*testCase) {
+	server := &testonly.FakeAdminServer{TreeID: 12345}
+
+	lis, stopFakeServer, err := testonly.StartFakeAdminServer(server)
+	if err != nil {
+		t.Fatalf("Error starting fake server: %v", err)
+	}
+	defer stopFakeServer()
+
+	ctx := context.Background()
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			defer flagsaver.Save().Restore()
+			*adminServerAddr = lis.Addr().String()
+			if test.setFlags != nil {
+				test.setFlags()
+			}
+
+			server.Err = test.updateErr
+
+			tree, err := freezeTree(ctx)
+			switch hasErr := err != nil; {
+			case hasErr != test.wantErr:
+				t.Errorf("updateTree() returned err = '%v', wantErr = %v", err, test.wantErr)
+				return
+			case hasErr:
+				return
+			}
+
+			if got, want := tree.TreeState.String(), test.wantState.String(); got != want {
+				t.Errorf("updated state incorrect got:%v want:%v", got, want)
+			}
+		})
+	}
+}
+
+// resetFlags sets all flags to their default values.
+func resetFlags() {
+	flag.Visit(func(f *flag.Flag) {
+		f.Value.Set(f.DefValue)
+	})
+}
