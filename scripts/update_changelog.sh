e518f1ed7fe00f225481bb80c4582034e81c2de3
Martin Smith <mhs@google.com>
2018-02-15 10:59:58 +0000

    Fixes

--- scripts/update_changelog.sh
+++ scripts/update_changelog.sh
@@ -7,15 +7,11 @@
 
 set +e
 d=${GOPATH[0]}
-PATH=$PATH:${d}/bin
 
 # Get and build the correct branch that includes markdown output
-go get -d -u github.com/Martin2112/github-release
-pushd $d/src/github.com/Martin2112/github-release
-git fetch origin add_changelog_output
-git checkout add_changelog_output
+# TODO(Martin2112): replace with upstream repo if/when aktau/github-release#81 is merged
 go install github.com/Martin2112/github-release
 
 # Generate the changelog
-github-release info -r trillian -u google --markdown > CHANGELOG.md
+${d}/bin/github-release info -r trillian -u google --markdown > CHANGELOG.md
 
