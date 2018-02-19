4a2e8cfca11eba2b590174592264454a5b087ba1
Martin Smith <mhs@google.com>
2018-02-19 14:05:38 +0000

    Update var declarations

--- server/map_rpc_server.go
+++ server/map_rpc_server.go
@@ -37,9 +37,11 @@ const (
 	mostRecentRevision = -1
 )
 
-var optsMapRead = storage.NewGetOpts(storage.Query, true, trillian.TreeType_MAP)
-var optsMapWrite = storage.NewGetOpts(storage.Update, false, trillian.TreeType_MAP)
-var optsMapAdmin = storage.NewGetOpts(storage.Admin, false, trillian.TreeType_MAP)
+var (
+	optsMapRead  = storage.NewGetOpts(storage.Query, true, trillian.TreeType_MAP)
+	optsMapWrite = storage.NewGetOpts(storage.Update, false, trillian.TreeType_MAP)
+	optsMapAdmin = storage.NewGetOpts(storage.Admin, false, trillian.TreeType_MAP)
+)
 
 // TODO(codingllama): There is no access control in the server yet and clients could easily modify
 // any tree.
