3cbade46e23b33e8a00a1e2b4ee8101de9307446
Martin Smith <mhs@google.com>
2018-02-22 14:29:52 +0000

    Fixup trees test

--- trees/trees_test.go
+++ trees/trees_test.go
@@ -222,7 +222,7 @@ func TestGetTree(t *testing.T) {
 
 		admin := storage.NewMockAdminStorage(ctrl)
 		tx := storage.NewMockReadOnlyAdminTX(ctrl)
-		admin.EXPECT().Snapshot(ctx, gomock.Any()).MaxTimes(1).Return(tx, test.beginErr)
+		admin.EXPECT().Snapshot(ctx).MaxTimes(1).Return(tx, test.beginErr)
 		tx.EXPECT().GetTree(ctx, test.treeID).MaxTimes(1).Return(test.storageTree, test.getErr)
 		tx.EXPECT().Close().MaxTimes(1).Return(nil)
 		tx.EXPECT().Commit().MaxTimes(1).Return(test.commitErr)
