b95d10bacbc5843f414c13053cadef22b56ae2b5
David Drysdale <drysdale@google.com>
2018-03-28 14:54:26 +0100

    merkle: drop objhashed
    This removes use of objecthash.

--- server/trillian_log_signer/main.go
+++ server/trillian_log_signer/main.go
@@ -38,7 +38,6 @@ import (
 	_ "github.com/google/trillian/crypto/keys/pem/proto"
 	_ "github.com/google/trillian/crypto/keys/pkcs11/proto"
 	// Load hashers
-	_ "github.com/google/trillian/merkle/objhasher"
 	_ "github.com/google/trillian/merkle/rfc6962"
 )
 
