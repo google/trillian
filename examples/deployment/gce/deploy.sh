d26459c03e253714321744e1f255bf006fa0a8e1
Al Cutter <al@google.com>
2018-05-04 15:01:13 +0100

    Update Kubernetes scripts

--- examples/deployment/gce/deploy.sh
+++ examples/deployment/gce/deploy.sh
@@ -1,7 +1,11 @@
+#!/usr/bin/env bash
 # Script assumptions:
+# - Cluster has already been created & configured using the create.sh script
 # - Go 1.9 is installed
-export PROJECT_NAME=TODO
-export TAG=latest
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source ${DIR}/config.sh
+
 export LOG_URL=TODO
 export MAP_URL=TODO
 
@@ -16,30 +20,24 @@ docker build -f examples/deployment/docker/log_signer/Dockerfile -t us.gcr.io/$P
 docker build -f examples/deployment/docker/map_server/Dockerfile -t us.gcr.io/$PROJECT_NAME/map_server:$TAG .
 
 # Connect to gcloud
-gcloud config set project $PROJECT_NAME
-gcloud config set compute/zone us-central1-b
-gcloud container clusters create "cluster-1" --machine-type "n1-standard-1" --image-type "COS" --num-nodes "2" --enable-autorepair --enable-autoupgrade
-# Create some dedicated node pools, the DB and MapServer will use the cluster's default pool
-gcloud container node-pools create "logserver-pool" --machine-type "n1-standard-1" --image-type "COS" --num-nodes "2" --enable-autorepair --enable-autoupgrade
-gcloud container node-pools create "signer-pool" --machine-type "n1-standard-1" --image-type "COS" --num-nodes "1" --enable-autorepair --enable-autoupgrade
-gcloud container node-pools create "ctfe-pool" --machine-type "n1-standard-1" --image-type "COS" --num-nodes "2" --enable-autorepair --enable-autoupgrade
-gcloud container clusters get-credentials cluster-1
+gcloud config set project "${PROJECT_NAME}"
+gcloud config set compute/zone "${ZONE}"
 
 # Push docker images
-gcloud docker -- push us.gcr.io/${PROJECT_NAME}/db:$TAG
-gcloud docker -- push us.gcr.io/${PROJECT_NAME}/log_server:$TAG
-gcloud docker -- push us.gcr.io/${PROJECT_NAME}/log_signer:$TAG
-gcloud docker -- push us.gcr.io/${PROJECT_NAME}/map_server:$TAG
+gcloud docker -- push "us.gcr.io/${PROJECT_NAME}/db:${TAG}"
+gcloud docker -- push "us.gcr.io/${PROJECT_NAME}/log_server:${TAG}"
+gcloud docker -- push "us.gcr.io/${PROJECT_NAME}/log_signer:${TAG}"
+gcloud docker -- push "us.gcr.io/${PROJECT_NAME}/map_server:${TAG}"
 
 # Prepare configmap:
 kubectl delete configmap deploy-config
-kubectl create -f examples/deployment/kubernetes/trillian-mysql.yaml
-
-# Work-around for etcd-operator role on GKE:
-kubectl create clusterrolebinding myname-cluster-admin-binding --clusterrole=cluster-admin
+envsubst < examples/deployment/kubernetes/trillian-mysql.yaml | kubectl create -f -
 
 # Launch with kubernetes
-kubectl apply -f examples/deployment/kubernetes/.
+envsubst < examples/deployment/kubernetes/trillian-log-deployment.yaml | kubectl apply -f -
+envsubst < examples/deployment/kubernetes/trillian-log-service.yaml | kubectl apply -f -
+envsubst < examples/deployment/kubernetes/trillian-log-signer-deployment.yaml | kubectl apply -f -
+envsubst < examples/deployment/kubernetes/trillian-log-signer-service.yaml | kubectl apply -f -
 kubectl get all
 kubectl get services
 
--- scripts/deploy_gce_ci.sh
+++ scripts/deploy_gce_ci.sh
@@ -4,37 +4,39 @@
 #set -o nounset
 #set -o xtrace
 
-export PROJECT_NAME_CI=trillian-opensource-ci
-export CLUSTER_NAME_CI=trillian-opensource-ci
-export CLOUDSDK_COMPUTE_ZONE=us-central1-a
+export PROJECT_NAME=trillian-opensource-lowlatency
+export CLUSTER_NAME=trillian-opensource-lowlatency
+export REGION=us-central1
+export ZONE=us-central1-b
+export CONFIGMAP=trillian-opensource-ci.yaml
 
 
-gcloud --quiet config set project ${PROJECT_NAME_CI}
-gcloud --quiet config set container/cluster ${CLUSTER_NAME_CI}
-gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
-gcloud --quiet container clusters get-credentials ${CLUSTER_NAME_CI}
+gcloud --quiet config set project ${PROJECT_NAME}
+gcloud --quiet config set container/cluster ${CLUSTER_NAME}
+gcloud --quiet config set compute/zone ${ZONE}
+gcloud --quiet container clusters get-credentials ${CLUSTER_NAME}
 
 
 echo "Building docker images..."
 cd $GOPATH/src/github.com/google/trillian
-docker build --quiet -f examples/deployment/docker/log_server/Dockerfile -t gcr.io/${PROJECT_NAME_CI}/log_server:${TRAVIS_COMMIT} .
-docker build --quiet -f examples/deployment/docker/log_signer/Dockerfile -t gcr.io/${PROJECT_NAME_CI}/log_signer:${TRAVIS_COMMIT} .
+docker build --quiet -f examples/deployment/docker/log_server/Dockerfile -t gcr.io/${PROJECT_NAME}/log_server:${TRAVIS_COMMIT} .
+docker build --quiet -f examples/deployment/docker/log_signer/Dockerfile -t gcr.io/${PROJECT_NAME}/log_signer:${TRAVIS_COMMIT} .
 
 echo "Pushing docker images..."
-gcloud docker -- push gcr.io/${PROJECT_NAME_CI}/log_server:${TRAVIS_COMMIT}
-gcloud docker -- push gcr.io/${PROJECT_NAME_CI}/log_signer:${TRAVIS_COMMIT}
+gcloud docker -- push gcr.io/${PROJECT_NAME}/log_server:${TRAVIS_COMMIT}
+gcloud docker -- push gcr.io/${PROJECT_NAME}/log_signer:${TRAVIS_COMMIT}
 
 echo "Tagging docker images..."
-gcloud --quiet container images add-tag gcr.io/${PROJECT_NAME_CI}/log_server:${TRAVIS_COMMIT} gcr.io/${PROJECT_NAME_CI}/log_server:latest
-gcloud --quiet container images add-tag gcr.io/${PROJECT_NAME_CI}/log_signer:${TRAVIS_COMMIT} gcr.io/${PROJECT_NAME_CI}/log_signer:latest
+gcloud --quiet container images add-tag gcr.io/${PROJECT_NAME}/log_server:${TRAVIS_COMMIT} gcr.io/${PROJECT_NAME}/log_server:latest
+gcloud --quiet container images add-tag gcr.io/${PROJECT_NAME}/log_signer:${TRAVIS_COMMIT} gcr.io/${PROJECT_NAME}/log_signer:latest
 
 echo "Updating jobs..."
 kubectl delete configmap deploy-config
-kubectl create -f examples/deployment/kubernetes/trillian-opensource-ci.yaml
+envsubst < examples/deployment/kubernetes/${CONFIGMAP} | kubectl create -f -
 
-kubectl apply -f examples/deployment/kubernetes/trillian-log-deployment.yaml
-kubectl apply -f examples/deployment/kubernetes/trillian-log-service.yaml
-kubectl apply -f examples/deployment/kubernetes/trillian-log-signer-deployment.yaml
-kubectl apply -f examples/deployment/kubernetes/trillian-log-signer-service.yaml
-kubectl set image deployment/trillian-logserver-deployment trillian-logserver=gcr.io/${PROJECT_NAME_CI}/log_server:${TRAVIS_COMMIT}
-kubectl set image deployment/trillian-logsigner-deployment trillian-log-signer=gcr.io/${PROJECT_NAME_CI}/log_signer:${TRAVIS_COMMIT}
+envsubst < examples/deployment/kubernetes/trillian-log-deployment.yaml | kubectl apply -f -
+envsubst < examples/deployment/kubernetes/trillian-log-service.yaml | kubectl apply -f -
+envsubst < examples/deployment/kubernetes/trillian-log-signer-deployment.yaml | kubectl apply -f -
+envsubst < examples/deployment/kubernetes/trillian-log-signer-service.yaml | kubectl apply -f -
+kubectl set image deployment/trillian-logserver-deployment trillian-logserver=gcr.io/${PROJECT_NAME}/log_server:${TRAVIS_COMMIT}
+kubectl set image deployment/trillian-logsigner-deployment trillian-log-signer=gcr.io/${PROJECT_NAME}/log_signer:${TRAVIS_COMMIT}
