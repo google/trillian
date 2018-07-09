1b0de85bb63c4dd336533c3eb0e9043129a53c46
Marwan Al Jubeh <jubeh@google.com>
2018-07-09 17:14:18 +0100

    Increase the gometalinter deadline even more (again)

--- scripts/presubmit.sh
+++ scripts/presubmit.sh
@@ -122,7 +122,7 @@ main() {
       'have you installed github.com/alecthomas/gometalinter?' || exit 1
 
     echo 'running gometalinter'
-    gometalinter --config=gometalinter.json --deadline=5m ./...
+    gometalinter --config=gometalinter.json --deadline=9m ./...
   fi
 
   if [[ "${run_generate}" -eq 1 ]]; then
