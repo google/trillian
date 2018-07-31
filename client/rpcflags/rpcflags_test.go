4db83f465f8d68adc476a8e71d44f48dc5c41490
Marwan Al Jubeh <jubeh@google.com>
2018-07-31 12:34:01 +0100

    Add tests for the rpcflags module
    Because it is not possible to test equality on grpc.DialOption objects,
    the rpcflags tests confirm it is working properly by using it to connect
    to Trillian servers configured to work with the expected settings.
    
    In order to simplify the tests, and because they might be useful for
    other tests, a `setup` test module is introduced, which includes the
    following methods:
    
    - `TempFile`: Returns a new temporary file and a cleanup method to
      delete it, which should be called with `defer`.
    - `NewTLSCertificate`: Creates a random new TLS certificate that can be
      used in tests.

---
+++ client/rpcflags/rpcflags_test.go
@@ -0,0 +1,131 @@
+// Copyright 2018 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package rpcflags
+
+import (
+	"context"
+	"encoding/pem"
+	"flag"
+	"strings"
+	"testing"
+
+	"github.com/google/trillian"
+	"github.com/google/trillian/testonly/integration"
+	"github.com/google/trillian/testonly/setup"
+	"github.com/google/trillian/util/flagsaver"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+)
+
+func TestNewClientDialOptionsFromFlagsWithTLSCertFileNotSet(t *testing.T) {
+	// Set up Trillian servers
+	const numSequencers = 2
+	serverOpts := []grpc.ServerOption{}
+	clientOpts := []grpc.DialOption{grpc.WithInsecure()}
+	logEnv, err := integration.NewLogEnvWithGRPCOptions(context.Background(), numSequencers, serverOpts, clientOpts)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer logEnv.Close()
+
+	dialOpts, err := NewClientDialOptionsFromFlags()
+	if err != nil {
+		t.Errorf("Got an unexpected error: %v", err)
+	}
+
+	// Check that the returned dial options can be used to connect and make
+	// requests against the Trillian services created above.
+	conn, err := grpc.Dial(logEnv.Address, dialOpts...)
+	if err != nil {
+		t.Errorf("failed to dial %v: %v", logEnv.Address, err)
+	}
+	defer conn.Close()
+
+	adminClient := trillian.NewTrillianAdminClient(conn)
+	_, err = adminClient.ListTrees(context.Background(), &trillian.ListTreesRequest{})
+	if err != nil {
+		t.Errorf("failed to request trees from the Admin Server: %v", err)
+	}
+}
+
+func TestNewClientDialOptionsFromFlagsWithTLSCertFileMissing(t *testing.T) {
+	defer flagsaver.Save().Restore()
+	err := flag.Set("tls_cert_file", "/a/missing/file")
+	if err != nil {
+		t.Errorf("Failed to set flag: %v", err)
+	}
+
+	dialOpts, err := NewClientDialOptionsFromFlags()
+	if err == nil || !strings.Contains(err.Error(), "no such file or directory") {
+		t.Errorf("Expected to get an error due to the file not being found, instead got: %v", err)
+	}
+
+	if dialOpts != nil {
+		t.Errorf("Expected returned dialOpts to be nil, instead got: %v", dialOpts)
+	}
+}
+
+func TestNewClientDialOptionsFromFlagsWithTLSCertFileSet(t *testing.T) {
+	// Create new TLS certificates for the test services, and write the Client
+	// certificate to a file (so we can refer to it using the flag).
+	crtFile, cleanupCrtFile := setup.TempFile(t, "test.crt.")
+	defer cleanupCrtFile()
+
+	tlsCert := setup.NewTLSCertificate(t)
+
+	// Certificate file and client dial options.
+	err := pem.Encode(crtFile, &pem.Block{Type: "CERTIFICATE", Bytes: tlsCert.Certificate[0]})
+	if err != nil {
+		t.Fatalf("Failed to encode the test TLS certificate %v", err)
+	}
+	clientCreds, err := credentials.NewClientTLSFromFile(crtFile.Name(), "")
+	if err != nil {
+		t.Fatalf("Failed to get credentials: %v", err)
+	}
+
+	// Set up Trillian servers (with TLS enabled)
+	const numSequencers = 2
+	serverCreds := credentials.NewServerTLSFromCert(&tlsCert)
+	serverOpts := []grpc.ServerOption{grpc.Creds(serverCreds)}
+	clientOpts := []grpc.DialOption{grpc.WithTransportCredentials(clientCreds)}
+	logEnv, err := integration.NewLogEnvWithGRPCOptions(context.Background(), numSequencers, serverOpts, clientOpts)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer logEnv.Close()
+
+	// Set up the flag.
+	defer flagsaver.Save().Restore()
+	err = flag.Set("tls_cert_file", crtFile.Name())
+
+	dialOpts, err := NewClientDialOptionsFromFlags()
+	if err != nil {
+		t.Errorf("Got an unexpected error: %v", err)
+	}
+
+	// Check that the returned dial options can be used to connect and make
+	// requests against the Trillian services created above.
+	conn, err := grpc.Dial(logEnv.Address, dialOpts...)
+	if err != nil {
+		t.Errorf("failed to dial %v: %v", logEnv.Address, err)
+	}
+	defer conn.Close()
+
+	adminClient := trillian.NewTrillianAdminClient(conn)
+	_, err = adminClient.ListTrees(context.Background(), &trillian.ListTreesRequest{})
+	if err != nil {
+		t.Errorf("failed to request trees from the Admin Server: %v", err)
+	}
+}
---
+++ testonly/setup/tls.go
@@ -0,0 +1,66 @@
+// Copyright 2018 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package setup
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/pem"
+	"math/big"
+	"testing"
+	"time"
+)
+
+// NewTLSCertificate returns a random TLS Certificate for testing.
+func NewTLSCertificate(t *testing.T) tls.Certificate {
+	t.Helper()
+
+	priv, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		t.Errorf("failed to generate RSA key: %s", err)
+	}
+
+	privBytes := x509.MarshalPKCS1PrivateKey(priv)
+	key := pem.EncodeToMemory(&pem.Block{Type: "PRIVATE KEY", Bytes: privBytes})
+
+	// Generate public certificate.
+	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
+	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
+	if err != nil {
+		t.Errorf("failed to generate serial number: %s", err)
+	}
+
+	template := x509.Certificate{
+		SerialNumber: serialNumber,
+		NotBefore:    time.Time{},
+		NotAfter:     time.Now().Add(time.Hour),
+		DNSNames:     []string{"localhost"},
+	}
+	pubBytes, err := x509.CreateCertificate(rand.Reader, &template, &template, &priv.PublicKey, priv)
+	if err != nil {
+		t.Errorf("failed to generate TLS public certificate: %s", err)
+	}
+
+	crt := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: pubBytes})
+
+	c, err := tls.X509KeyPair(crt, key)
+	if err != nil {
+		t.Errorf("failed to parse the public/private key pair: %s", err)
+	}
+
+	return c
+}
