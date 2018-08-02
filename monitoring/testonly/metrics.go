ab187114836aa0b8c796a46608c3e35afa88832f
Pavel Kalinnikov <pkalinnikov@google.com>
2018-08-02 14:12:42 +0100

    Clean up monitoring tests

--- monitoring/testonly/metrics.go
+++ monitoring/testonly/metrics.go
@@ -44,25 +44,31 @@ func TestCounter(t *testing.T, factory monitoring.MetricFactory) {
 		},
 	}
 	for _, test := range tests {
-		counter := factory.NewCounter("test_"+test.name, "Test only", test.labelNames...)
-		if got, want := counter.Value(test.labelVals...), 0.0; got != want {
-			t.Errorf("Counter(test_%s)[%v].Value()=%v; want %v", test.name, test.labelVals, got, want)
-		}
-		counter.Inc(test.labelVals...)
-		if got, want := counter.Value(test.labelVals...), 1.0; got != want {
-			t.Errorf("Counter(test_%s)[%v].Value()=%v; want %v", test.name, test.labelVals, got, want)
-		}
-		counter.Add(2.5, test.labelVals...)
-		if got, want := counter.Value(test.labelVals...), 3.5; got != want {
-			t.Errorf("Counter(test_%s)[%v].Value()=%v; want %v", test.name, test.labelVals, got, want)
-		}
-		// Use an invalid number of labels.
-		libels := append(test.labelVals, "bogus")
-		counter.Add(10.0, libels...)
-		counter.Inc(libels...)
-		if got, want := counter.Value(libels...), 0.0; got != want {
-			t.Errorf("Counter(test_%s)[%v].Value()=%v; want %v", test.name, test.labelVals, got, want)
-		}
+		t.Run(test.name, func(t *testing.T) {
+			counter := factory.NewCounter(test.name, "Test only", test.labelNames...)
+			if got, want := counter.Value(test.labelVals...), 0.0; got != want {
+				t.Errorf("Counter[%v].Value()=%v; want %v", test.labelVals, got, want)
+			}
+			counter.Inc(test.labelVals...)
+			if got, want := counter.Value(test.labelVals...), 1.0; got != want {
+				t.Errorf("Counter[%v].Value()=%v; want %v", test.labelVals, got, want)
+			}
+			counter.Add(2.5, test.labelVals...)
+			if got, want := counter.Value(test.labelVals...), 3.5; got != want {
+				t.Errorf("Counter[%v].Value()=%v; want %v", test.labelVals, got, want)
+			}
+			// Use an invalid number of labels.
+			libels := append(test.labelVals, "bogus")
+			counter.Add(10.0, libels...)
+			counter.Inc(libels...)
+			if got, want := counter.Value(libels...), 0.0; got != want {
+				t.Errorf("Counter[%v].Value()=%v; want %v", libels, got, want)
+			}
+			// Check that the value hasn't changed.
+			if got, want := counter.Value(test.labelVals...), 3.5; got != want {
+				t.Errorf("Counter[%v].Value()=%v; want %v", test.labelVals, got, want)
+			}
+		})
 	}
 }
 
@@ -90,36 +96,42 @@ func TestGauge(t *testing.T, factory monitoring.MetricFactory) {
 		},
 	}
 	for _, test := range tests {
-		gauge := factory.NewGauge("test_"+test.name, "Test only", test.labelNames...)
-		if got, want := gauge.Value(test.labelVals...), 0.0; got != want {
-			t.Errorf("Gauge(test_%s)[%v].Value()=%v; want %v", test.name, test.labelVals, got, want)
-		}
-		gauge.Inc(test.labelVals...)
-		if got, want := gauge.Value(test.labelVals...), 1.0; got != want {
-			t.Errorf("Gauge(test_%s)[%v].Value()=%v; want %v", test.name, test.labelVals, got, want)
-		}
-		gauge.Dec(test.labelVals...)
-		if got, want := gauge.Value(test.labelVals...), 0.0; got != want {
-			t.Errorf("Gauge(test_%s)[%v].Value()=%v; want %v", test.name, test.labelVals, got, want)
-		}
-		gauge.Add(2.5, test.labelVals...)
-		if got, want := gauge.Value(test.labelVals...), 2.5; got != want {
-			t.Errorf("Gauge(test_%s)[%v].Value()=%v; want %v", test.name, test.labelVals, got, want)
-		}
-		gauge.Set(42.0, test.labelVals...)
-		if got, want := gauge.Value(test.labelVals...), 42.0; got != want {
-			t.Errorf("Gauge(test_%s)[%v].Value()=%v; want %v", test.name, test.labelVals, got, want)
-		}
-		// Use an invalid number of labels.
-		libels := append(test.labelVals, "bogus")
-		gauge.Add(10.0, libels...)
-		gauge.Inc(libels...)
-		gauge.Dec(libels...)
-		gauge.Set(120.0, libels...)
-		// Ask for an invalid number of labels.
-		if got, want := gauge.Value(libels...), 0.0; got != want {
-			t.Errorf("Counter(test_%s)[%v].Value()=%v; want %v", test.name, test.labelVals, got, want)
-		}
+		t.Run(test.name, func(t *testing.T) {
+			gauge := factory.NewGauge(test.name, "Test only", test.labelNames...)
+			if got, want := gauge.Value(test.labelVals...), 0.0; got != want {
+				t.Errorf("Gauge[%v].Value()=%v; want %v", test.labelVals, got, want)
+			}
+			gauge.Inc(test.labelVals...)
+			if got, want := gauge.Value(test.labelVals...), 1.0; got != want {
+				t.Errorf("Gauge[%v].Value()=%v; want %v", test.labelVals, got, want)
+			}
+			gauge.Dec(test.labelVals...)
+			if got, want := gauge.Value(test.labelVals...), 0.0; got != want {
+				t.Errorf("Gauge[%v].Value()=%v; want %v", test.labelVals, got, want)
+			}
+			gauge.Add(2.5, test.labelVals...)
+			if got, want := gauge.Value(test.labelVals...), 2.5; got != want {
+				t.Errorf("Gauge[%v].Value()=%v; want %v", test.labelVals, got, want)
+			}
+			gauge.Set(42.0, test.labelVals...)
+			if got, want := gauge.Value(test.labelVals...), 42.0; got != want {
+				t.Errorf("Gauge[%v].Value()=%v; want %v", test.labelVals, got, want)
+			}
+			// Use an invalid number of labels.
+			libels := append(test.labelVals, "bogus")
+			gauge.Add(10.0, libels...)
+			gauge.Inc(libels...)
+			gauge.Dec(libels...)
+			gauge.Set(120.0, libels...)
+			// Ask for an invalid number of labels.
+			if got, want := gauge.Value(libels...), 0.0; got != want {
+				t.Errorf("Gauge[%v].Value()=%v; want %v", libels, got, want)
+			}
+			// Check that the value hasn't changed.
+			if got, want := gauge.Value(test.labelVals...), 42.0; got != want {
+				t.Errorf("Gauge[%v].Value()=%v; want %v", test.labelVals, got, want)
+			}
+		})
 	}
 }
 
@@ -147,26 +159,33 @@ func TestHistogram(t *testing.T, factory monitoring.MetricFactory) {
 		},
 	}
 	for _, test := range tests {
-		histogram := factory.NewHistogram("test_"+test.name, "Test only", test.labelNames...)
-		gotCount, gotSum := histogram.Info(test.labelVals...)
-		if wantCount, wantSum := uint64(0), 0.0; gotCount != wantCount || gotSum != wantSum {
-			t.Errorf("Gauge(test_%s)[%v].Value()=%v,%v; want %v,%v", test.name, test.labelVals, gotCount, gotSum, wantCount, wantSum)
-		}
-		histogram.Observe(1.0, test.labelVals...)
-		histogram.Observe(2.0, test.labelVals...)
-		histogram.Observe(3.0, test.labelVals...)
-		gotCount, gotSum = histogram.Info(test.labelVals...)
-		if wantCount, wantSum := uint64(3), 6.0; gotCount != wantCount || gotSum != wantSum {
-			t.Errorf("Gauge(test_%s)[%v].Value()=%v,%v; want %v,%v", test.name, test.labelVals, gotCount, gotSum, wantCount, wantSum)
-		}
+		t.Run(test.name, func(t *testing.T) {
+			histogram := factory.NewHistogram(test.name, "Test only", test.labelNames...)
+			gotCount, gotSum := histogram.Info(test.labelVals...)
+			if wantCount, wantSum := uint64(0), 0.0; gotCount != wantCount || gotSum != wantSum {
+				t.Errorf("Histogram[%v].Info()=%v,%v; want %v,%v", test.labelVals, gotCount, gotSum, wantCount, wantSum)
+			}
+			histogram.Observe(1.0, test.labelVals...)
+			histogram.Observe(2.0, test.labelVals...)
+			histogram.Observe(3.0, test.labelVals...)
+			gotCount, gotSum = histogram.Info(test.labelVals...)
+			if wantCount, wantSum := uint64(3), 6.0; gotCount != wantCount || gotSum != wantSum {
+				t.Errorf("Histogram[%v].Info()=%v,%v; want %v,%v", test.labelVals, gotCount, gotSum, wantCount, wantSum)
+			}
 
-		// Use an invalid number of labels.
-		libels := append(test.labelVals, "bogus")
-		histogram.Observe(100.0, libels...)
-		histogram.Observe(200.0, libels...)
-		gotCount, gotSum = histogram.Info(libels...)
-		if wantCount, wantSum := uint64(0), 0.0; gotCount != wantCount || gotSum != wantSum {
-			t.Errorf("Gauge(test_%s)[%v].Value()=%v,%v; want %v,%v", test.name, test.labelVals, gotCount, gotSum, wantCount, wantSum)
-		}
+			// Use an invalid number of labels.
+			libels := append(test.labelVals, "bogus")
+			histogram.Observe(100.0, libels...)
+			histogram.Observe(200.0, libels...)
+			gotCount, gotSum = histogram.Info(libels...)
+			if wantCount, wantSum := uint64(0), 0.0; gotCount != wantCount || gotSum != wantSum {
+				t.Errorf("Histogram[%v].Info()=%v,%v; want %v,%v", libels, gotCount, gotSum, wantCount, wantSum)
+			}
+			// Check that the histogram hasn't changed.
+			gotCount, gotSum = histogram.Info(test.labelVals...)
+			if wantCount, wantSum := uint64(3), 6.0; gotCount != wantCount || gotSum != wantSum {
+				t.Errorf("Histogram[%v].Info()=%v,%v; want %v,%v", test.labelVals, gotCount, gotSum, wantCount, wantSum)
+			}
+		})
 	}
 }
