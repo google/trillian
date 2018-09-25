0d9576ecbb70454200f9d27d859638ff1a85306d
Rob Percival <robpercival@google.com>
2018-09-25 16:58:50 +0100

    Add tests for ExecutePass failing in log_operation_manager_test.go (#1274)
    * Add tests for ExecutePass failing in log_operation_manager_test.go
    
    Checks that monitoring counters are updated correctly. This improves
    test coverage, since the tests previously had all calls to ExecutePass()
    return (0, nil), which only exercised a single code path around
    ExecutePass() calls (the one that handled ExecutePass() doing nothing).
    
    In order to check the monitoring counters, this commit adds a
    CounterSnapshot struct that can be used to examine the delta between
    counter values at different points in a test. This is particularly
    useful since counters are package variables which do not get reset between
    tests.

---
+++ monitoring/testonly/delta.go
@@ -0,0 +1,50 @@
+// Copyright 2018 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package testonly
+
+import (
+	"github.com/google/trillian/monitoring"
+)
+
+// CounterSnapshot records the latest value from a time series in a counter.
+// This value can then be compared with future values. Note that a counter can
+// contain many time series, but a CounterSnapshot will track only one.
+// A CounterSnapshot is useful in tests because counters do not reset between
+// test cases, and so their absolute value is not amenable to testing. Instead,
+// the delta between the value at the start and end of the test should be used.
+// This assumes that multiple tests that all affect the counter are not run in
+// parallel.
+type CounterSnapshot struct {
+	c      monitoring.Counter
+	labels []string
+	value  float64
+}
+
+// NewCounterSnapshot records the latest value of a time series in c identified
+// by the given labels. This value can be compared to future values to determine
+// how it has changed over time.
+func NewCounterSnapshot(c monitoring.Counter, labels ...string) CounterSnapshot {
+	return CounterSnapshot{
+		c:      c,
+		labels: labels,
+		value:  c.Value(labels...),
+	}
+}
+
+// Delta returns the difference between the latest value of the time series
+// and the value when the CounterSnapshot was created.
+func (s CounterSnapshot) Delta() float64 {
+	return s.c.Value(s.labels...) - s.value
+}
