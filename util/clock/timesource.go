67aa8d2852b90ea86d0bb267432502744aab7397
Pavel Kalinnikov <pkalinnikov@google.com>
2018-12-13 13:53:42 +0000

    Add package comment

--- util/clock/timesource.go
+++ util/clock/timesource.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package clock contains time utilities, and types that allow mocking system
+// time in tests.
 package clock
 
 import (
