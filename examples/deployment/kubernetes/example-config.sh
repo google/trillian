562021b7595cd90373934dc483d009f735c2dc79
Rob Percival <robpercival@google.com>
2019-01-31 16:08:57 +0000

    Fix Kubernetes README to not refer to non-existent config.sh file
    Add an example-config.sh file and refer to that.

---
+++ examples/deployment/kubernetes/example-config.sh
@@ -0,0 +1,15 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Set the following variables to the correct values for your Google Cloud
+# project and Kubernetes cluster.
+export PROJECT_ID="GOOGLE CLOUD PROJECT ID HERE (https://cloud.google.com/resource-manager/docs/creating-managing-projects#identifying_projects)"
+export CLUSTER_NAME=trillian
+export REGION=us-east4
+export MASTER_ZONE="${REGION}-a"
+export NODE_LOCATIONS="${REGION}-a,${REGION}-b,${REGION}-c"
+export CONFIGMAP=${DIR}/trillian-cloudspanner.yaml
+
+export POOLSIZE=2
+export MACHINE_TYPE="n1-standard-2"
+
+export RUN_MAP=false
