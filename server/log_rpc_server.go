0f7e057ee9aae467d32823e4032142e11e0a9b2c
Martin Smith <mhs@google.com>
2019-03-19 09:16:16 +0000

    In GetLeavesByIndex in RPC server make sure root unmarshalls without error

--- server/log_rpc_server.go
+++ server/log_rpc_server.go
@@ -485,12 +485,16 @@ func (t *TrillianLogRPCServer) GetLeavesByIndex(ctx context.Context, req *trilli
 		return nil, err
 	}
 
-	root, err := tx.LatestSignedLogRoot(ctx)
+	slr, err := tx.LatestSignedLogRoot(ctx)
 	if err != nil {
 		return nil, err
 	}
+	var root types.LogRootV1
+	if err := root.UnmarshalBinary(slr.LogRoot); err != nil {
+		return nil, status.Errorf(codes.Internal, "Could not read current log root: %v", err)
+	}
 
-	return &trillian.GetLeavesByIndexResponse{Leaves: leaves, SignedLogRoot: &root}, nil
+	return &trillian.GetLeavesByIndexResponse{Leaves: leaves, SignedLogRoot: &slr}, nil
 }
 
 // GetLeavesByRange obtains leaves based on a range of sequence numbers within the tree.
