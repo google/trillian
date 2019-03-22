05b74af79bb013abb54b959954fbfdad8ee4e8a2
Kat Joyce <katjoyce@google.com>
2019-03-22 13:24:44 +0000

    Fix create.sh to use config file provided. (#1466)
    create.sh was still using config.sh, which has been deleted.  Update it
    to take in a config file name as a command-line argument.
    
    Update the README to reflect this.

--- examples/deployment/kubernetes/create.sh
+++ examples/deployment/kubernetes/create.sh
@@ -3,10 +3,28 @@
 # This script (optionally) creates and then prepares a Google Cloud project to host a
 # Trillian instance using Kubernetes.
 
+function checkEnv() {
+  if [ -z ${PROJECT_ID+x} ] ||
+     [ -z ${CLUSTER_NAME+x} ] ||
+     [ -z ${REGION+x} ] ||
+     [ -z ${NODE_LOCATIONS+x} ] ||
+     [ -z ${MASTER_ZONE+x} ] ||
+     [ -z ${CONFIGMAP+x} ] ||
+     [ -z ${POOLSIZE+x} ] ||
+     [ -z ${MACHINE_TYPE+x} ]; then
+    echo "You must either pass an argument which is a config file, or set all the required environment variables" >&2
+    exit 1
+  fi
+}
+
 set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source ${DIR}/config.sh
+if [ $# -eq 1 ]; then
+  source $1
+else
+  checkEnv
+fi
 
 # Check required binaries are installed
 if ! gcloud --help > /dev/null; then
