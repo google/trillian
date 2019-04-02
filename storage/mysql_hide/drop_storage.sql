c0eabf281804cb068b7b060eb19c7a09e88b19ae
Ubuntu <bigjim@CTDev.k0sbcohsd5runncen0xdepekhd.dx.internal.cloudapp.net>
2019-04-02 19:22:39 +0000

    Re-added renamed mysql directory

---
+++ storage/mysql_hide/drop_storage.sql
@@ -0,0 +1,13 @@
+-- Caution - this removes all tables in our schema
+
+DROP TABLE IF EXISTS Unsequenced;
+DROP TABLE IF EXISTS Subtree;
+DROP TABLE IF EXISTS SequencedLeafData;
+DROP TABLE IF EXISTS TreeHead;
+DROP TABLE IF EXISTS LeafData;
+DROP TABLE IF EXISTS MapLeaf;
+DROP TABLE IF EXISTS MapHead;
+DROP TABLE IF EXISTS TreeControl;
+DROP TABLE IF EXISTS MapHead;
+DROP TABLE IF EXISTS MapLeaf;
+DROP TABLE IF EXISTS Trees;
