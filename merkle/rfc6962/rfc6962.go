cebe9e0c152ee733a2c284f05de9881d2498c193
Al Cutter <al@google.com>
2019-04-09 15:16:45 +0100

    Pre-build buffer to hash

--- merkle/rfc6962/rfc6962.go
+++ merkle/rfc6962/rfc6962.go
@@ -60,12 +60,26 @@ func (t *Hasher) HashLeaf(leaf []byte) ([]byte, error) {
 	return h.Sum(nil), nil
 }
 
-// HashChildren returns the inner Merkle tree node hash of the the two child nodes l and r.
+// HashChildrenOld returns the inner Merkle tree node hash of the the two child nodes l and r.
 // The hashed structure is NodeHashPrefix||l||r.
-func (t *Hasher) HashChildren(l, r []byte) []byte {
+func (t *Hasher) HashChildrenOld(l, r []byte) []byte {
 	h := t.New()
 	h.Write([]byte{RFC6962NodeHashPrefix})
 	h.Write(l)
 	h.Write(r)
+	return h.Sum(nil)
+}
+
+// HashChildren returns the inner Merkle tree node hash of the the two child nodes l and r.
+// The hashed structure is NodeHashPrefix||l||r.
+func (t *Hasher) HashChildren(l, r []byte) []byte {
+	h := t.New()
+	b := append(append(append(
+		make([]byte, 0, 1+len(l)+len(r)),
+		RFC6962NodeHashPrefix),
+		l...),
+		r...)
+
+	h.Write(b)
 	return h.Sum(nil)
 }
