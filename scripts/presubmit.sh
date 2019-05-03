a21a290ae1380d68680caf6ccba84f13f88a8248
Gary Belvin <gdb@google.com>
2019-05-03 15:11:54 +0100

    Don't auto-install

--- scripts/presubmit.sh
+++ scripts/presubmit.sh
@@ -112,13 +112,8 @@ main() {
   if [[ "${run_lint}" -eq 1 ]]; then
     check_cmd golangci-lint \
       'have you installed github.com/golangci/golangci-lint?' || exit 1
-    if ! type -p "prototool" > /dev/null; then
-      TMP="$(mktemp -d)"
-      trap 'rm -rf "${TMP}"' EXIT
-      cd "${TMP}"
-      GO111MODULE=on go get github.com/uber/prototool/cmd/prototool@v1.6
-      cd -
-    fi
+    check_cmd prototool-lint \
+      'have you installed github.com/uber/prototool/cmd/prototool?' || exit 1
 
     echo 'running golangci-lint'
     golangci-lint run
