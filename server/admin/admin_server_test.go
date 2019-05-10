d80b158968f5872afeea15b625538c00d770f997
Jim Nolan <bigjimnolan@hotmail.com>
2019-05-10 15:35:25 +0000

    will this pass the lint check?

--- server/admin/admin_server_test.go
+++ server/admin/admin_server_test.go
@@ -810,9 +810,9 @@ func TestServer_DeleteTreeErrors(t *testing.T) {
 	for _, test := range tests {
 		setup := setupAdminServer(
 			ctrl,
-			nil,   // keygen */
-			false, // snapshot */
-			test.deleteErr == nil, /* shouldCommit */
+			nil,   // keygen 
+			false, // snapshot 
+			test.deleteErr == nil, // shouldCommit
 			test.commitErr)
 		req := &trillian.DeleteTreeRequest{TreeId: 10}
 
