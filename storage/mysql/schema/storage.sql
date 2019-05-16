b676adf1ad3ed26ca7578fd3fa3872584ada775f
Rob Percival <robpercival@google.com>
2019-05-16 12:32:33 +0100

    Format storage/mysql/schema/storage.sql using `skeema lint`
    I had to manually replace "CREATE [UNIQUE] INDEX" statements with
    "[UNIQUE] KEY" options within the "CREATE TABLE" statements, because
    Skeema doesn't support the former.
    
    I've made the comments on columns/indices into actual column/index
    comments, because Skeema deletes comments within "CREATE TABLE" statements.
    This makes them a little harder to read in the file, but will surface them
    through tools that read the live database schema. A word of warning is
    that Skeema will delete and recreate the primary key of the Subtree table
    in order to add a comment to it, which could take a while if the table
    has a lot of rows.
    
    Skeema deletes the "IF NOT EXISTS" part of the "CREATE TABLE" statements
    because it doesn't support them. This should be fine if we switch to using
    Skeema, because it handles this functionality (creating or altering tables
    as necessary).

--- storage/mysql/schema/storage.sql
+++ storage/mysql/schema/storage.sql
@@ -6,62 +6,58 @@
 
 -- Tree parameters should not be changed after creation. Doing so can
 -- render the data in the tree unusable or inconsistent.
-CREATE TABLE IF NOT EXISTS Trees(
-  TreeId                BIGINT NOT NULL,
-  TreeState             ENUM('ACTIVE', 'FROZEN', 'DRAINING') NOT NULL,
-  TreeType              ENUM('LOG', 'MAP', 'PREORDERED_LOG') NOT NULL,
-  HashStrategy          ENUM('RFC6962_SHA256', 'TEST_MAP_HASHER', 'OBJECT_RFC6962_SHA256', 'CONIKS_SHA512_256', 'CONIKS_SHA256') NOT NULL,
-  HashAlgorithm         ENUM('SHA256') NOT NULL,
-  SignatureAlgorithm    ENUM('ECDSA', 'RSA') NOT NULL,
-  DisplayName           VARCHAR(20),
-  Description           VARCHAR(200),
-  CreateTimeMillis      BIGINT NOT NULL,
-  UpdateTimeMillis      BIGINT NOT NULL,
-  MaxRootDurationMillis BIGINT NOT NULL,
-  PrivateKey            MEDIUMBLOB NOT NULL,
-  PublicKey             MEDIUMBLOB NOT NULL,
-  Deleted               BOOLEAN,
-  DeleteTimeMillis      BIGINT,
-  PRIMARY KEY(TreeId)
-);
+CREATE TABLE `Trees` (
+  `TreeId` bigint(20) NOT NULL,
+  `TreeState` enum('ACTIVE','FROZEN','DRAINING') NOT NULL,
+  `TreeType` enum('LOG','MAP','PREORDERED_LOG') NOT NULL,
+  `HashStrategy` enum('RFC6962_SHA256','TEST_MAP_HASHER','OBJECT_RFC6962_SHA256','CONIKS_SHA512_256','CONIKS_SHA256') NOT NULL,
+  `HashAlgorithm` enum('SHA256') NOT NULL,
+  `SignatureAlgorithm` enum('ECDSA','RSA') NOT NULL,
+  `DisplayName` varchar(20) DEFAULT NULL,
+  `Description` varchar(200) DEFAULT NULL,
+  `CreateTimeMillis` bigint(20) NOT NULL,
+  `UpdateTimeMillis` bigint(20) NOT NULL,
+  `MaxRootDurationMillis` bigint(20) NOT NULL,
+  `PrivateKey` mediumblob NOT NULL,
+  `PublicKey` mediumblob NOT NULL,
+  `Deleted` tinyint(1) DEFAULT NULL,
+  `DeleteTimeMillis` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`TreeId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- This table contains tree parameters that can be changed at runtime such as for
 -- administrative purposes.
-CREATE TABLE IF NOT EXISTS TreeControl(
-  TreeId                  BIGINT NOT NULL,
-  SigningEnabled          BOOLEAN NOT NULL,
-  SequencingEnabled       BOOLEAN NOT NULL,
-  SequenceIntervalSeconds INTEGER NOT NULL,
-  PRIMARY KEY(TreeId),
-  FOREIGN KEY(TreeId) REFERENCES Trees(TreeId) ON DELETE CASCADE
-);
+CREATE TABLE `TreeControl` (
+  `TreeId` bigint(20) NOT NULL,
+  `SigningEnabled` tinyint(1) NOT NULL,
+  `SequencingEnabled` tinyint(1) NOT NULL,
+  `SequenceIntervalSeconds` int(11) NOT NULL,
+  PRIMARY KEY (`TreeId`),
+  CONSTRAINT `TreeControl_ibfk_1` FOREIGN KEY (`TreeId`) REFERENCES `Trees` (`TreeId`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS Subtree(
-  TreeId               BIGINT NOT NULL,
-  SubtreeId            VARBINARY(255) NOT NULL,
-  Nodes                MEDIUMBLOB NOT NULL,
-  SubtreeRevision      INTEGER NOT NULL,
-  -- Key columns must be in ASC order in order to benefit from group-by/min-max
-  -- optimization in MySQL.
-  PRIMARY KEY(TreeId, SubtreeId, SubtreeRevision),
-  FOREIGN KEY(TreeId) REFERENCES Trees(TreeId) ON DELETE CASCADE
-);
+CREATE TABLE `Subtree` (
+  `TreeId` bigint(20) NOT NULL,
+  `SubtreeId` varbinary(255) NOT NULL,
+  `Nodes` mediumblob NOT NULL,
+  `SubtreeRevision` int(11) NOT NULL,
+  PRIMARY KEY (`TreeId`,`SubtreeId`,`SubtreeRevision`) COMMENT 'Key columns must be in ASC order in order to benefit from group-by/min-max optimization in MySQL.',
+  CONSTRAINT `Subtree_ibfk_1` FOREIGN KEY (`TreeId`) REFERENCES `Trees` (`TreeId`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- The TreeRevisionIdx is used to enforce that there is only one STH at any
 -- tree revision
-CREATE TABLE IF NOT EXISTS TreeHead(
-  TreeId               BIGINT NOT NULL,
-  TreeHeadTimestamp    BIGINT,
-  TreeSize             BIGINT,
-  RootHash             VARBINARY(255) NOT NULL,
-  RootSignature        VARBINARY(1024) NOT NULL,
-  TreeRevision         BIGINT,
-  PRIMARY KEY(TreeId, TreeHeadTimestamp),
-  FOREIGN KEY(TreeId) REFERENCES Trees(TreeId) ON DELETE CASCADE
-);
-
-CREATE UNIQUE INDEX TreeHeadRevisionIdx
-  ON TreeHead(TreeId, TreeRevision);
+CREATE TABLE `TreeHead` (
+  `TreeId` bigint(20) NOT NULL,
+  `TreeHeadTimestamp` bigint(20) NOT NULL,
+  `TreeSize` bigint(20) DEFAULT NULL,
+  `RootHash` varbinary(255) NOT NULL,
+  `RootSignature` varbinary(1024) NOT NULL,
+  `TreeRevision` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`TreeId`,`TreeHeadTimestamp`),
+  UNIQUE KEY `TreeHeadRevisionIdx` (`TreeId`,`TreeRevision`),
+  CONSTRAINT `TreeHead_ibfk_1` FOREIGN KEY (`TreeId`) REFERENCES `Trees` (`TreeId`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- ---------------------------------------------
 -- Log specific stuff here
@@ -73,23 +69,15 @@ CREATE UNIQUE INDEX TreeHeadRevisionIdx
 
 -- A leaf that has not been sequenced has a row in this table. If duplicate leaves
 -- are allowed they will all reference this row.
-CREATE TABLE IF NOT EXISTS LeafData(
-  TreeId               BIGINT NOT NULL,
-  -- This is a personality specific has of some subset of the leaf data.
-  -- It's only purpose is to allow Trillian to identify duplicate entries in
-  -- the context of the personality.
-  LeafIdentityHash     VARBINARY(255) NOT NULL,
-  -- This is the data stored in the leaf for example in CT it contains a DER encoded
-  -- X.509 certificate but is application dependent
-  LeafValue            LONGBLOB NOT NULL,
-  -- This is extra data that the application can associate with the leaf should it wish to.
-  -- This data is not included in signing and hashing.
-  ExtraData            LONGBLOB,
-  -- The timestamp from when this leaf data was first queued for inclusion.
-  QueueTimestampNanos  BIGINT NOT NULL,
-  PRIMARY KEY(TreeId, LeafIdentityHash),
-  FOREIGN KEY(TreeId) REFERENCES Trees(TreeId) ON DELETE CASCADE
-);
+CREATE TABLE `LeafData` (
+  `TreeId` bigint(20) NOT NULL,
+  `LeafIdentityHash` varbinary(255) NOT NULL COMMENT 'This is a personality specific hash of some subset of the leaf data. It''s only purpose is to allow Trillian to identify duplicate entries in the context of the personality.',
+  `LeafValue` longblob NOT NULL COMMENT 'This is the data stored in the leaf for example in CT it contains a DER encoded X.509 certificate but is application dependent.',
+  `ExtraData` longblob COMMENT 'This is extra data that the application can associate with the leaf should it wish to. This data is not included in signing and hashing.',
+  `QueueTimestampNanos` bigint(20) NOT NULL COMMENT 'The timestamp from when this leaf data was first queued for inclusion.',
+  PRIMARY KEY (`TreeId`,`LeafIdentityHash`),
+  CONSTRAINT `LeafData_ibfk_1` FOREIGN KEY (`TreeId`) REFERENCES `Trees` (`TreeId`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- When a leaf is sequenced a row is added to this table. If logs allow duplicates then
 -- multiple rows will exist with different sequence numbers. The signed timestamp
@@ -97,73 +85,55 @@ CREATE TABLE IF NOT EXISTS LeafData(
 -- on the log parameters and we can't insert into this table until we have the sequence number
 -- which is not available at the time we queue the entry. We need both hashes because the
 -- LeafData table is keyed by the raw data hash.
-CREATE TABLE IF NOT EXISTS SequencedLeafData(
-  TreeId               BIGINT NOT NULL,
-  SequenceNumber       BIGINT UNSIGNED NOT NULL,
-  -- This is a personality specific has of some subset of the leaf data.
-  -- It's only purpose is to allow Trillian to identify duplicate entries in
-  -- the context of the personality.
-  LeafIdentityHash     VARBINARY(255) NOT NULL,
-  -- This is a MerkleLeafHash as defined by the treehasher that the log uses. For example for
-  -- CT this hash will include the leaf prefix byte as well as the leaf data.
-  MerkleLeafHash       VARBINARY(255) NOT NULL,
-  IntegrateTimestampNanos BIGINT NOT NULL,
-  PRIMARY KEY(TreeId, SequenceNumber),
-  FOREIGN KEY(TreeId) REFERENCES Trees(TreeId) ON DELETE CASCADE,
-  FOREIGN KEY(TreeId, LeafIdentityHash) REFERENCES LeafData(TreeId, LeafIdentityHash) ON DELETE CASCADE
-);
+CREATE TABLE `SequencedLeafData` (
+  `TreeId` bigint(20) NOT NULL,
+  `SequenceNumber` bigint(20) unsigned NOT NULL,
+  `LeafIdentityHash` varbinary(255) NOT NULL COMMENT 'This is a personality specific has of some subset of the leaf data. It''s only purpose is to allow Trillian to identify duplicate entries in the context of the personality.',
+  `MerkleLeafHash` varbinary(255) NOT NULL COMMENT 'This is a MerkleLeafHash as defined by the treehasher that the log uses. For example for CT this hash will include the leaf prefix byte as well as the leaf data.',
+  `IntegrateTimestampNanos` bigint(20) NOT NULL,
+  PRIMARY KEY (`TreeId`,`SequenceNumber`),
+  KEY `TreeId` (`TreeId`,`LeafIdentityHash`),
+  KEY `SequencedLeafMerkleIdx` (`TreeId`,`MerkleLeafHash`),
+  CONSTRAINT `SequencedLeafData_ibfk_1` FOREIGN KEY (`TreeId`) REFERENCES `Trees` (`TreeId`) ON DELETE CASCADE,
+  CONSTRAINT `SequencedLeafData_ibfk_2` FOREIGN KEY (`TreeId`, `LeafIdentityHash`) REFERENCES `LeafData` (`TreeId`, `LeafIdentityHash`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
-CREATE INDEX SequencedLeafMerkleIdx
-  ON SequencedLeafData(TreeId, MerkleLeafHash);
-
-CREATE TABLE IF NOT EXISTS Unsequenced(
-  TreeId               BIGINT NOT NULL,
-  -- The bucket field is to allow the use of time based ring bucketed schemes if desired. If
-  -- unused this should be set to zero for all entries.
-  Bucket               INTEGER NOT NULL,
-  -- This is a personality specific hash of some subset of the leaf data.
-  -- It's only purpose is to allow Trillian to identify duplicate entries in
-  -- the context of the personality.
-  LeafIdentityHash     VARBINARY(255) NOT NULL,
-  -- This is a MerkleLeafHash as defined by the treehasher that the log uses. For example for
-  -- CT this hash will include the leaf prefix byte as well as the leaf data.
-  MerkleLeafHash       VARBINARY(255) NOT NULL,
-  QueueTimestampNanos  BIGINT NOT NULL,
-  -- This is a SHA256 hash of the TreeID, LeafIdentityHash and QueueTimestampNanos. It is used
-  -- for batched deletes from the table when trillian_log_server and trillian_log_signer are
-  -- built with the batched_queue tag.
-  QueueID VARBINARY(32) DEFAULT NULL UNIQUE,
-  PRIMARY KEY (TreeId, Bucket, QueueTimestampNanos, LeafIdentityHash)
-);
+CREATE TABLE `Unsequenced` (
+  `TreeId` bigint(20) NOT NULL,
+  `Bucket` int(11) NOT NULL COMMENT 'The bucket field is to allow the use of time based ring bucketed schemes if desired. If unused this should be set to zero for all entries.',
+  `LeafIdentityHash` varbinary(255) NOT NULL COMMENT 'This is a personality specific hash of some subset of the leaf data. It''s only purpose is to allow Trillian to identify duplicate entries in the context of the personality.',
+  `MerkleLeafHash` varbinary(255) NOT NULL COMMENT 'This is a MerkleLeafHash as defined by the treehasher that the log uses. For example for CT this hash will include the leaf prefix byte as well as the leaf data.',
+  `QueueTimestampNanos` bigint(20) NOT NULL,
+  `QueueID` varbinary(32) DEFAULT NULL COMMENT 'This is a SHA256 hash of the TreeID, LeafIdentityHash and QueueTimestampNanos. It is used for batched deletes from the table when trillian_log_server and trillian_log_signer are built with the batched_queue tag.',
+  PRIMARY KEY (`TreeId`,`Bucket`,`QueueTimestampNanos`,`LeafIdentityHash`),
+  UNIQUE KEY `QueueID` (`QueueID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
 -- ---------------------------------------------
 -- Map specific stuff here
 -- ---------------------------------------------
 
-CREATE TABLE IF NOT EXISTS MapLeaf(
-  TreeId                BIGINT NOT NULL,
-  KeyHash               VARBINARY(255) NOT NULL,
-  -- MapRevision is stored negated to invert ordering in the primary key index
-  -- st. more recent revisions come first.
-  MapRevision           BIGINT NOT NULL,
-  LeafValue             LONGBLOB NOT NULL,
-  PRIMARY KEY(TreeId, KeyHash, MapRevision),
-  FOREIGN KEY(TreeId) REFERENCES Trees(TreeId) ON DELETE CASCADE
-);
+CREATE TABLE `MapLeaf` (
+  `TreeId` bigint(20) NOT NULL,
+  `KeyHash` varbinary(255) NOT NULL,
+  `MapRevision` bigint(20) NOT NULL COMMENT 'MapRevision is stored negated to invert ordering in the primary key index st. more recent revisions come first.',
+  `LeafValue` longblob NOT NULL,
+  PRIMARY KEY (`TreeId`,`KeyHash`,`MapRevision`),
+  CONSTRAINT `MapLeaf_ibfk_1` FOREIGN KEY (`TreeId`) REFERENCES `Trees` (`TreeId`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
-CREATE TABLE IF NOT EXISTS MapHead(
-  TreeId               BIGINT NOT NULL,
-  MapHeadTimestamp     BIGINT,
-  RootHash             VARBINARY(255) NOT NULL,
-  MapRevision          BIGINT,
-  RootSignature        VARBINARY(1024) NOT NULL,
-  MapperData           MEDIUMBLOB,
-  PRIMARY KEY(TreeId, MapHeadTimestamp),
-  FOREIGN KEY(TreeId) REFERENCES Trees(TreeId) ON DELETE CASCADE
-);
+CREATE TABLE `MapHead` (
+  `TreeId` bigint(20) NOT NULL,
+  `MapHeadTimestamp` bigint(20) NOT NULL,
+  `RootHash` varbinary(255) NOT NULL,
+  `MapRevision` bigint(20) DEFAULT NULL,
+  `RootSignature` varbinary(1024) NOT NULL,
+  `MapperData` mediumblob,
+  PRIMARY KEY (`TreeId`,`MapHeadTimestamp`),
+  UNIQUE KEY `MapHeadRevisionIdx` (`TreeId`,`MapRevision`),
+  CONSTRAINT `MapHead_ibfk_1` FOREIGN KEY (`TreeId`) REFERENCES `Trees` (`TreeId`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE UNIQUE INDEX MapHeadRevisionIdx
-  ON MapHead(TreeId, MapRevision);
