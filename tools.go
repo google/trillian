902167a8aa5c403d452ce86d2c8c1c1a183c4dab
Gary Belvin <gdb@google.com>
2019-06-18 12:00:18 +0100

    Add tools package comment

--- tools.go
+++ tools.go
@@ -14,6 +14,8 @@
 
 // +build tools
 
+// Package tools tracks dependencies on binaries not otherwise referenced in the trillian codebase.
+// https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
 package tools
 
 import (
