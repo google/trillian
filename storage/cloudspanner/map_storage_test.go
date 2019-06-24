89d327d1bb2925284eaecb162452e4bf6b7a7970
Gary Belvin <gdb@google.com>
2019-06-24 16:30:55 +0100

    Set license headers

--- storage/cloudspanner/map_storage_test.go
+++ storage/cloudspanner/map_storage_test.go
@@ -1,3 +1,17 @@
+// Copyright 2019 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package cloudspanner
 
 import (
@@ -8,6 +22,10 @@ import (
 	"cloud.google.com/go/spanner"
 	"github.com/google/trillian/storage/testharness"
 )
+
+// To run cloudspanner tests,
+// 1) Set the -test_cloud_spanner_database flag
+// 2) Set application default credentials `gcloud auth application-default login`
 
 var cloudDBPath = flag.String("test_cloud_spanner_database", "", "projects/my-project/instances/my-instance/database/my-db")
 
