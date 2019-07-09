3328a6f001e895bca02d412ce6ef03cc4f002254
Pavel Kalinnikov <pkalinnikov@google.com>
2019-07-09 16:18:12 +0100

    Add RangeNodes test

--- merkle/compact/nodes_test.go
+++ merkle/compact/nodes_test.go
@@ -44,3 +44,46 @@ func TestRangeNodesForPrefix(t *testing.T) {
 		})
 	}
 }
+
+func TestRangeNodes(t *testing.T) {
+	n := func(level uint, index uint64) NodeID {
+		return NewNodeID(level, index)
+	}
+	for _, tc := range []struct {
+		begin uint64
+		end   uint64
+		want  []NodeID
+	}{
+		// Empty ranges.
+		{end: 0, want: []NodeID{}},
+		{begin: 10, end: 10, want: []NodeID{}},
+		{begin: 1024, end: 1024, want: []NodeID{}},
+		// One entry.
+		{begin: 10, end: 11, want: []NodeID{n(0, 10)}},
+		{begin: 1024, end: 1025, want: []NodeID{n(0, 1024)}},
+		{begin: 1025, end: 1026, want: []NodeID{n(0, 1025)}},
+		// Two entries.
+		{begin: 10, end: 12, want: []NodeID{n(1, 5)}},
+		{begin: 1024, end: 1026, want: []NodeID{n(1, 512)}},
+		{begin: 1025, end: 1027, want: []NodeID{n(0, 1025), n(0, 1026)}},
+		// Only left border.
+		{begin: 0, end: 16, want: []NodeID{n(4, 0)}},
+		{begin: 1, end: 16, want: []NodeID{n(0, 1), n(1, 1), n(2, 1), n(3, 1)}},
+		{begin: 1, end: 16, want: []NodeID{n(0, 1), n(1, 1), n(2, 1), n(3, 1)}},
+		{begin: 2, end: 16, want: []NodeID{n(1, 1), n(2, 1), n(3, 1)}},
+		{begin: 3, end: 16, want: []NodeID{n(0, 3), n(2, 1), n(3, 1)}},
+		{begin: 4, end: 16, want: []NodeID{n(2, 1), n(3, 1)}},
+		{begin: 6, end: 16, want: []NodeID{n(1, 3), n(3, 1)}},
+		{begin: 8, end: 16, want: []NodeID{n(3, 1)}},
+		{begin: 11, end: 16, want: []NodeID{n(0, 11), n(2, 3)}},
+		// Two-sided.
+		{begin: 1, end: 31, want: []NodeID{n(0, 1), n(1, 1), n(2, 1), n(3, 1), n(3, 2), n(2, 6), n(1, 14), n(0, 30)}},
+		{begin: 1, end: 17, want: []NodeID{n(0, 1), n(1, 1), n(2, 1), n(3, 1), n(0, 16)}},
+	} {
+		t.Run(fmt.Sprintf("range:%d:%d", tc.begin, tc.end), func(t *testing.T) {
+			if got, want := RangeNodes(tc.begin, tc.end), tc.want; !reflect.DeepEqual(got, tc.want) {
+				t.Fatalf("RangeNodes: got %v, want %v", got, want)
+			}
+		})
+	}
+}
