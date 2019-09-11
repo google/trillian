c161853a2698fb5bbb0adf7d2e7ef85a1bd63013
Thiébaud Weksteen <tweek@google.com>
2019-09-11 17:50:41 +0200

    examples/deployment: define default for $NAMESPACE
    Commit 8a50790 requires the definition of a default namespace for
    deploy.sh to succeed. This was added to the ci-config but not to
    the example one.

--- examples/deployment/kubernetes/example-config.sh
+++ examples/deployment/kubernetes/example-config.sh
@@ -8,6 +8,7 @@ export REGION=us-east4
 export MASTER_ZONE="${REGION}-a"
 export NODE_LOCATIONS="${REGION}-a,${REGION}-b,${REGION}-c"
 export CONFIGMAP=${DIR}/trillian-cloudspanner.yaml
+export NAMESPACE=default
 
 export POOLSIZE=2
 export MACHINE_TYPE="n1-standard-2"
