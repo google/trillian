d97bdcfcd7ab5a8c9d255fcc87db0e2b58176812
Pavel Kalinnikov <pkalinnikov@google.com>
2019-09-19 16:55:05 +0100

    storage: Move primitives to a separate package

--- storage/tree/node.go
+++ storage/tree/node.go
@@ -12,7 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package storage
+// Package tree defines types that help navigating a tree in storage.
+package tree
 
 import (
 	"bytes"
