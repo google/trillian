d3232a7002badd7b1097e31964100814c0d7376f
Pavel Kalinnikov <pkalinnikov@google.com>
2019-09-19 17:38:49 +0100

    Uncomment the aliases

--- storage/aliases.go
+++ storage/aliases.go
@@ -14,10 +14,11 @@
 
 package storage
 
+import tree "github.com/google/trillian/storage/tree"
+
 // TODO(pavelkalinnikov): These aliases were created to not break the code that
 // depended on these type. We should delete this file eventually.
 
-/*
 type NodeID = tree.NodeID
 type Node = tree.Node
 type Suffix = tree.Suffix
@@ -34,4 +35,3 @@ var (
 	EmptySuffix = tree.EmptySuffix
 	ParseSuffix = tree.ParseSuffix
 )
-*/
