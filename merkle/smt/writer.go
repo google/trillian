5835cfb590e2c0d33058249067bb0f9492ed0a61
Pavel Kalinnikov <pkalinnikov@google.com>
2019-10-03 13:42:34 +0100

    Commenting and yagnis

--- merkle/smt/writer.go
+++ merkle/smt/writer.go
@@ -22,7 +22,10 @@ import (
 	"github.com/google/trillian/storage/tree"
 )
 
-// NodeBatchAccessor reads and writes batches of Merkle tree node hashes.
+// NodeBatchAccessor reads and writes batches of Merkle tree node hashes. It is
+// a batch interface for efficiency reasons, as it is designed to guard tree
+// storage / database access. The Writer type operates on a per-shard basis,
+// i.e. it calls Get and Set method exactly once for each shard.
 type NodeBatchAccessor interface {
 	// Get returns the hashes of the given nodes, as a map keyed by their IDs.
 	// The returned hashes may be missing or be nil for empty subtrees.
@@ -31,49 +34,41 @@ type NodeBatchAccessor interface {
 	Set(upd []NodeUpdate) error
 }
 
-// Writer handles sharded writes to a sparse Merkle tree.
+// Writer handles sharded writes to a sparse Merkle tree. The tree has two
+// levels of shards: the single topmost shard spanning depths from 0 to split,
+// and 2^split second-level shards each spanning levels from split to height.
+// If the split height is 0 then effectively there is only one "global" shard.
 type Writer struct {
 	treeID int64
 	hasher hashers.MapHasher
-	// depths maps the depth of "leaf" nodes of a shard to its root depth.
-	depths map[uint]uint
+	height uint // The height of the tree.
+	split  uint // The height of the top shard.
 }
 
 // NewWriter creates a new Writer for the specified tree of the given height,
-// with 2 levels of sharding. One shard spans the the upper levels down to
-// split point, and all the other shards span the levels below.
+// with two levels of sharding, where the upper shard is `split` levels high.
 func NewWriter(treeID int64, hasher hashers.MapHasher, height, split uint) *Writer {
 	if split > height {
 		panic(fmt.Errorf("NewWriter: split(%d) > height(%d)", split, height))
 	}
-	depths := map[uint]uint{height: split, split: 0}
-	return &Writer{treeID: treeID, hasher: hasher, depths: depths}
+	return &Writer{treeID: treeID, hasher: hasher, height: height, split: split}
 }
 
 // Split sorts and splits the given list of node hash updates into shards, i.e.
 // the subsets belonging to different subtrees. The updates must belong to the
-// same tree level, one of the sharding split points. For example, with a
-// 2-level sharding it only makes sense to Split leaf updates.
+// same tree level which is equal to the tree height.
 func (w *Writer) Split(upd []NodeUpdate) ([][]NodeUpdate, error) {
-	if len(upd) == 0 { // Nothing to split.
-		return nil, nil
-	}
-	depth := upd[0].ID.BitLen()
-	if err := sortUpdates(upd, depth); err != nil {
+	if err := sortUpdates(upd, w.height); err != nil {
 		return nil, err
-	}
-	top, found := w.depths[depth]
-	if !found {
-		return nil, fmt.Errorf("unexpected depth %d", depth)
 	}
 	// TODO(pavelkalinnikov): Try estimating the capacity for this slice.
 	var shards [][]NodeUpdate
 	// The updates are sorted, so we can split them by prefix.
 	for begin, i := 0, 0; i < len(upd); i++ {
-		pref := upd[i].ID.Prefix(top)
+		pref := upd[i].ID.Prefix(w.split)
 		next := i + 1
 		// Check if this ID ends the shard.
-		if next == len(upd) || upd[next].ID.Prefix(top) != pref {
+		if next == len(upd) || upd[next].ID.Prefix(w.split) != pref {
 			shards = append(shards, upd[begin:next])
 			begin = next
 		}
@@ -83,24 +78,29 @@ func (w *Writer) Split(upd []NodeUpdate) ([][]NodeUpdate, error) {
 
 // Write applies the given list of updates to a single shard, and returns the
 // resulting update of the shard root. It uses the given node accessor for
-// reading and writing tree nodes. Typically, the input updates have been
-// obtained from Split method.
+// reading and writing tree nodes.
+//
+// The typical usage pattern is as follows. For the lower shards, the input is
+// the []NodeUpdate slices returned from the Split method. For the top shard,
+// the input is all the NodeUpdates from the lower shards Write calls.
+//
+// In another case, Write can be performed without Split if the shard split
+// depth is 0, which effectively means that there is only one "global" shard.
 func (w *Writer) Write(upd []NodeUpdate, acc NodeBatchAccessor) (NodeUpdate, error) {
 	if len(upd) == 0 {
 		return NodeUpdate{}, errors.New("nothing to write")
 	}
 	depth := upd[0].ID.BitLen()
-	top, found := w.depths[depth]
-	if !found {
-		return NodeUpdate{}, fmt.Errorf("unexpected depth %d", depth)
+	top, err := w.shardTop(depth)
+	if err != nil {
+		return NodeUpdate{}, err
 	}
 
 	hs, err := NewHStar3(upd, w.hasher.HashChildren, depth, top)
 	if err != nil {
 		return NodeUpdate{}, err
 	}
-	ids := hs.Prepare()
-	nodes, err := acc.Get(ids)
+	nodes, err := acc.Get(hs.Prepare())
 	if err != nil {
 		return NodeUpdate{}, err
 	}
@@ -118,6 +118,17 @@ func (w *Writer) Write(upd []NodeUpdate, acc NodeBatchAccessor) (NodeUpdate, err
 	return topUpd[0], nil
 }
 
+// shardTop returns the depth of a shard top based on its bottom depth.
+func (w *Writer) shardTop(depth uint) (uint, error) {
+	switch depth {
+	case w.height:
+		return w.split, nil
+	case w.split:
+		return 0, nil
+	}
+	return 0, fmt.Errorf("unexpected depth %d", depth)
+}
+
 // newAccessor returns a NodeAccessor for HStar3 algorithm based on the set of
 // preloaded node hashes.
 func (w *Writer) newAccessor(nodes map[tree.NodeID2][]byte) *shardAccessor {
@@ -129,7 +140,8 @@ func (w *Writer) newAccessor(nodes map[tree.NodeID2][]byte) *shardAccessor {
 	return &shardAccessor{w: w, reads: nodes, writes: writes}
 }
 
-// shardAccessor provides read and write access to nodes used by HStar3.
+// shardAccessor provides read and write access to nodes used by HStar3. It
+// operates entirely in-memory.
 type shardAccessor struct {
 	w      *Writer
 	reads  map[tree.NodeID2][]byte
