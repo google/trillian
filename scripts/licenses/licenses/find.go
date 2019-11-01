b95ebd60dac2c78350d90b5884c327e2ee23abc9
Rob Percival <robpercival@google.com>
2019-11-01 16:48:47 +0000

    Add more filenames to licenseRegexp
    Now that the file contents are checked for a license, it's fine to
    have false positives on filename matches - they'll get filtered out.

--- scripts/licenses/licenses/find.go
+++ scripts/licenses/licenses/find.go
@@ -23,7 +23,7 @@ import (
 )
 
 var (
-	licenseRegexp = regexp.MustCompile(`^LICEN(S|C)E(\.(txt|md))?$`)
+	licenseRegexp = regexp.MustCompile(`^(LICEN(S|C)E|COPYING|README|NOTICE)(\.(txt|md))?$`)
 	srcDirRegexps = func() []*regexp.Regexp {
 		var rs []*regexp.Regexp
 		for _, s := range build.Default.SrcDirs() {
