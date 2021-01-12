ce41e9baaed085c4d07135d0b98f7108ebc5f94c
Al Cutter <al@google.com>
2021-01-12 14:24:10 +0000

    Move Merkle verification stuff into new package

--- merkle/verifier/hash_chainer.go
+++ merkle/verifier/hash_chainer.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package merkle
+package verifier
 
 import (
 	"github.com/google/trillian/merkle/hashers"
