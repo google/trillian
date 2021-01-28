5b42e69c70ff835601c5422d7f6d0e59b5593e69
Pavel Kalinnikov <pkalinnikov@google.com>
2021-01-28 14:19:01 -0500

    Add comments to prepare.sh

--- integration/cloudbuild/prepare.sh
+++ integration/cloudbuild/prepare.sh
@@ -2,8 +2,22 @@
 
 set -ex
 
+# TODO(pavelkalinnikov): This script can be made a definitive "how to" for
+# setting up dev environment. Eliminate duplicating these steps in many places.
+
+# Install Google API definitions. Some APIs are used by the protoc tool when
+# [re-]generating Trillian API packages from protobufs.
+#
+# TODO(pavelkalinnikov): It doesn't have to be within $GOPATH. There is no Go
+# code/module in this repository, and we use it only for API proto definitions.
 git clone --depth=1 https://github.com/googleapis/googleapis.git "$GOPATH/src/github.com/googleapis/googleapis"
 
+# Install the tooling used for auto-generating code. Specifically, these are the
+# tools mentioned in //go:generate comments throughout this repository, and used
+# by the "go generate" command. In CI this is used for ensuring that developers
+# commit the generated files in an up-to-date state.
+#
+# TODO(pavelkalinnikov): Sort lexicographically (here and in other places).
 go install \
     github.com/golang/protobuf/proto \
     github.com/golang/mock/mockgen \
@@ -12,4 +26,5 @@ go install \
     github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc
 
 # Cache MySQL image for later steps.
+# TODO(pavelkalinnikov): Specific to MySQL tests. Move to a better place.
 docker-compose -f ./integration/cloudbuild/docker-compose-mysql.yaml pull
