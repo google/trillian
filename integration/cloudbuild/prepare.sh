cc85ed7864b72a0c5802ad86444258fdcfc66ef1
Pavel Kalinnikov <pkalinnikov@google.com>
2021-01-29 05:44:27 -0500

    Sort installs lexicographically

--- integration/cloudbuild/prepare.sh
+++ integration/cloudbuild/prepare.sh
@@ -16,14 +16,12 @@ git clone --depth=1 https://github.com/googleapis/googleapis.git "$GOPATH/src/gi
 # tools mentioned in //go:generate comments throughout this repository, and used
 # by the "go generate" command. In CI this is used for ensuring that developers
 # commit the generated files in an up-to-date state.
-#
-# TODO(pavelkalinnikov): Sort lexicographically (here and in other places).
 go install \
-    github.com/golang/protobuf/proto \
     github.com/golang/mock/mockgen \
-    golang.org/x/tools/cmd/stringer \
+    github.com/golang/protobuf/proto \
     github.com/golang/protobuf/protoc-gen-go \
-    github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc
+    github.com/pseudomuto/protoc-gen-doc/cmd/protoc-gen-doc \
+    golang.org/x/tools/cmd/stringer
 
 # Cache MySQL image for later steps.
 # TODO(pavelkalinnikov): Specific to MySQL tests. Move to a better place.
