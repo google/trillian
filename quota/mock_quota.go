ec74a4c914e57ce8b65ae066921456565fec9c86
Martin Hutchinson <mhutchinson@gmail.com>
2021-02-23 16:54:11 +0000

    Upgrade gomock (#2368)
    This is #2363 but with the mock files updated. This upgrade from 1.4.4 to 1.5.0.
    
    Note that after updating past this commit you will need to do `go install github.com/golang/mock/mockgen` to get the latest mockgen locally.

--- quota/mock_quota.go
+++ quota/mock_quota.go
@@ -6,34 +6,35 @@ package quota
 
 import (
 	context "context"
-	gomock "github.com/golang/mock/gomock"
 	reflect "reflect"
+
+	gomock "github.com/golang/mock/gomock"
 )
 
-// MockManager is a mock of Manager interface
+// MockManager is a mock of Manager interface.
 type MockManager struct {
 	ctrl     *gomock.Controller
 	recorder *MockManagerMockRecorder
 }
 
-// MockManagerMockRecorder is the mock recorder for MockManager
+// MockManagerMockRecorder is the mock recorder for MockManager.
 type MockManagerMockRecorder struct {
 	mock *MockManager
 }
 
-// NewMockManager creates a new mock instance
+// NewMockManager creates a new mock instance.
 func NewMockManager(ctrl *gomock.Controller) *MockManager {
 	mock := &MockManager{ctrl: ctrl}
 	mock.recorder = &MockManagerMockRecorder{mock}
 	return mock
 }
 
-// EXPECT returns an object that allows the caller to indicate expected use
+// EXPECT returns an object that allows the caller to indicate expected use.
 func (m *MockManager) EXPECT() *MockManagerMockRecorder {
 	return m.recorder
 }
 
-// GetTokens mocks base method
+// GetTokens mocks base method.
 func (m *MockManager) GetTokens(arg0 context.Context, arg1 int, arg2 []Spec) error {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "GetTokens", arg0, arg1, arg2)
@@ -41,13 +42,13 @@ func (m *MockManager) GetTokens(arg0 context.Context, arg1 int, arg2 []Spec) err
 	return ret0
 }
 
-// GetTokens indicates an expected call of GetTokens
+// GetTokens indicates an expected call of GetTokens.
 func (mr *MockManagerMockRecorder) GetTokens(arg0, arg1, arg2 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetTokens", reflect.TypeOf((*MockManager)(nil).GetTokens), arg0, arg1, arg2)
 }
 
-// PutTokens mocks base method
+// PutTokens mocks base method.
 func (m *MockManager) PutTokens(arg0 context.Context, arg1 int, arg2 []Spec) error {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "PutTokens", arg0, arg1, arg2)
@@ -55,13 +56,13 @@ func (m *MockManager) PutTokens(arg0 context.Context, arg1 int, arg2 []Spec) err
 	return ret0
 }
 
-// PutTokens indicates an expected call of PutTokens
+// PutTokens indicates an expected call of PutTokens.
 func (mr *MockManagerMockRecorder) PutTokens(arg0, arg1, arg2 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "PutTokens", reflect.TypeOf((*MockManager)(nil).PutTokens), arg0, arg1, arg2)
 }
 
-// ResetQuota mocks base method
+// ResetQuota mocks base method.
 func (m *MockManager) ResetQuota(arg0 context.Context, arg1 []Spec) error {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "ResetQuota", arg0, arg1)
@@ -69,7 +70,7 @@ func (m *MockManager) ResetQuota(arg0 context.Context, arg1 []Spec) error {
 	return ret0
 }
 
-// ResetQuota indicates an expected call of ResetQuota
+// ResetQuota indicates an expected call of ResetQuota.
 func (mr *MockManagerMockRecorder) ResetQuota(arg0, arg1 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ResetQuota", reflect.TypeOf((*MockManager)(nil).ResetQuota), arg0, arg1)
--- testonly/tmock/mock_admin_server.go
+++ testonly/tmock/mock_admin_server.go
@@ -6,35 +6,36 @@ package tmock
 
 import (
 	context "context"
+	reflect "reflect"
+
 	gomock "github.com/golang/mock/gomock"
 	trillian "github.com/google/trillian"
-	reflect "reflect"
 )
 
-// MockTrillianAdminServer is a mock of TrillianAdminServer interface
+// MockTrillianAdminServer is a mock of TrillianAdminServer interface.
 type MockTrillianAdminServer struct {
 	ctrl     *gomock.Controller
 	recorder *MockTrillianAdminServerMockRecorder
 }
 
-// MockTrillianAdminServerMockRecorder is the mock recorder for MockTrillianAdminServer
+// MockTrillianAdminServerMockRecorder is the mock recorder for MockTrillianAdminServer.
 type MockTrillianAdminServerMockRecorder struct {
 	mock *MockTrillianAdminServer
 }
 
-// NewMockTrillianAdminServer creates a new mock instance
+// NewMockTrillianAdminServer creates a new mock instance.
 func NewMockTrillianAdminServer(ctrl *gomock.Controller) *MockTrillianAdminServer {
 	mock := &MockTrillianAdminServer{ctrl: ctrl}
 	mock.recorder = &MockTrillianAdminServerMockRecorder{mock}
 	return mock
 }
 
-// EXPECT returns an object that allows the caller to indicate expected use
+// EXPECT returns an object that allows the caller to indicate expected use.
 func (m *MockTrillianAdminServer) EXPECT() *MockTrillianAdminServerMockRecorder {
 	return m.recorder
 }
 
-// CreateTree mocks base method
+// CreateTree mocks base method.
 func (m *MockTrillianAdminServer) CreateTree(arg0 context.Context, arg1 *trillian.CreateTreeRequest) (*trillian.Tree, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "CreateTree", arg0, arg1)
@@ -43,13 +44,13 @@ func (m *MockTrillianAdminServer) CreateTree(arg0 context.Context, arg1 *trillia
 	return ret0, ret1
 }
 
-// CreateTree indicates an expected call of CreateTree
+// CreateTree indicates an expected call of CreateTree.
 func (mr *MockTrillianAdminServerMockRecorder) CreateTree(arg0, arg1 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "CreateTree", reflect.TypeOf((*MockTrillianAdminServer)(nil).CreateTree), arg0, arg1)
 }
 
-// DeleteTree mocks base method
+// DeleteTree mocks base method.
 func (m *MockTrillianAdminServer) DeleteTree(arg0 context.Context, arg1 *trillian.DeleteTreeRequest) (*trillian.Tree, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "DeleteTree", arg0, arg1)
@@ -58,13 +59,13 @@ func (m *MockTrillianAdminServer) DeleteTree(arg0 context.Context, arg1 *trillia
 	return ret0, ret1
 }
 
-// DeleteTree indicates an expected call of DeleteTree
+// DeleteTree indicates an expected call of DeleteTree.
 func (mr *MockTrillianAdminServerMockRecorder) DeleteTree(arg0, arg1 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "DeleteTree", reflect.TypeOf((*MockTrillianAdminServer)(nil).DeleteTree), arg0, arg1)
 }
 
-// GetTree mocks base method
+// GetTree mocks base method.
 func (m *MockTrillianAdminServer) GetTree(arg0 context.Context, arg1 *trillian.GetTreeRequest) (*trillian.Tree, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "GetTree", arg0, arg1)
@@ -73,13 +74,13 @@ func (m *MockTrillianAdminServer) GetTree(arg0 context.Context, arg1 *trillian.G
 	return ret0, ret1
 }
 
-// GetTree indicates an expected call of GetTree
+// GetTree indicates an expected call of GetTree.
 func (mr *MockTrillianAdminServerMockRecorder) GetTree(arg0, arg1 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetTree", reflect.TypeOf((*MockTrillianAdminServer)(nil).GetTree), arg0, arg1)
 }
 
-// ListTrees mocks base method
+// ListTrees mocks base method.
 func (m *MockTrillianAdminServer) ListTrees(arg0 context.Context, arg1 *trillian.ListTreesRequest) (*trillian.ListTreesResponse, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "ListTrees", arg0, arg1)
@@ -88,13 +89,13 @@ func (m *MockTrillianAdminServer) ListTrees(arg0 context.Context, arg1 *trillian
 	return ret0, ret1
 }
 
-// ListTrees indicates an expected call of ListTrees
+// ListTrees indicates an expected call of ListTrees.
 func (mr *MockTrillianAdminServerMockRecorder) ListTrees(arg0, arg1 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ListTrees", reflect.TypeOf((*MockTrillianAdminServer)(nil).ListTrees), arg0, arg1)
 }
 
-// UndeleteTree mocks base method
+// UndeleteTree mocks base method.
 func (m *MockTrillianAdminServer) UndeleteTree(arg0 context.Context, arg1 *trillian.UndeleteTreeRequest) (*trillian.Tree, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "UndeleteTree", arg0, arg1)
@@ -103,13 +104,13 @@ func (m *MockTrillianAdminServer) UndeleteTree(arg0 context.Context, arg1 *trill
 	return ret0, ret1
 }
 
-// UndeleteTree indicates an expected call of UndeleteTree
+// UndeleteTree indicates an expected call of UndeleteTree.
 func (mr *MockTrillianAdminServerMockRecorder) UndeleteTree(arg0, arg1 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "UndeleteTree", reflect.TypeOf((*MockTrillianAdminServer)(nil).UndeleteTree), arg0, arg1)
 }
 
-// UpdateTree mocks base method
+// UpdateTree mocks base method.
 func (m *MockTrillianAdminServer) UpdateTree(arg0 context.Context, arg1 *trillian.UpdateTreeRequest) (*trillian.Tree, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "UpdateTree", arg0, arg1)
@@ -118,7 +119,7 @@ func (m *MockTrillianAdminServer) UpdateTree(arg0 context.Context, arg1 *trillia
 	return ret0, ret1
 }
 
-// UpdateTree indicates an expected call of UpdateTree
+// UpdateTree indicates an expected call of UpdateTree.
 func (mr *MockTrillianAdminServerMockRecorder) UpdateTree(arg0, arg1 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "UpdateTree", reflect.TypeOf((*MockTrillianAdminServer)(nil).UpdateTree), arg0, arg1)
