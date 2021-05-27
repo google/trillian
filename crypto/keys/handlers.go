178321e23ec70fbb5ac3993f3f2e53415bac67d8
Pierre Phaneuf <pphaneuf@google.com>
2021-05-27 09:44:01 +0100

    Inline the docs in crypto/.
    It's all fairly simple one-liners, there is no benefit to keeping them
    separate.

--- crypto/keys/handlers.go
+++ crypto/keys/handlers.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package keys provides access to public and private keys for signing and verification of signatures.
 package keys
 
 import (
