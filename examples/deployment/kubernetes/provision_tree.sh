ec8964d4371b6ca94bd165fad6f88d0783ec3635
Martin Hutchinson <mhutchinson@gmail.com>
2022-02-17 10:21:45 +0000

    Update provision tree script in k8s to use createtree
    This tool replaces the manual instructions that involved the REST API.

--- examples/deployment/kubernetes/provision_tree.sh
+++ examples/deployment/kubernetes/provision_tree.sh
@@ -14,29 +14,18 @@ fi
 
 # Set up tunnel:
 PORT=35791
-export LOG_URL="http://127.0.0.1:${PORT}"
+export LOG_RPC="http://127.0.0.1:${PORT}"
 
-echo kubectl port-forward service/trillian-log-service ${PORT}:8091
-kubectl port-forward service/trillian-log-service ${PORT}:8091 &
+echo kubectl port-forward service/trillian-log-service ${PORT}:8090
+kubectl port-forward service/trillian-log-service ${PORT}:8090 &
 trap "kill %1" 0 1 2 3 4 6 9 15
 
 sleep 3
 
 echo "Creating tree..."
-# TODO(al): use cmd/createtree instead.
-TREE=$(curl -sb -X POST ${LOG_URL}/v1beta1/trees -d '{ "tree":{ "tree_state":"ACTIVE", "tree_type":"LOG", "hash_strategy":"RFC6962_SHA256", "signature_algorithm":"ECDSA", "max_root_duration":"0", "hash_algorithm":"SHA256" }, "key_spec":{ "ecdsa_params":{ "curve":"P256" } } }')
-
-echo $TREE
-
-TREEID=$(echo ${TREE} | jq -r .tree_id)
+TREEID=$(go run github.com/google/trillian/cmd/createtree --admin_server=${LOG_RPC})
 
 echo "Created tree ${TREEID}:"
-echo ${TREE} | jq
-
-echo "Initialising tree ${TREEID}:"
-STH=$(curl -s -X POST ${LOG_URL}/v1beta1/logs/${TREEID}:init)
-echo "Created STH:"
-echo ${STH} | jq
 
 # stop port-forwarding
 kill %1
