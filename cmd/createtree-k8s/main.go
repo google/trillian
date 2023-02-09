b7d95be4c48425fead6382efb7d60bad02fd286a
Ville Aikas <vaikas@chainguard.dev>
2023-02-09 08:23:57 -0800

    use build-ko/setup-ko, go 1.19, add package comment. Signed-off-by: Ville Aikas <vaikas@chainguard.dev>

--- cmd/createtree-k8s/main.go
+++ cmd/createtree-k8s/main.go
@@ -1,10 +1,10 @@
-// Copyright 2022 The Sigstore Authors
+// Copyright 2023 Google LLC. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,6 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package main contains the implementation and entry point for the
+// creating a tree in Trillian and adding it into a ConfigMap.
+// More details and use cases are in:
+// ../../examples/deployment/kubernetes/README-SCAFFOLDING.md
 package main
 
 import (
