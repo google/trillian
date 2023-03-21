dc3c8bb5f08c251b14970cab8559470602b8a5f2
Simba Peng <1531315@qq.com>
2023-03-21 21:27:34 +0800

    Add test cases in case of execution errors

--- storage/stmtcache/stmtcache_test.go
+++ storage/stmtcache/stmtcache_test.go
@@ -38,10 +38,10 @@ func TestStmtExecContext(t *testing.T) {
 	sql := `SELECT ` + stmtcache.PlaceholderSQL
 	stmt, err := cache.GetStmt(ctx, sql, 1, "?", "")
 	if err != nil {
-		t.Fatalf("failed to cache.GetStmt: %s", err)
+		t.Fatalf("Failed to cache.GetStmt: %s", err)
 	}
 	if _, err = stmt.ExecContext(ctx, ""); err != nil {
-		t.Fatalf("failed to stmt.ExecContext: %s", err)
+		t.Fatalf("Failed to stmt.ExecContext: %s", err)
 	}
 }
 
@@ -51,23 +51,23 @@ func TestStmtQueryContext(t *testing.T) {
 	sql := `SELECT ` + stmtcache.PlaceholderSQL
 	stmt, err := cache.GetStmt(ctx, sql, 1, "?", "")
 	if err != nil {
-		t.Fatalf("failed to cache.GetStmt: %s", err)
+		t.Fatalf("Failed to cache.GetStmt: %s", err)
 	}
 	want := "TestQuery"
 	rows, err := stmt.QueryContext(ctx, want)
 	if err != nil {
-		t.Fatalf("failed to stmt.QueryContext: %s", err)
+		t.Fatalf("Failed to stmt.QueryContext: %s", err)
 	}
 	defer rows.Close()
 
 	rows.Next()
 	var res string
 	if err := rows.Scan(&res); err != nil {
-		t.Fatalf("failed to rows.Scan: %s", err)
+		t.Fatalf("Failed to rows.Scan: %s", err)
 	}
 
 	if res != want {
-		t.Errorf("unexpected results")
+		t.Errorf("Unexpected results")
 	}
 }
 
@@ -77,31 +77,31 @@ func TestStmtQueryRowContext(t *testing.T) {
 	sql := `SELECT ` + stmtcache.PlaceholderSQL
 	stmt, err := cache.GetStmt(ctx, sql, 1, "?", "")
 	if err != nil {
-		t.Fatalf("failed to cache.GetStmt: %s", err)
+		t.Fatalf("Failed to cache.GetStmt: %s", err)
 	}
 	want := "TestQueryRow"
 	row := stmt.QueryRowContext(ctx, want)
 	if err != nil {
-		t.Fatalf("failed to stmt.QueryRowContext: %s", err)
+		t.Fatalf("Failed to stmt.QueryRowContext: %s", err)
 	}
 	var res string
 	if err := row.Scan(&res); err != nil {
-		t.Fatalf("failed to row.Scan: %s", err)
+		t.Fatalf("Failed to row.Scan: %s", err)
 	}
 
 	if res != want {
-		t.Errorf("unexpected results")
+		t.Errorf("Unexpected results")
 	}
 }
 
 func TestStmtWithTx(t *testing.T) {
 	ctx := context.Background()
 	if _, err := db.ExecContext(ctx, "CREATE TABLE TestStmtWithTx(ID int)"); err != nil {
-		t.Fatalf("failed to create table: %s", err)
+		t.Fatalf("Failed to create table: %s", err)
 	}
 	defer func() {
 		if _, err := db.ExecContext(ctx, "DROP TABLE TestStmtWithTx"); err != nil {
-			klog.Errorf("failed to drop table: %s", err)
+			klog.Errorf("Failed to drop table: %s", err)
 		}
 	}()
 
@@ -109,11 +109,11 @@ func TestStmtWithTx(t *testing.T) {
 	sql := `INSERT INTO TestStmtWithTx(ID) ` + stmtcache.PlaceholderSQL
 	stmt, err := cache.GetStmt(ctx, sql, 1, "VALUES(?)", "(?)")
 	if err != nil {
-		t.Fatalf("failed to cache.GetStmt: %s", err)
+		t.Fatalf("Failed to cache.GetStmt: %s", err)
 	}
 	tx, err := db.BeginTx(ctx, nil)
 	if err != nil {
-		t.Fatalf("failed to db.BeginTx: %s", err)
+		t.Fatalf("Failed to db.BeginTx: %s", err)
 	}
 	stx := stmt.WithTx(ctx, tx)
 	defer stx.Close()
@@ -121,36 +121,45 @@ func TestStmtWithTx(t *testing.T) {
 	id := 1
 	_, err = stx.ExecContext(ctx, id)
 	if err != nil {
-		t.Fatalf("failed to stx.ExecContext: %s", err)
+		t.Fatalf("Failed to stx.ExecContext: %s", err)
 	}
 
 	if err := tx.Rollback(); err != nil {
-		klog.Errorf("failed to tx.Rollback: %s", err)
+		klog.Errorf("Failed to tx.Rollback: %s", err)
 	}
 
 	row := db.QueryRowContext(ctx, "SELECT COUNT(*) FROM TestStmtWithTx WHERE ID = ?", id)
 	var count int
 	if err = row.Scan(&count); err != nil {
-		t.Fatalf("failed to row.Scan: %s", err)
+		t.Fatalf("Failed to row.Scan: %s", err)
 	}
 
 	if count != 0 {
-		t.Errorf("transaction not rolled back")
+		t.Errorf("Transaction not rolled back")
 	}
 }
 
-// func TestStmtExecutionError(t *testing.T) {
-// 	db.SetMaxIdleConns(0)
-// 	cache := stmtcache.New(db, nil)
-// 	ctx := context.Background()
-// 	sql := `SELECT ` + stmtcache.PlaceholderSQL
-// 	_, err := cache.GetStmt(ctx, sql, 1, "?", "")
-// 	if err != nil {
-// 		t.Fatalf("failed to cache.GetStmt: %s", err)
-// 	}
-// 	row := db.QueryRowContext(ctx, "SELECT sys.ps_thread_id(connection_id())")
-// 	var tid int
-// 	if err = row.Scan(&tid); err != nil {
-// 		t.Fatalf("failed to get sys.ps_thread_id: %s", err)
-// 	}
-// }
+func TestStmtExecutionError(t *testing.T) {
+	cache := stmtcache.New(db, nil)
+	ctx := context.Background()
+	sql := `SELECT ` + stmtcache.PlaceholderSQL
+	stmt, err := cache.GetStmt(ctx, sql, 1, "?", "")
+	if err != nil {
+		t.Fatalf("Failed to cache.GetStmt: %s", err)
+	}
+
+	if err = stmt.SQLStmt().Close(); err != nil {
+		t.Fatalf("Failed to close sql.Stmt: %s", err)
+	}
+	// Execution error trigger cache clear logic
+	if _, err = stmt.ExecContext(ctx, ""); err == nil {
+		t.Fatal("Unexpected execution succeeded")
+	}
+	stmt, err = cache.GetStmt(ctx, sql, 1, "?", "")
+	if err != nil {
+		t.Fatalf("Failed to cache.GetStmt: %s", err)
+	}
+	if _, err = stmt.ExecContext(ctx, ""); err != nil {
+		t.Fatalf("Failed to stmt.ExecContext: %s", err)
+	}
+}
