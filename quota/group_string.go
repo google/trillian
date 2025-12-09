9bcaca70e2bbb0a145dd8275066b73b6d93d3034
Martin Hutchinson <mhutchinson@gmail.com>
2025-12-09 11:58:15 +0000

    Regenerate generated files

--- quota/group_string.go
+++ quota/group_string.go
@@ -18,8 +18,9 @@ const _Group_name = "GlobalTreeUser"
 var _Group_index = [...]uint8{0, 6, 10, 14}
 
 func (i Group) String() string {
-	if i < 0 || i >= Group(len(_Group_index)-1) {
+	idx := int(i) - 0
+	if i < 0 || idx >= len(_Group_index)-1 {
 		return "Group(" + strconv.FormatInt(int64(i), 10) + ")"
 	}
-	return _Group_name[_Group_index[i]:_Group_index[i+1]]
+	return _Group_name[_Group_index[idx]:_Group_index[idx+1]]
 }
